9097b56db0ce25a9455c95625c36236ea0058922
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 04:05:03 +0530

    Add CallbackQueryTests

---
+++ test/Telegram.Bots.Json.Tests.Units/CallbackQueryTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Telegram.Bots.Types;
+using Xunit;
+
+namespace Telegram.Bots.Json.Tests.Units
+{
+  public sealed class CallbackQueryTests : IClassFixture<Serializer>
+  {
+    private readonly Serializer _serializer;
+
+    public CallbackQueryTests(Serializer serializer) => _serializer = serializer;
+
+    public static TheoryData<(string, Type)> DeserializationData => new TheoryData<(string, Type)>
+    {
+      (@"{""message"":{""text"":""""},""game_short_name"":""""}", typeof(GameCallbackQuery)),
+      (@"{""message"":{""text"":""""},""data"":""""}", typeof(MessageCallbackQuery)),
+      (@"{""inline_message_id"":"""",""game_short_name"":""""}", typeof(InlineGameCallbackQuery)),
+      (@"{""inline_message_id"":"""",""data"":""""}", typeof(InlineMessageCallbackQuery))
+    };
+
+    [Theory(DisplayName = "CallbackQuery deserializes correctly")]
+    [MemberData(nameof(DeserializationData))]
+    public void CallbackQueryDeserializesCorrectly((string, Type) tuple)
+    {
+      var (data, type) = tuple;
+
+      Assert.IsAssignableFrom(type, _serializer.Deserialize<CallbackQuery>(data));
+    }
+  }
+}
