8a015ff4ca23a9a50c984d88327bf94e1b4156b5
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 03:02:07 +0530

    Add StringEnumTests

---
+++ test/Telegram.Bots.Json.Tests.Units/StringEnumTests.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace Telegram.Bots.Json.Tests.Units
+{
+  public sealed class StringEnumTests : IClassFixture<Serializer>
+  {
+    private const string Json = @"[""test_enum"",""another""]";
+
+    private static readonly IEnumerable<MyEnum> Values =
+      new List<MyEnum> { MyEnum.TestEnum, MyEnum.Another };
+
+    private readonly Serializer _serializer;
+
+    public StringEnumTests(Serializer serializer) => _serializer = serializer;
+
+    [Fact(DisplayName = "Serialization uses string name for enum values")]
+    public void SerializationUsesStringNameForEnumValues() =>
+      Assert.Equal(Json, _serializer.Serialize(Values));
+
+    [Fact(DisplayName = "Deserialization uses string name for enum values")]
+    public void DeserializationUsesStringNameForEnumValues() =>
+      Assert.Equal(Values, _serializer.Deserialize<IReadOnlyList<MyEnum>>(Json));
+
+    [Fact(DisplayName = "Serialization respects EnumMember attribute")]
+    public void SerializationRespectsEnumMemberAttribute() =>
+      Assert.Equal(@"""🎲""", _serializer.Serialize(MyEnum.Dice));
+
+    [Fact(DisplayName = "Deserialization respects EnumMember attribute")]
+    public void DeserializationRespectsEnumMemberAttribute() =>
+      Assert.Equal(MyEnum.Dice, _serializer.Deserialize<MyEnum>(@"""🎲"""));
+
+    private enum MyEnum
+    {
+      TestEnum,
+      Another,
+      [EnumMember(Value = "🎲")] Dice
+    }
+  }
+}
