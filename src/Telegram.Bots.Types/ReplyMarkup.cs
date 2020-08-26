ef9f1e98dda4a6bbae6a6d08a805fb82ac964892
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 05:28:02 +0530

    Add ReplyMarkup

---
+++ src/Telegram.Bots.Types/ReplyMarkup.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace Telegram.Bots.Types
+{
+  using Keyboard = IEnumerable<KeyboardButton[]>;
+  using InlineKeyboard = IEnumerable<InlineKeyboardButton[]>;
+
+  public abstract class ReplyMarkup { }
+
+  public sealed class KeyboardMarkup : ReplyMarkup
+  {
+    public Keyboard Keyboard { get; }
+
+    public bool? Resize { get; set; }
+
+    public bool? OneTime { get; set; }
+
+    public bool? Selective { get; set; }
+
+    public KeyboardMarkup(Keyboard keyboard) => Keyboard = keyboard;
+  }
+
+  public sealed class InlineKeyboardMarkup : ReplyMarkup
+  {
+    public InlineKeyboard Keyboard { get; }
+
+    public InlineKeyboardMarkup(InlineKeyboard keyboard) => Keyboard = keyboard;
+  }
+
+  public sealed class ForceReplyMarkup : ReplyMarkup
+  {
+    public bool ForceReply { get; } = true;
+
+    public bool? Selective { get; set; }
+  }
+
+  public sealed class RemoveMarkup : ReplyMarkup
+  {
+    public bool RemoveKeyboard { get; } = true;
+
+    public bool? Selective { get; set; }
+  }
+}
