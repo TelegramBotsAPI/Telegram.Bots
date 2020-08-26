5167bd5d6727c5b94c87860567b6a0f4da16abf7
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-26 20:37:32 +0530

    Add ChatAction

---
+++ src/Telegram.Bots.Types/ChatAction.cs
@@ -0,0 +1,16 @@
+namespace Telegram.Bots.Types
+{
+  public enum ChatAction
+  {
+    Typing,
+    UploadPhoto,
+    RecordVideo,
+    UploadVideo,
+    RecordAudio,
+    UploadAudio,
+    UploadDocument,
+    FindLocation,
+    RecordVideoNote,
+    UploadVideoNote
+  }
+}
