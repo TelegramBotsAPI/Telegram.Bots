10756f72f57cf1df03aada9fe51ddb2d1c7484ad
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-26 19:34:54 +0530

    Add Message

---
+++ src/Telegram.Bots.Types/Message.cs
@@ -0,0 +1,33 @@
+namespace Telegram.Bots.Types
+{
+  public abstract class Message
+  {
+    public int Id { get; set; }
+
+    public User? From { get; set; }
+
+    public long Date { get; set; }
+
+    public Chat Chat { get; set; } = null!;
+
+    public User? ForwardFrom { get; set; }
+
+    public Chat? ForwardFromChat { get; set; }
+
+    public int? ForwardFromMessageId { get; set; }
+
+    public string? ForwardSignature { get; set; }
+
+    public string? ForwardSenderName { get; set; }
+
+    public long? ForwardDate { get; set; }
+
+    public Message? ReplyToMessage { get; set; }
+
+    public User? ViaBot { get; set; }
+
+    public long? EditDate { get; set; }
+
+    public string? AuthorSignature { get; set; }
+  }
+}
