e3eb03e13b895ef3f120854d596f14db994964bd
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 04:13:49 +0530

    Add InputFileTests

---
+++ test/Telegram.Bots.Json.Tests.Units/InputFileTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using Telegram.Bots.Types;
+using Xunit;
+
+namespace Telegram.Bots.Json.Tests.Units
+{
+  public sealed class InputFileTests : IClassFixture<Serializer>
+  {
+    private static readonly InputFile File = new InputFile(Stream.Null);
+
+    private readonly Serializer _serializer;
+
+    public InputFileTests(Serializer serializer) => _serializer = serializer;
+
+    [Fact(DisplayName = "Serialization prepends \"attach://\" to Id")]
+    public void SerializationPrependsAttachSchemeToId() =>
+      Assert.Equal($@"""attach://{File.Id}""", _serializer.Serialize(File));
+
+    [Fact(DisplayName = "Deserialization throws NotImplementedException")]
+    public void DeserializationThrowsNotImplementedException() =>
+      Assert.Throws<NotImplementedException>(() =>
+        _serializer.Deserialize<InputFile>(_serializer.Serialize(File)));
+  }
+}
