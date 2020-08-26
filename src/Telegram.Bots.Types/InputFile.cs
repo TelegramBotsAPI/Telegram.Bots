d5bdc6a80561cca1d07bb52a9747746cdc82e5ce
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-26 22:10:12 +0530

    Add InputFile

---
+++ src/Telegram.Bots.Types/InputFile.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+
+namespace Telegram.Bots.Types
+{
+  public sealed class InputFile
+  {
+    public string Id { get; } = Guid.NewGuid().ToString();
+
+    public Stream Data { get; }
+
+    public InputFile(Stream data)
+    {
+      Data = data;
+    }
+  }
+}
