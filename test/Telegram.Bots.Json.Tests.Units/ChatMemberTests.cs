149f4800d611ee92967c4585f710f006218299b0
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 06:23:07 +0530

    Add ChatMemberTests

---
+++ test/Telegram.Bots.Json.Tests.Units/ChatMemberTests.cs
@@ -0,0 +1,32 @@
+using System;
+using Telegram.Bots.Types;
+using Xunit;
+
+namespace Telegram.Bots.Json.Tests.Units
+{
+  public sealed class ChatMemberTests : IClassFixture<Serializer>
+  {
+    private readonly Serializer _serializer;
+
+    public ChatMemberTests(Serializer serializer) => _serializer = serializer;
+
+    public static TheoryData<(string, Type)> DeserializationData => new TheoryData<(string, Type)>
+    {
+      (@"{""status"":""creator""}", typeof(Creator)),
+      (@"{""status"":""administrator""}", typeof(Administrator)),
+      (@"{""status"":""member""}", typeof(NormalMember)),
+      (@"{""status"":""restricted""}", typeof(RestrictedMember)),
+      (@"{""status"":""left""}", typeof(LeftMember)),
+      (@"{""status"":""kicked""}", typeof(KickedMember))
+    };
+
+    [Theory(DisplayName = "ChatMember deserializes correctly")]
+    [MemberData(nameof(DeserializationData))]
+    public void ChatMemberDeserializesCorrectly((string, Type) tuple)
+    {
+      var (value, type) = tuple;
+
+      Assert.IsAssignableFrom(type, _serializer.Deserialize<ChatMember>(value));
+    }
+  }
+}
