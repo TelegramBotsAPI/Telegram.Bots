ac1a5d3ceb033cfd8c939c14e4ec2a9bd438a798
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 05:57:47 +0530

    Update ContractResolver

--- src/Telegram.Bots.Json/Internal/ContractResolver.cs
+++ src/Telegram.Bots.Json/Internal/ContractResolver.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Telegram.Bots.Types;
+using Telegram.Bots.Types.Games;
 
 namespace Telegram.Bots.Json.Internal
 {
@@ -32,7 +33,22 @@ namespace Telegram.Bots.Json.Internal
             { "id", "file_id" }, { "unique_id", "file_unique_id" }, { "size", "file_size" }
           }
         },
-        { typeof(FileInfo), new Data { { "path", "file_path" } } }
+        { typeof(FileInfo), new Data { { "path", "file_path" } } },
+        {
+          typeof(KeyboardMarkup),
+          new Data { { "resize", "resize_keyboard" }, { "one_time", "one_time_keyboard" } }
+        },
+        { typeof(InlineKeyboardMarkup), new Data { { "keyboard", "inline_keyboard" } } },
+        { typeof(CallbackDataButton), new Data { { "data", "callback_data" } } },
+        { typeof(SwitchInlineQueryButton), new Data { { "query", "switch_inline_query" } } },
+        {
+          typeof(SwitchInlineQueryCurrentChatButton),
+          new Data { { "query", "switch_inline_query_current_chat" } }
+        },
+        { typeof(CallbackGameButton), new Data { { "game", "callback_game" } } },
+        { typeof(Game), new Data { { "photo_set", "photo" } } },
+        { typeof(Message), new Data { { "id", "message_id" } } },
+        { typeof(UserProfilePhotos), new Data { { "photo_sets", "photos" } } }
       };
 
     protected override JsonProperty CreateProperty(
