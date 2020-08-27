2fcdd9b6bb8c79dedfdf8b072702b20e2502cf89
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 03:40:06 +0530

    Add SendLocation

---
+++ src/Telegram.Bots.Requests/SendLocation.cs
@@ -0,0 +1,46 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class SendLocation<TChatId> : IRequest<LocationMessage>,
+    IChatTargetable<TChatId>, INotifiable, IReplyable, IMarkupable
+  {
+    public TChatId ChatId { get; }
+
+    public double Latitude { get; }
+
+    public double Longitude { get; }
+
+    public int? LivePeriod { get; set; }
+
+    public bool? DisableNotification { get; set; }
+
+    public int? ReplyToMessageId { get; set; }
+
+    public ReplyMarkup? ReplyMarkup { get; set; }
+
+    public string Method { get; } = "sendLocation";
+
+    protected SendLocation(TChatId chatId, double latitude, double longitude)
+    {
+      ChatId = chatId;
+      Latitude = latitude;
+      Longitude = longitude;
+    }
+  }
+
+  public sealed class SendLocation : SendLocation<long>
+  {
+    public SendLocation(long chatId, double latitude, double longitude) :
+      base(chatId, latitude, longitude) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendLocation : SendLocation<string>
+    {
+      public SendLocation(string username, double latitude, double longitude) :
+        base(username, latitude, longitude) { }
+    }
+  }
+}
