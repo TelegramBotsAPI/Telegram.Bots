e5fc3093dfd3b0f0fa3d09ae1a142f6cb4b03f77
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 12:43:24 +0530

    Add Update with UpdateType

---
+++ src/Telegram.Bots.Types/Update.cs
@@ -0,0 +1,65 @@
+using Telegram.Bots.Types.Inline;
+using Telegram.Bots.Types.Payments;
+
+namespace Telegram.Bots.Types
+{
+  public abstract class Update
+  {
+    public int Id { get; set; }
+  }
+
+  public sealed class MessageUpdate : Update
+  {
+    public Message Data { get; set; } = null!;
+  }
+
+  public sealed class EditedMessageUpdate : Update
+  {
+    public Message Data { get; set; } = null!;
+  }
+
+  public sealed class ChannelPostUpdate : Update
+  {
+    public Message Data { get; set; } = null!;
+  }
+
+  public sealed class EditedChannelPostUpdate : Update
+  {
+    public Message Data { get; set; } = null!;
+  }
+
+  public sealed class InlineQueryUpdate : Update
+  {
+    public InlineQuery Data { get; set; } = null!;
+  }
+
+  public sealed class ChosenInlineResultUpdate : Update
+  {
+    public ChosenInlineResult Data { get; set; } = null!;
+  }
+
+  public sealed class CallbackQueryUpdate : Update
+  {
+    public CallbackQuery Data { get; set; } = null!;
+  }
+
+  public sealed class ShippingQueryUpdate : Update
+  {
+    public ShippingQuery Data { get; set; } = null!;
+  }
+
+  public sealed class PreCheckoutQueryUpdate : Update
+  {
+    public PreCheckoutQuery Data { get; set; } = null!;
+  }
+
+  public sealed class PollUpdate : Update
+  {
+    public Poll Data { get; set; } = null!;
+  }
+
+  public sealed class PollAnswerUpdate : Update
+  {
+    public PollAnswer Data { get; set; } = null!;
+  }
+}
---
+++ src/Telegram.Bots.Types/UpdateType.cs
@@ -0,0 +1,17 @@
+namespace Telegram.Bots.Types
+{
+  public enum UpdateType
+  {
+    Message,
+    EditedMessage,
+    ChannelPost,
+    EditedChannelPost,
+    InlineQuery,
+    ChosenInlineResult,
+    CallbackQuery,
+    ShippingQuery,
+    PreCheckoutQuery,
+    Poll,
+    PollAnswer
+  }
+}
