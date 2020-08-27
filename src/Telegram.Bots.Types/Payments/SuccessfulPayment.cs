de98b02b1f6c71da6888f667cf3745dd9785f314
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 10:53:31 +0530

    Add SuccessfulPayment

---
+++ src/Telegram.Bots.Types/Payments/SuccessfulPayment.cs
@@ -0,0 +1,19 @@
+namespace Telegram.Bots.Types.Payments
+{
+  public sealed class SuccessfulPayment
+  {
+    public string Currency { get; set; } = null!;
+
+    public int TotalAmount { get; set; }
+
+    public string InvoicePayload { get; set; } = null!;
+
+    public string? ShippingOptionId { get; set; }
+
+    public OrderInfo? OrderInfo { get; set; }
+
+    public string TelegramPaymentChargeId { get; set; } = null!;
+
+    public string ProviderPaymentChargeId { get; set; } = null!;
+  }
+}
