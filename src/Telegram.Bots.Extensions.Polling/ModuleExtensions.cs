e55037fcff246a849759b3cefd5108e51d7fc34d
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 20:44:57 +0530

    Add ModuleExtensions for Integrating PollingService

---
+++ src/Telegram.Bots.Extensions.Polling/ModuleExtensions.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bots.Extensions.Polling.Configs;
+using Telegram.Bots.Extensions.Polling.Services;
+
+namespace Telegram.Bots.Extensions.Polling
+{
+  using IServices = IServiceCollection;
+
+  public static class ModuleExtensions
+  {
+    public static IServices AddPolling(this IServices services, IConfiguration? config = null)
+    {
+      var pollingConfig = config is null
+        ? new PollingConfig()
+        : config.GetSection("Polling").Get<PollingConfig>();
+
+      return services.AddPolling(pollingConfig);
+    }
+
+    public static IServices AddPolling(this IServices services, PollingConfig config) =>
+      services.AddSingleton(config).AddHostedService<PollingService>();
+  }
+}
