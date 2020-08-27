e16d95604dd1228149baf0522078463aee629c47
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 20:39:18 +0530

    Add PollingConfig

---
+++ src/Telegram.Bots.Extensions.Polling/Configs/PollingConfig.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Extensions.Polling.Configs
+{
+  public sealed class PollingConfig
+  {
+    public int Limit { get; set; } = 100;
+
+    public int Interval { get; set; } = 10;
+
+    public int Timeout { get; set; } = 60;
+
+    public IEnumerable<UpdateType>? AllowedUpdates { get; set; }
+  }
+}
