d4627b5e468e0e634663a9258b6615357a118d3b
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 12:02:34 +0530

    Update ContractResolverTests with Message Data

--- test/Telegram.Bots.Json.Tests.Units/ContractResolverTests.cs
+++ test/Telegram.Bots.Json.Tests.Units/ContractResolverTests.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections.Generic;
 using Telegram.Bots.Types;
 using Telegram.Bots.Types.Games;
+using Telegram.Bots.Types.Passport;
+using Telegram.Bots.Types.Payments;
+using Telegram.Bots.Types.Stickers;
 using Xunit;
 
 namespace Telegram.Bots.Json.Tests.Units
 {
   public sealed class ContractResolverTests : IClassFixture<Serializer>
   {
+    private static readonly Uri Uri = new Uri("https://example.com");
+
     private readonly Serializer _serializer;
 
     public ContractResolverTests(Serializer serializer) => _serializer = serializer;
@@ -35,6 +40,39 @@ namespace Telegram.Bots.Json.Tests.Units
       (@"""callback_game"":{", new CallbackGameButton("")),
       (@"""photo"":[", new Game { PhotoSet = new List<Photo>() }),
       (@"""message_id"":1", new TextMessage { Id = 1 }),
+      (@"""animation"":{", new AnimationMessage { Animation = new Animation() }),
+      (@"""venue"":{", new VenueMessage { Venue = new Venue() }),
+      (@"""text"":""Test""", new TextMessage { Text = "Test" }),
+      (@"""audio"":{", new AudioMessage { Audio = new Audio() }),
+      (@"""document"":{", new DocumentMessage { Document = new Document() }),
+      (@"""photo"":[", new PhotoMessage { PhotoSet = new List<Photo>() }),
+      (@"""sticker"":{", new StickerMessage { Sticker = new Sticker() }),
+      (@"""video"":{", new VideoMessage { Video = new Video() }),
+      (@"""video_note"":{", new VideoNoteMessage { VideoNote = new VideoNote() }),
+      (@"""voice"":{", new VoiceMessage { Voice = new Voice() }),
+      (@"""contact"":{", new ContactMessage { Contact = new Contact() }),
+      (@"""dice"":{", new DiceMessage { Dice = new Dice() }),
+      (@"""game"":{", new GameMessage { Game = new Game() }),
+      (@"""poll"":{", new PollMessage { Poll = new RegularPoll() }),
+      (@"""location"":{", new LocationMessage { Location = new Location() }),
+      (@"""new_chat_members"":[", new NewChatMembersMessage { Members = new List<User>() }),
+      (@"""left_chat_member"":{", new LeftChatMemberMessage { Member = new User() }),
+      (@"""new_chat_title"":""""", new NewChatTitleMessage { Title = "" }),
+      (@"""new_chat_photo"":[", new NewChatPhotoMessage { PhotoSet = new List<Photo>() }),
+      (@"""delete_chat_photo"":true", new DeleteChatPhotoMessage { Deleted = true }),
+      (@"""group_chat_created"":true", new GroupChatCreatedMessage { Created = true }),
+      (@"""supergroup_chat_created"":true", new SupergroupChatCreatedMessage { Created = true }),
+      (@"""channel_chat_created"":true", new ChannelChatCreatedMessage { Created = true }),
+      (@"""migrate_to_chat_id"":1", new MigrateToChatMessage { ChatId = 1 }),
+      (@"""migrate_from_chat_id"":1", new MigrateFromChatMessage { ChatId = 1 }),
+      (@"""pinned_message"":{", new PinnedMessage { Pinned = new TextMessage() }),
+      (@"""invoice"":{", new InvoiceMessage { Invoice = new Invoice() }),
+      (@"""successful_payment"":{",
+        new SuccessfulPaymentMessage { Payment = new SuccessfulPayment() }),
+      (@"""connected_website"":""", new ConnectedWebsiteMessage { Website = Uri }),
+      (@"""passport_data"":{", new PassportDataMessage { PassportData = new PassportData() }),
+      (@"""reply_markup"":{",
+        new ReplyMarkupMessage { ReplyMarkup = new InlineKeyboardMarkup(null!) }),
       (@"""photos"":[", new UserProfilePhotos { PhotoSets = new List<Photo[]>() })
     };
 
