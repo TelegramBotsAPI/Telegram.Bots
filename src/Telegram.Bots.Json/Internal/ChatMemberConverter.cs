dfe896e99a2603759a7bdda78e20b3cb551d9dfc
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 06:22:52 +0530

    Add ChatMemberConverter

---
+++ src/Telegram.Bots.Json/Internal/ChatMemberConverter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Json.Internal
+{
+  using static ChatMemberStatus;
+
+  internal sealed class ChatMemberConverter : JsonConverter
+  {
+    public override bool CanWrite => false;
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
+      throw new NotImplementedException();
+
+    public override object? ReadJson(
+      JsonReader reader,
+      Type objectType,
+      object? existingValue,
+      JsonSerializer serializer)
+    {
+      var data = JObject.Load(reader);
+
+      if (data.Type == JTokenType.Null) return null;
+
+      return data.Properties().Single(property => property.Name == Status).Value.ToString() switch
+      {
+        Creator => Get<Creator>(),
+        Administrator => Get<Administrator>(),
+        Member => Get<NormalMember>(),
+        Restricted => Get<RestrictedMember>(),
+        Left => Get<LeftMember>(),
+        Kicked => Get<KickedMember>(),
+        _ => null
+      };
+
+      T Get<T>() => data.ToObject<T>(serializer)!;
+    }
+
+    public override bool CanConvert(Type objectType) => objectType == typeof(ChatMember);
+  }
+
+  internal static class ChatMemberStatus
+  {
+    public const string Status = "status";
+    public const string Creator = "creator";
+    public const string Administrator = "administrator";
+    public const string Member = "member";
+    public const string Restricted = "restricted";
+    public const string Left = "left";
+    public const string Kicked = "kicked";
+  }
+}
