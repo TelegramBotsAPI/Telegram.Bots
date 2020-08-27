d6aef2976df652fa7cee55ed9500d757b3c15f11
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 03:39:59 +0530

    Add SendMediaGroup

---
+++ src/Telegram.Bots.Requests/SendMediaGroup.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  using IGroupableMediaList = IEnumerable<IGroupableMedia>;
+
+  public abstract class SendMediaGroup<TChatId> : IRequest<IReadOnlyList<InputMediaGroupMessage>>,
+    IChatTargetable<TChatId>, INotifiable, IReplyable, IUploadable
+  {
+    public TChatId ChatId { get; }
+
+    public IGroupableMediaList Media { get; }
+
+    public bool? DisableNotification { get; set; }
+
+    public int? ReplyToMessageId { get; set; }
+
+    public string Method { get; } = "sendMediaGroup";
+
+    protected SendMediaGroup(TChatId chatId, IEnumerable<IGroupableMedia> media)
+    {
+      ChatId = chatId;
+      Media = media;
+    }
+
+    public IEnumerable<InputFile?> GetFiles() =>
+      Media.OfType<IUploadableMedia>().SelectMany(media => media.GetFiles());
+  }
+
+  public sealed class SendMediaGroup : SendMediaGroup<long>
+  {
+    public SendMediaGroup(long chatId, IGroupableMediaList media) : base(chatId, media) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendMediaGroup : SendMediaGroup<string>
+    {
+      public SendMediaGroup(string username, IGroupableMediaList media) : base(username, media) { }
+    }
+  }
+}
