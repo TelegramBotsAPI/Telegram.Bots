6c0bc9d4bb37c2ec0e7a1a4b248499b18181908a
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 20:08:04 +0530

    Add BotConfig

---
+++ src/Telegram.Bots/Configs/BotConfig.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Telegram.Bots.Configs
+{
+  public sealed class BotConfig : IBotConfig
+  {
+    public Uri BaseAddress { get; set; } = new Uri("https://api.telegram.org/");
+
+    public string Token { get; set; } = null!;
+
+    public int Timeout { get; set; } = 90;
+
+    public int HandlerLifetime { get; set; } = 600;
+
+    public IEnumerable<int> WaitsBeforeRetry { get; set; } = new[] {1, 2, 5};
+
+    public int EventsAllowedBeforeBreaking { get; set; } = 3;
+
+    public int BreakDuration { get; set; } = 30;
+  }
+}
