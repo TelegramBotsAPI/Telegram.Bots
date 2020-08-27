2f6ad5de9d92b62797830546881b468a098c8d07
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 06:57:57 +0530

    Add PhotoMedia

---
+++ src/Telegram.Bots.Types/PhotoMedia.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Telegram.Bots.Types
+{
+  public abstract class PhotoMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
+  {
+    public override InputMediaType Type { get; } = InputMediaType.Photo;
+
+    protected PhotoMedia(TMedia media) : base(media) { }
+  }
+
+  public sealed class CachedPhoto : PhotoMedia<string>
+  {
+    public CachedPhoto(string fileId) : base(fileId) { }
+  }
+
+  public sealed class PhotoUrl : PhotoMedia<Uri>
+  {
+    public PhotoUrl(Uri url) : base(url) { }
+  }
+
+  public sealed class PhotoFile : PhotoMedia<InputFile>, IUploadableMedia
+  {
+    public PhotoFile(InputFile file) : base(file) { }
+
+    public IEnumerable<InputFile?> GetFiles() => new[] { Media };
+  }
+}
