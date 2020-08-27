0082bd59c802d11c49207ba6810c3a1c6aecc36e
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-27 06:30:14 +0530

    Add PollConverter

---
+++ src/Telegram.Bots.Json/Internal/PollConverter.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Json.Internal
+{
+  using static PollSchema;
+
+  internal sealed class PollConverter : JsonConverter
+  {
+    public override bool CanWrite => false;
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
+      throw new NotImplementedException();
+
+    public override object? ReadJson(
+      JsonReader reader,
+      Type objectType,
+      object? existingValue,
+      JsonSerializer serializer)
+    {
+      var data = JObject.Load(reader);
+
+      if (data.Type == JTokenType.Null) return null;
+
+      return data.Properties().Single(property => property.Name == Type).Value.ToString() switch
+      {
+        Regular => Get<RegularPoll>(),
+        Quiz => Get<QuizPoll>(),
+        _ => null
+      };
+
+      T Get<T>() => data.ToObject<T>(serializer)!;
+    }
+
+    public override bool CanConvert(Type objectType) => objectType == typeof(Poll);
+  }
+
+  internal static class PollSchema
+  {
+    public const string Type = "type";
+    public const string Regular = "regular";
+    public const string Quiz = "quiz";
+  }
+}
