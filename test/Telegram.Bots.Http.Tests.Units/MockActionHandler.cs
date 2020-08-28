bb7519f6f7909de29913ee3460444edc0475bec8
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 23:15:58 +0530

    Add MockActionHandler

---
+++ test/Telegram.Bots.Http.Tests.Units/MockActionHandler.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Telegram.Bots.Http.Tests.Units
+{
+  using HttpAction = Func<HttpRequestMessage, HttpResponseMessage>;
+
+  public class MockActionHandler : DelegatingHandler
+  {
+    private readonly HttpAction _action;
+
+    public MockActionHandler(HttpAction action) => _action = action;
+
+    protected override Task<HttpResponseMessage> SendAsync(
+      HttpRequestMessage request,
+      CancellationToken cancellationToken) => Task.FromResult(_action(request));
+  }
+}
