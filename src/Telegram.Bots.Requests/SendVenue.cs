bee8d98f163dd4a1d00e9ee19dd0663856934f5a
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 06:51:03 +0530

    Add SendVenue

---
+++ src/Telegram.Bots.Requests/SendVenue.cs
@@ -0,0 +1,63 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class SendVenue<TChatId> : IRequest<LocationMessage>,
+    IChatTargetable<TChatId>, INotifiable, IReplyable, IMarkupable
+  {
+    public TChatId ChatId { get; }
+
+    public double Latitude { get; }
+
+    public double Longitude { get; }
+
+    public string Title { get; }
+
+    public string Address { get; }
+
+    public string? FoursquareId { get; set; }
+
+    public string? FoursquareType { get; set; }
+
+    public bool? DisableNotification { get; set; }
+
+    public int? ReplyToMessageId { get; set; }
+
+    public ReplyMarkup? ReplyMarkup { get; set; }
+
+    public string Method { get; } = "sendVenue";
+
+    protected SendVenue(
+      TChatId chatId,
+      double latitude,
+      double longitude,
+      string title,
+      string address)
+    {
+      ChatId = chatId;
+      Latitude = latitude;
+      Longitude = longitude;
+      Title = title;
+      Address = address;
+    }
+  }
+
+  public sealed class SendVenue : SendVenue<long>
+  {
+    public SendVenue(long chatId, double latitude, double longitude, string title, string address) :
+      base(chatId, latitude, longitude, title, address) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendVenue : SendVenue<string>
+    {
+      public SendVenue(
+        string chatId,
+        double latitude,
+        double longitude,
+        string title,
+        string address) : base(chatId, latitude, longitude, title, address) { }
+    }
+  }
+}
