9573478ec2219c11427a9ebcf0033df73aa3c371
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 06:54:53 +0530

    Add SendContact

---
+++ src/Telegram.Bots.Requests/SendContact.cs
@@ -0,0 +1,48 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class SendContact<TChatId> : IRequest<LocationMessage>,
+    IChatTargetable<TChatId>, INotifiable, IReplyable, IMarkupable
+  {
+    public TChatId ChatId { get; }
+
+    public string PhoneNumber { get; }
+
+    public string FirstName { get; }
+
+    public string? LastName { get; set; }
+
+    public string? Vcard { get; set; }
+
+    public bool? DisableNotification { get; set; }
+
+    public int? ReplyToMessageId { get; set; }
+
+    public ReplyMarkup? ReplyMarkup { get; set; }
+
+    public string Method { get; } = "sendContact";
+
+    protected SendContact(TChatId chatId, string phoneNumber, string firstName)
+    {
+      ChatId = chatId;
+      PhoneNumber = phoneNumber;
+      FirstName = firstName;
+    }
+  }
+
+  public sealed class SendContact : SendContact<long>
+  {
+    public SendContact(long chatId, string phoneNumber, string firstName) :
+      base(chatId, phoneNumber, firstName) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendContact : SendContact<string>
+    {
+      public SendContact(string username, string phoneNumber, string firstName) :
+        base(username, phoneNumber, firstName) { }
+    }
+  }
+}
