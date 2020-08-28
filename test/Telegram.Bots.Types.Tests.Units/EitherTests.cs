3bf9eda1bc356c40379925e02dd236f02344b06a
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 16:46:40 +0530

    Add EitherTests

---
+++ test/Telegram.Bots.Types.Tests.Units/EitherTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+namespace Telegram.Bots.Types.Tests.Units
+{
+  public sealed class EitherTests
+  {
+    [Fact(DisplayName = "Left Either has IsLeft as True and Right Value as Default")]
+    public void LeftEitherHasIsLeftAsTrueAndRightValueAsDefault()
+    {
+      Either<bool, int> either = false;
+
+      Assert.True(either.IsLeft);
+      Assert.False(either.Left);
+      Assert.Equal(default, either.Right);
+    }
+
+    [Fact(DisplayName = "Right Either has IsLeft as False and Left Value as Default")]
+    public void RightEitherHasIsLeftAsFalseAndLeftValueAsDefault()
+    {
+      Either<bool, int> either = 4;
+
+      Assert.False(either.IsLeft);
+      Assert.Equal(default, either.Left);
+      Assert.Equal(4, either.Right);
+    }
+  }
+}
