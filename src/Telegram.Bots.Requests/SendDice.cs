a34a9815485d1961f900ceb3a827a34f13a7e725
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-28 11:48:33 +0530

    Add SendDice

---
+++ src/Telegram.Bots.Requests/SendDice.cs
@@ -0,0 +1,39 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class SendDice<TChatId> : IRequest<DiceMessage>,
+    IChatTargetable<TChatId>, INotifiable, IReplyable, IMarkupable
+  {
+    public TChatId ChatId { get; }
+
+    public Emoji Emoji { get; }
+
+    public bool? DisableNotification { get; set; }
+
+    public int? ReplyToMessageId { get; set; }
+
+    public ReplyMarkup? ReplyMarkup { get; set; }
+
+    public string Method { get; } = "sendDice";
+
+    protected SendDice(TChatId chatId, Emoji emoji = Emoji.Dice)
+    {
+      ChatId = chatId;
+      Emoji = emoji;
+    }
+  }
+
+  public sealed class SendDice : SendDice<long>
+  {
+    public SendDice(long chatId, Emoji emoji = Emoji.Dice) : base(chatId, emoji) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendDice : SendDice<string>
+    {
+      public SendDice(string username, Emoji emoji = Emoji.Dice) : base(username, emoji) { }
+    }
+  }
+}
