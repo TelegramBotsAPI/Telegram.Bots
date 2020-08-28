ef944873b72de95f4a23e8efa62891c1de237191
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 03:44:31 +0530

    Make ButtonPollType a Read-Only Struct

--- src/Telegram.Bots.Types/ButtonPollType.cs
+++ src/Telegram.Bots.Types/ButtonPollType.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Telegram.Bots.Types
 {
-  public sealed class ButtonPollType
+  public readonly struct ButtonPollType : IEquatable<ButtonPollType>
   {
     public static readonly ButtonPollType AnyPoll = new ButtonPollType(null);
 
@@ -8,8 +10,18 @@ namespace Telegram.Bots.Types
 
     public ButtonPollType(PollType? type) => Type = type;
 
+    public bool Equals(ButtonPollType other) => Type == other.Type;
+
+    public override bool Equals(object? obj) => obj is ButtonPollType other && Equals(other);
+
+    public override int GetHashCode() => Type.GetHashCode();
+
     public static implicit operator ButtonPollType(PollType type) => ToButtonPollType(type);
 
     public static ButtonPollType ToButtonPollType(PollType type) => new ButtonPollType(type);
+
+    public static bool operator ==(ButtonPollType left, ButtonPollType right) => left.Equals(right);
+
+    public static bool operator !=(ButtonPollType left, ButtonPollType right) => !(left == right);
   }
 }
