5909d6fd34f794e8be771ed633556303d2b30bd3
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 04:13:10 +0530

    Add SendChatAction

---
+++ src/Telegram.Bots.Requests/SendChatAction.cs
@@ -0,0 +1,32 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class SendChatAction<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public ChatAction Action { get; }
+
+    public string Method { get; } = "sendChatAction";
+
+    protected SendChatAction(TChatId chatId, ChatAction action)
+    {
+      ChatId = chatId;
+      Action = action;
+    }
+  }
+
+  public sealed class SendChatAction : SendChatAction<long>
+  {
+    public SendChatAction(long chatId, ChatAction action) : base(chatId, action) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SendChatAction : SendChatAction<string>
+    {
+      public SendChatAction(string username, ChatAction action) : base(username, action) { }
+    }
+  }
+}
