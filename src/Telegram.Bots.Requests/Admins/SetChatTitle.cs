d159f6a9680ffbc28fc499c19d1f5c1c29030875
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 15:00:25 +0530

    Add SetChatTitle

---
+++ src/Telegram.Bots.Requests/Admins/SetChatTitle.cs
@@ -0,0 +1,30 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class SetChatTitle<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Title { get; }
+
+    public string Method { get; } = "setChatTitle";
+
+    protected SetChatTitle(TChatId chatId, string title)
+    {
+      ChatId = chatId;
+      Title = title;
+    }
+  }
+
+  public sealed class SetChatTitle : SetChatTitle<long>
+  {
+    public SetChatTitle(long chatId, string title) : base(chatId, title) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SetChatTitle : SetChatTitle<string>
+    {
+      public SetChatTitle(string username, string title) : base(username, title) { }
+    }
+  }
+}
