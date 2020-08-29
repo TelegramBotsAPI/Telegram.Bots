f9ed4f85815296b7bd7c1fdb7d0502cdcb80f622
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 14:49:14 +0530

    Add SetChatPhoto

---
+++ src/Telegram.Bots.Requests/Admins/SetChatPhoto.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class SetChatPhoto<TChatId> : IRequest<bool>, IChatTargetable<TChatId>,
+    IUploadable
+  {
+    public TChatId ChatId { get; }
+
+    public InputFile Photo { get; }
+
+    public string Method { get; } = "setChatPhoto";
+
+    protected SetChatPhoto(TChatId chatId, InputFile photo)
+    {
+      ChatId = chatId;
+      Photo = photo;
+    }
+
+    public IEnumerable<InputFile?> GetFiles() => new[] { Photo };
+  }
+
+  public sealed class SetChatPhoto : SetChatPhoto<long>
+  {
+    public SetChatPhoto(long chatId, InputFile photo) : base(chatId, photo) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SetChatPhoto : SetChatPhoto<string>
+    {
+      public SetChatPhoto(string username, InputFile photo) : base(username, photo) { }
+    }
+  }
+}
