55c5824d8845d98b6c89afb5b8320c4292381f6f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 02:20:40 +0530

    Add AnswerInlineQuery

---
+++ src/Telegram.Bots.Requests/Inline/AnswerInlineQuery.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Telegram.Bots.Types.Inline;
+
+namespace Telegram.Bots.Requests.Inline
+{
+  public sealed class AnswerInlineQuery : IRequest<bool>
+  {
+    public string QueryId { get; }
+
+    public IEnumerable<InlineResult> Results { get; }
+
+    public int? CacheTime { get; set; }
+
+    public bool? IsPersonal { get; set; }
+
+    public string? NextOffset { get; set; }
+
+    public string? SwitchPmText { get; set; }
+
+    public string? SwitchPmParameter { get; set; }
+
+    public string Method { get; } = "answerInlineQuery";
+
+    public AnswerInlineQuery(string queryId, IEnumerable<InlineResult> results)
+    {
+      QueryId = queryId;
+      Results = results;
+    }
+  }
+}
