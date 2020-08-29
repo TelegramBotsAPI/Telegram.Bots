7820e1ea8525045ee786804adb853a313899dfca
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 16:11:39 +0530

    Add DeleteChatStickerSet

---
+++ src/Telegram.Bots.Requests/Admins/DeleteChatStickerSet.cs
@@ -0,0 +1,24 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class DeleteChatStickerSet<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Method { get; } = "deleteChatStickerSet";
+
+    protected DeleteChatStickerSet(TChatId chatId) => ChatId = chatId;
+  }
+
+  public sealed class DeleteChatStickerSet : DeleteChatStickerSet<long>
+  {
+    public DeleteChatStickerSet(long chatId) : base(chatId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class DeleteChatStickerSet : DeleteChatStickerSet<string>
+    {
+      public DeleteChatStickerSet(string username) : base(username) { }
+    }
+  }
+}
