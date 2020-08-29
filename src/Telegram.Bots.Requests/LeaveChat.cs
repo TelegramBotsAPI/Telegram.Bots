098cd63882045064ade74327f1462cea026df680
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 15:15:03 +0530

    Add LeaveChat

---
+++ src/Telegram.Bots.Requests/LeaveChat.cs
@@ -0,0 +1,24 @@
+namespace Telegram.Bots.Requests
+{
+  public abstract class LeaveChat<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Method { get; } = "leaveChat";
+
+    protected LeaveChat(TChatId chatId) => ChatId = chatId;
+  }
+
+  public sealed class LeaveChat : LeaveChat<long>
+  {
+    public LeaveChat(long chatId) : base(chatId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class LeaveChat : LeaveChat<string>
+    {
+      public LeaveChat(string username) : base(username) { }
+    }
+  }
+}
