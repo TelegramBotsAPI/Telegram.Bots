456ec9c86033f99cbe9bbc18a43bc51683cc014f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 14:46:55 +0530

    Add SetChatPermissions

---
+++ src/Telegram.Bots.Requests/Admins/SetChatPermissions.cs
@@ -0,0 +1,34 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class SetChatPermissions<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public ChatPermissions Permissions { get; }
+
+    public string Method { get; } = "SetChatPermissions";
+
+    protected SetChatPermissions(TChatId chatId, ChatPermissions permissions)
+    {
+      ChatId = chatId;
+      Permissions = permissions;
+    }
+  }
+
+  public sealed class SetChatPermissions : SetChatPermissions<long>
+  {
+    public SetChatPermissions(long chatId, ChatPermissions permissions) :
+      base(chatId, permissions) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SetChatPermissions : SetChatPermissions<string>
+    {
+      public SetChatPermissions(string username, ChatPermissions permissions) :
+        base(username, permissions) { }
+    }
+  }
+}
