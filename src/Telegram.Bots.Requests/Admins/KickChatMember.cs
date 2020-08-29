cb3d85566dc15ee0ce6b334d2581c339134bea71
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 14:40:14 +0530

    Add KickChatMember

---
+++ src/Telegram.Bots.Requests/Admins/KickChatMember.cs
@@ -0,0 +1,32 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class KickChatMember<TChatId> : IRequest<bool>, IChatMemberTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public int UserId { get; }
+
+    public long? UntilDate { get; set; }
+
+    public string Method { get; } = "kickChatMember";
+
+    protected KickChatMember(TChatId chatId, int userId)
+    {
+      ChatId = chatId;
+      UserId = userId;
+    }
+  }
+
+  public sealed class KickChatMember : KickChatMember<long>
+  {
+    public KickChatMember(long chatId, int userId) : base(chatId, userId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class KickChatMember : KickChatMember<string>
+    {
+      public KickChatMember(string username, int userId) : base(username, userId) { }
+    }
+  }
+}
