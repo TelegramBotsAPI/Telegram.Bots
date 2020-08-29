e52701cf3e1ae58d33396bfc2ba74a63deeb4937
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 14:53:43 +0530

    Add DeleteChatPhoto

---
+++ src/Telegram.Bots.Requests/Admins/DeleteChatPhoto.cs
@@ -0,0 +1,24 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class DeleteChatPhoto<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Method { get; } = "deleteChatPhoto";
+
+    protected DeleteChatPhoto(TChatId chatId) => ChatId = chatId;
+  }
+
+  public sealed class DeleteChatPhoto : DeleteChatPhoto<long>
+  {
+    public DeleteChatPhoto(long chatId) : base(chatId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class DeleteChatPhoto : DeleteChatPhoto<string>
+    {
+      public DeleteChatPhoto(string username) : base(username) { }
+    }
+  }
+}
