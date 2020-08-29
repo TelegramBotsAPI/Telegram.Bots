172d631fb7c8058b2f19fffff8f3220ee21e2bfd
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 15:58:44 +0530

    Add GetChatAdmins

---
+++ src/Telegram.Bots.Requests/GetChatAdmins.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests
+{
+  public abstract class GetChatAdmins<TChatId> : IRequest<IReadOnlyList<PrivilegedMember>>,
+    IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Method { get; } = "getChatAdministrators";
+
+    protected GetChatAdmins(TChatId chatId) => ChatId = chatId;
+  }
+
+  public sealed class GetChatAdmins : GetChatAdmins<long>
+  {
+    public GetChatAdmins(long chatId) : base(chatId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class GetChatAdmins : GetChatAdmins<string>
+    {
+      public GetChatAdmins(string username) : base(username) { }
+    }
+  }
+}
