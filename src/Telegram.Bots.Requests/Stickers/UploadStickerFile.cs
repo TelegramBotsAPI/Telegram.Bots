07eaa2aaa19dd9c044c8801b3da9b14052b4021f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 02:26:52 +0530

    Rename Refactor UploadStickerFile

--- src/Telegram.Bots.Requests/Stickers/UploadStickerFile.cs
+++ src/Telegram.Bots.Requests/Stickers/UploadStickerFile.cs
@@ -3,7 +3,7 @@ using Telegram.Bots.Types;
 
 namespace Telegram.Bots.Requests.Stickers
 {
-  public sealed class UploadPngStickerFile : IRequest<FileInfo>, IUserTargetable, IUploadable
+  public sealed class UploadStickerFile : IRequest<FileInfo>, IUserTargetable, IUploadable
   {
     public int UserId { get; }
 
@@ -11,7 +11,7 @@ namespace Telegram.Bots.Requests.Stickers
 
     public string Method { get; } = "uploadStickerFile";
 
-    public UploadPngStickerFile(int userId, InputFile sticker)
+    public UploadStickerFile(int userId, InputFile sticker)
     {
       UserId = userId;
       Sticker = sticker;
