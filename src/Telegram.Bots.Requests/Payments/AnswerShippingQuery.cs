9c9bd0a8ef8bbab7a874851f13a01a376ce810ce
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 01:29:01 +0530

    Add AnswerShippingQuery

---
+++ src/Telegram.Bots.Requests/Payments/AnswerShippingQuery.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Telegram.Bots.Types.Payments;
+
+namespace Telegram.Bots.Requests.Payments
+{
+  public sealed class AnswerShippingQuery : IRequest<bool>
+  {
+    public string QueryId { get; }
+
+    public bool Ok { get; }
+
+    public IEnumerable<ShippingOption>? Options { get; }
+
+    public string? ErrorMessage { get; }
+
+    public string Method { get; } = "answerShippingQuery";
+
+    public AnswerShippingQuery(string queryId, IEnumerable<ShippingOption>? options)
+    {
+      QueryId = queryId;
+      Options = options;
+      Ok = true;
+    }
+
+    public AnswerShippingQuery(string queryId, string? errorMessage)
+    {
+      QueryId = queryId;
+      ErrorMessage = errorMessage;
+    }
+  }
+}
