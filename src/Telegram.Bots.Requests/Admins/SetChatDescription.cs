20f4751066e422be2715360f6ed50335dc0b1711
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 15:05:24 +0530

    Add SetChatDescription

---
+++ src/Telegram.Bots.Requests/Admins/SetChatDescription.cs
@@ -0,0 +1,31 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class SetChatDescription<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Description { get; }
+
+    public string Method { get; } = "setChatDescription";
+
+    protected SetChatDescription(TChatId chatId, string description)
+    {
+      ChatId = chatId;
+      Description = description;
+    }
+  }
+
+  public sealed class SetChatDescription : SetChatDescription<long>
+  {
+    public SetChatDescription(long chatId, string description) : base(chatId, description) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SetChatDescription : SetChatDescription<string>
+    {
+      public SetChatDescription(string username, string description) :
+        base(username, description) { }
+    }
+  }
+}
