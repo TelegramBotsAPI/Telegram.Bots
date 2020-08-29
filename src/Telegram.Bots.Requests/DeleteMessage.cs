b6bff389078f4a4f23d58a04f2289c6c99aa7f1d
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 19:54:28 +0530

    Add DeleteMessage

---
+++ src/Telegram.Bots.Requests/DeleteMessage.cs
@@ -0,0 +1,30 @@
+namespace Telegram.Bots.Requests
+{
+  public abstract class DeleteMessage<TChatId> : IRequest<bool>, IChatMessageTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public int MessageId { get; }
+
+    public string Method { get; } = "deleteMessage";
+
+    protected DeleteMessage(TChatId chatId, int messageId)
+    {
+      ChatId = chatId;
+      MessageId = messageId;
+    }
+  }
+
+  public sealed class DeleteMessage : DeleteMessage<long>
+  {
+    public DeleteMessage(long chatId, int messageId) : base(chatId, messageId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class DeleteMessage : DeleteMessage<string>
+    {
+      public DeleteMessage(string username, int messageId) : base(username, messageId) { }
+    }
+  }
+}
