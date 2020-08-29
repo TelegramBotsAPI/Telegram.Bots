01084f5b79cf5b2cbedea2cb09d20fccf2adf9ab
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 16:09:16 +0530

    Add SetChatStickerSet

---
+++ src/Telegram.Bots.Requests/Admins/SetChatStickerSet.cs
@@ -0,0 +1,31 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class SetChatStickerSet<TChatId> : IRequest<bool>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string StickerSetName { get; }
+
+    public string Method { get; } = "setChatStickerSet";
+
+    protected SetChatStickerSet(TChatId chatId, string stickerSetName)
+    {
+      ChatId = chatId;
+      StickerSetName = stickerSetName;
+    }
+  }
+
+  public sealed class SetChatStickerSet : SetChatStickerSet<long>
+  {
+    public SetChatStickerSet(long chatId, string stickerSetName) : base(chatId, stickerSetName) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class SetChatStickerSet : SetChatStickerSet<string>
+    {
+      public SetChatStickerSet(string username, string stickerSetName) :
+        base(username, stickerSetName) { }
+    }
+  }
+}
