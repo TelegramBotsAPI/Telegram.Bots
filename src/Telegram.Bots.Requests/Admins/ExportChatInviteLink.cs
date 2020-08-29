8609752e5f336d5331740a406d4406243b5930a2
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-29 14:47:00 +0530

    Add ExportChatInviteLink

---
+++ src/Telegram.Bots.Requests/Admins/ExportChatInviteLink.cs
@@ -0,0 +1,24 @@
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract class ExportChatInviteLink<TChatId> : IRequest<string>, IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string Method { get; } = "exportChatInviteLink";
+
+    protected ExportChatInviteLink(TChatId chatId) => ChatId = chatId;
+  }
+
+  public sealed class ExportChatInviteLink : ExportChatInviteLink<long>
+  {
+    public ExportChatInviteLink(long chatId) : base(chatId) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed class ExportChatInviteLink : ExportChatInviteLink<string>
+    {
+      public ExportChatInviteLink(string username) : base(username) { }
+    }
+  }
+}
