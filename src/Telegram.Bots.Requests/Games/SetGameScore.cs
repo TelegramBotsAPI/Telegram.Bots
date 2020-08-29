8c098a16fb86ab36c3a3744092e308defe7e39e4
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 01:51:58 +0530

    Add SetGameScore

---
+++ src/Telegram.Bots.Requests/Games/SetGameScore.cs
@@ -0,0 +1,56 @@
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests.Games
+{
+  using Result = Either<bool, GameMessage>;
+
+  public abstract class SetGameScoreBase : IRequest<Result>, IUserTargetable
+  {
+    public int UserId { get; }
+
+    public uint Score { get; }
+
+    public bool? Force { get; set; }
+
+    public bool? DisableEdit { get; set; }
+
+    public string Method { get; } = "setGameScore";
+
+    protected SetGameScoreBase(int userId, uint score)
+    {
+      UserId = userId;
+      Score = score;
+    }
+  }
+
+  public abstract class SetGameScore<TChatId> : SetGameScoreBase, IChatMessageTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public int MessageId { get; }
+
+    protected SetGameScore(TChatId chatId, int messageId, int userId, uint score) :
+      base(userId, score)
+    {
+      ChatId = chatId;
+      MessageId = messageId;
+    }
+  }
+
+  public sealed class SetGameScore : SetGameScore<long>
+  {
+    public SetGameScore(long chatId, int messageId, int userId, uint score) :
+      base(chatId, messageId, userId, score) { }
+  }
+
+  namespace Inline
+  {
+    public sealed class SetGameScore : SetGameScoreBase, IInlineMessageTargetable
+    {
+      public string MessageId { get; }
+
+      public SetGameScore(string messageId, int userId, uint score) : base(userId, score) =>
+        MessageId = messageId;
+    }
+  }
+}
