06932561e051171a8ba8d1b1b67b5b50f0e37f43
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 23:04:10 +0530

    Add InlineVenue

---
+++ src/Telegram.Bots.Types/Inline/InlineVenue.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Telegram.Bots.Types.Inline
+{
+  public sealed class InlineVenue : ReplaceableResult, IThumbable
+  {
+    public override ResultType Type { get; } = ResultType.Venue;
+
+    public string Title { get; }
+
+    public string Address { get; }
+
+    public double Latitude { get; }
+
+    public double Longitude { get; }
+
+    public string? FoursquareId { get; set; }
+
+    public string? FoursquareType { get; set; }
+
+    public Uri? Thumb { get; set; }
+
+    public int? ThumbWidth { get; set; }
+
+    public int? ThumbHeight { get; set; }
+
+    public InlineVenue(string id, string title, string address, double latitude, double longitude) :
+      base(id)
+    {
+      Title = title;
+      Address = address;
+      Latitude = latitude;
+      Longitude = longitude;
+    }
+  }
+}
