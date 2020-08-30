0400a79bc10d9ba6480f73ddb9c6918ff25c6010
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 23:01:56 +0530

    Add InlineContact

---
+++ src/Telegram.Bots.Types/Inline/InlineContact.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Telegram.Bots.Types.Inline
+{
+  public sealed class InlineContact : ReplaceableResult, IThumbable
+  {
+    public override ResultType Type { get; } = ResultType.Contact;
+
+    public string PhoneNumber { get; }
+
+    public string FirstName { get; }
+
+    public string? LastName { get; set; }
+
+    public string? Vcard { get; set; }
+
+    public Uri? Thumb { get; set; }
+
+    public int? ThumbWidth { get; set; }
+
+    public int? ThumbHeight { get; set; }
+
+    public InlineContact(string id, string phoneNumber, string firstName) : base(id)
+    {
+      PhoneNumber = phoneNumber;
+      FirstName = firstName;
+    }
+  }
+}
