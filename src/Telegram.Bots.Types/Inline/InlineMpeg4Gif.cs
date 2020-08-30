14c7223bee40aff557d26170842569c1c4df3c6f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 23:03:42 +0530

    Add InlineMpeg4Gif

---
+++ src/Telegram.Bots.Types/Inline/InlineMpeg4Gif.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Telegram.Bots.Types.Inline
+{
+  public abstract class InlineMpeg4Gif<TMpeg4> : ReplaceableResult
+  {
+    public override ResultType Type { get; } = ResultType.Mpeg4Gif;
+
+    public TMpeg4 Mpeg4 { get; }
+
+    public string? Title { get; set; }
+
+    public string? Caption { get; set; }
+
+    public ParseMode? ParseMode { get; set; }
+
+    protected InlineMpeg4Gif(string id, TMpeg4 mpeg4) : base(id) => Mpeg4 = mpeg4;
+  }
+
+  public sealed class InlineMpeg4Gif : InlineMpeg4Gif<Uri>
+  {
+    public int? Width { get; set; }
+
+    public int? Height { get; set; }
+
+    public int? Duration { get; set; }
+
+    public Uri Thumb { get; }
+
+    public ThumbMimeType? ThumbMimeType { get; set; }
+
+    public InlineMpeg4Gif(string id, Uri mpeg4, Uri thumb) : base(id, mpeg4) => Thumb = thumb;
+  }
+
+  public sealed class InlineCachedMpeg4Gif : InlineMpeg4Gif<string>
+  {
+    public InlineCachedMpeg4Gif(string id, string mpeg4) : base(id, mpeg4) { }
+  }
+}
