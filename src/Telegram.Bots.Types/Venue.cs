bb4504a41433a7b6ea5783fd82160cd9800b5011
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 22:59:06 +0530

    Reformat Venue

--- src/Telegram.Bots.Types/Venue.cs
+++ src/Telegram.Bots.Types/Venue.cs
@@ -2,11 +2,11 @@ namespace Telegram.Bots.Types
 {
   public sealed class Venue
   {
-    public Location Location { get; set; } = null!;
-
     public string Title { get; set; } = null!;
 
     public string Address { get; set; } = null!;
+
+    public Location Location { get; set; } = null!;
 
     public string? FoursquareId { get; set; }
 
