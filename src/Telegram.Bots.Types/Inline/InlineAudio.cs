d84b819ff97174e87ed089b582d188a781a5346c
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 23:01:48 +0530

    Add InlineAudio

---
+++ src/Telegram.Bots.Types/Inline/InlineAudio.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Telegram.Bots.Types.Inline
+{
+  public abstract class InlineAudio<TAudio> : ReplaceableResult
+  {
+    public override ResultType Type { get; } = ResultType.Audio;
+
+    public TAudio Audio { get; }
+
+    public string? Caption { get; set; }
+
+    public ParseMode? ParseMode { get; set; }
+
+    protected InlineAudio(string id, TAudio audio) : base(id) => Audio = audio;
+  }
+
+  public sealed class InlineAudio : InlineAudio<Uri>
+  {
+    public string Title { get; }
+
+    public string? Performer { get; set; }
+
+    public int? Duration { get; set; }
+
+    public InlineAudio(string id, string title, Uri audio) : base(id, audio) => Title = title;
+  }
+
+  public sealed class InlineCachedAudio : InlineAudio<string>
+  {
+    public InlineCachedAudio(string id, string audio) : base(id, audio) { }
+  }
+}
