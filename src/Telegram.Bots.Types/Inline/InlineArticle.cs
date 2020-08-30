bb4f01b71e9f5a59db2abc8a363dcfa4e5164f01
Aman Agnihotri <aman.agnihotri@yahoo.com>
2020-08-30 23:01:38 +0530

    Add InlineArticle

---
+++ src/Telegram.Bots.Types/Inline/InlineArticle.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Telegram.Bots.Types.Inline
+{
+  public sealed class InlineArticle : ReplaceableResult, IThumbable
+  {
+    public override ResultType Type { get; } = ResultType.Article;
+
+    public string Title { get; }
+
+    public Uri? Url { get; set; }
+
+    public bool? HideUrl { get; set; }
+
+    public string? Description { get; set; }
+
+    public Uri? Thumb { get; set; }
+
+    public int? ThumbWidth { get; set; }
+
+    public int? ThumbHeight { get; set; }
+
+    public InlineArticle(string id, string title) : base(id) => Title = title;
+  }
+}
