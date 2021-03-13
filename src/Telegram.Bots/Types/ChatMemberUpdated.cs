33c031572b55f6be4cc77a3a7110e05ebb3c5a6f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-03-13 13:01:20 +0530

    Add ChatMemberUpdated

---
+++ src/Telegram.Bots/Types/ChatMemberUpdated.cs
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+using System;
+
+namespace Telegram.Bots.Types
+{
+  public sealed record ChatMemberUpdated
+  {
+    public Chat Chat { get; init; }
+
+    public User From { get; init; }
+
+    public DateTime Date { get; init; }
+
+    public ChatMember OldChatMember { get; init; }
+
+    public ChatMember NewChatMember { get; init; }
+
+    public ChatInviteLink? InviteLink { get; init; }
+  }
+}
