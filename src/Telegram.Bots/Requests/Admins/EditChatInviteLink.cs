28b58bf146c58391e1cf4bee3be17ffe0f1c666e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-03-13 15:04:30 +0530

    Add EditChatInviteLink

---
+++ src/Telegram.Bots/Requests/Admins/EditChatInviteLink.cs
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+using System;
+using Telegram.Bots.Types;
+
+namespace Telegram.Bots.Requests.Admins
+{
+  public abstract record EditChatInviteLink<TChatId> : IRequest<ChatInviteLink>,
+    IChatTargetable<TChatId>
+  {
+    public TChatId ChatId { get; }
+
+    public string InviteLink { get; }
+
+    public DateTime? ExpireDate { get; init; }
+
+    public int? MemberLimit { get; init; }
+
+    public string Method { get; } = "editChatInviteLink";
+
+    protected EditChatInviteLink(TChatId chatId, string inviteLink)
+    {
+      ChatId = chatId;
+      InviteLink = inviteLink;
+    }
+  }
+
+  public sealed record EditChatInviteLink : EditChatInviteLink<long>
+  {
+    public EditChatInviteLink(long chatId, string inviteLink) : base(chatId, inviteLink) { }
+  }
+
+  namespace Usernames
+  {
+    public sealed record EditChatInviteLink : EditChatInviteLink<string>
+    {
+      public EditChatInviteLink(string username, string inviteLink) : base(username, inviteLink) { }
+    }
+  }
+}
