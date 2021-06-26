39bac529e6a781ed0ebabe8da074b2a6ea20e0f7
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-06-27 00:04:25 +0530

    Add ChatType to InlineQuery

--- src/Telegram.Bots/Types/Inline/InlineQuery.cs
+++ src/Telegram.Bots/Types/Inline/InlineQuery.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2021 Aman Agnihotri
 
 namespace Telegram.Bots.Types.Inline
 {
@@ -14,5 +14,7 @@ namespace Telegram.Bots.Types.Inline
     public string Query { get; init; } = null!;
 
     public string Offset { get; init; } = null!;
+
+    public ChatType? ChatType { get; init; }
   }
 }
