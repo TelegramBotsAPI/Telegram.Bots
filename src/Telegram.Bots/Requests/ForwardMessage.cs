dc98974828df7099275220b75a13df307ef95f24
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-06 22:56:09 +0530

    Add ProtectContent to ForwardMessage

--- src/Telegram.Bots/Requests/ForwardMessage.cs
+++ src/Telegram.Bots/Requests/ForwardMessage.cs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 using Telegram.Bots.Types;
 
 namespace Telegram.Bots.Requests
 {
   public abstract record ForwardMessage<TChatId, TFromChatId> : IRequest<Message>,
-    IChatMessageTargetable<TChatId>, INotifiable
+    IChatMessageTargetable<TChatId>, INotifiable, IProtectable
   {
     public TChatId ChatId { get; }
 
@@ -15,6 +15,8 @@ namespace Telegram.Bots.Requests
     public int MessageId { get; }
 
     public bool? DisableNotification { get; init; }
+    
+    public bool? ProtectContent { get; init; }
 
     public string Method { get; } = "forwardMessage";
 
