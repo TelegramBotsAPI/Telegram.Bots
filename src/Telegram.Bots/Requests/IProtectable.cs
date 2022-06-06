b5c41f091430dac5b4def80921f95c481b593a9b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-06 21:24:14 +0530

    Add IProtectable

---
+++ src/Telegram.Bots/Requests/IProtectable.cs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2022 Aman Agnihotri
+
+namespace Telegram.Bots.Requests
+{
+  public interface IProtectable
+  {
+    bool? ProtectContent { get; init; }
+  }
+}
