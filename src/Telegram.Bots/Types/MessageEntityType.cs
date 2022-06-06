1e993c98605cf57dc5eaabf968512c1571324a39
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-06 21:16:59 +0530

    Add Spoiler to MessageEntityType

--- src/Telegram.Bots/Types/MessageEntityType.cs
+++ src/Telegram.Bots/Types/MessageEntityType.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Types
 {
@@ -16,6 +16,7 @@ namespace Telegram.Bots.Types
     PhoneNumber,
     Pre,
     Strikethrough,
+    Spoiler,
     TextLink,
     TextMention,
     Underline,
