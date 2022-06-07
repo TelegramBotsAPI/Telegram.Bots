194b3f308ea681d2094102560383ee47e48a6cfc
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-07 13:41:11 +0530

    Add LastSynchronizationErrorDate to WebhookInfo

--- src/Telegram.Bots/Types/Webhooks/WebhookInfo.cs
+++ src/Telegram.Bots/Types/Webhooks/WebhookInfo.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 using System;
 using System.Collections.Generic;
@@ -19,6 +19,8 @@ namespace Telegram.Bots.Types.Webhooks
     public DateTime? LastErrorDate { get; init; }
 
     public string? LastErrorMessage { get; init; }
+    
+    public DateTime? LastSynchronizationErrorDate { get; init; }
 
     public uint? MaxConnections { get; init; }
 
