a4ace5f474788bf8be3d5a1fae96ff9958ea6f21
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-07 11:49:52 +0530

    Add IsVideo to StickerSet

--- src/Telegram.Bots/Types/Stickers/StickerSet.cs
+++ src/Telegram.Bots/Types/Stickers/StickerSet.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 using System.Collections.Generic;
 
@@ -12,6 +12,8 @@ namespace Telegram.Bots.Types.Stickers
     public string Title { get; init; } = null!;
 
     public bool IsAnimated { get; init; }
+    
+    public bool IsVideo { get; init; }
 
     public bool ContainsMasks { get; init; }
 
