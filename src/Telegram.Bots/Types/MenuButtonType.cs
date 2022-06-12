1276de71cc23be4c6bd4180c1314bbb395a52c2e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-12 14:10:40 +0530

    Add MenuButtonType

---
+++ src/Telegram.Bots/Types/MenuButtonType.cs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types
+{
+  public enum MenuButtonType
+  {
+    Default,
+    Commands,
+    WebApp
+  }
+}
