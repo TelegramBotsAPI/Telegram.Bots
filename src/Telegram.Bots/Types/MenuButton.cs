14c946f569182a3d1a822666f331ca9b300f37f4
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-12 14:10:49 +0530

    Add MenuButton

---
+++ src/Telegram.Bots/Types/MenuButton.cs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types
+{
+  public abstract record MenuButton
+  {
+    public abstract MenuButtonType Type { get; }
+  }
+
+  public sealed record DefaultMenuButton : MenuButton
+  {
+    public override MenuButtonType Type => MenuButtonType.Default;
+  }
+
+  public sealed record CommandsMenuButton : MenuButton
+  {
+    public override MenuButtonType Type => MenuButtonType.Commands;
+  }
+
+  public sealed record WebAppMenuButton : MenuButton
+  {
+    public override MenuButtonType Type => MenuButtonType.WebApp;
+
+    public string Text { get; }
+
+    public WebAppInfo WebApp { get; }
+
+    public WebAppMenuButton(string text, WebAppInfo webApp)
+    {
+      Text = text;
+      WebApp = webApp;
+    }
+  }
+}
