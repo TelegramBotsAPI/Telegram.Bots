71f38c0d78f85c22d8cee4725d96b3bf60c9f053
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-14 15:29:09 +0530

    Refactor BotClient

--- src/Telegram.Bots/Http/BotClient.cs
+++ src/Telegram.Bots/Http/BotClient.cs
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
+using static System.Text.Encoding;
+using static System.Net.Http.HttpCompletionOption;
+using static System.UriKind;
+
+namespace Telegram.Bots.Http;
+
+using Configs;
+using Requests;
 using System;
 using System.IO;
 using System.Linq;
@@ -8,136 +16,167 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using Telegram.Bots.Configs;
-using Telegram.Bots.Requests;
-using Telegram.Bots.Types;
-using static System.Text.Encoding;
-using static System.Net.Http.HttpCompletionOption;
-using FileInfo = Telegram.Bots.Types.FileInfo;
+using Types;
+using FileInfo = Types.FileInfo;
 
-namespace Telegram.Bots.Http
+public sealed class BotClient : IBotClient
 {
-  public sealed class BotClient : IBotClient
+  private readonly HttpClient _client;
+  private readonly IBotConfig _config;
+  private readonly ISerializer _serializer;
+
+  public BotClient(HttpClient client, IBotConfig config, ISerializer serializer)
   {
-    private readonly HttpClient _client;
-    private readonly IBotConfig _config;
-    private readonly ISerializer _serializer;
+    _client = client;
+    _config = config;
+    _serializer = serializer;
+  }
 
-    public BotClient(HttpClient client, IBotConfig config, ISerializer serializer)
+  public async Task<Response<T>> HandleAsync<T>(
+    IRequest<T> request,
+    CancellationToken token)
+  {
+    if (request is null)
     {
-      _client = client;
-      _config = config;
-      _serializer = serializer;
+      throw new ArgumentNullException(nameof(request));
     }
 
-    public async Task<Response<T>> HandleAsync<T>(IRequest<T> request, CancellationToken token)
+    HttpRequestMessage? httpRequest = null;
+
+    try
     {
-      if (request is null) throw new ArgumentNullException(nameof(request));
-
-      HttpRequestMessage? httpRequest = null;
-
-      try
+      httpRequest = new()
       {
-        httpRequest = new()
+        Method = HttpMethod.Post,
+        RequestUri = new($"/bot{_config.Token}/{request.Method}", Relative),
+        Content = request switch
         {
-          Method = HttpMethod.Post,
-          RequestUri = new($"/bot{_config.Token}/{request.Method}", UriKind.Relative),
-          Content = request switch
-          {
-            IUploadable data when data.HasFiles() => GetMultipartCreator()(data),
-            _ => new StringContent(_serializer.Serialize(request), UTF8, "application/json")
-          }
-        };
-
-        using var httpResponse = await _client.SendAsync(httpRequest, ResponseHeadersRead, token)
-          .ConfigureAwait(false);
-
-        var httpContent = await httpResponse.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
-
-        return httpResponse.IsSuccessStatusCode
-          ? new(_serializer.Deserialize<Success<T>>(httpContent).Result)
-          : new(_serializer.Deserialize<Failure>(httpContent));
-      }
-      catch (TaskCanceledException)
-      {
-        return new(Canceled);
-      }
-      catch (OperationCanceledException)
-      {
-        return token.IsCancellationRequested ? new(Canceled) : new(TimedOut);
-      }
-      finally
-      {
-        httpRequest?.Dispose();
-      }
-
-      Func<IUploadable, HttpContent> GetMultipartCreator() => data =>
-      {
-        var content = new MultipartFormDataContent();
-
-        foreach (var property in _serializer.GetProperties(data))
-          content.Add(new StringContent(property.Value), property.Name);
-
-        foreach (var file in data.GetFiles().Where(file => file != null))
-          content.Add(new StreamContent(file!.Data)
-            {Headers = {ContentType = Stream}}, file.Id, file.Id);
-
-        return content;
+          IUploadable data when data.HasFiles() => GetMultipartCreator()(data),
+          _ => new StringContent(_serializer.Serialize(request), UTF8, Json)
+        }
       };
+
+      using HttpResponseMessage httpResponse = await _client
+        .SendAsync(httpRequest, ResponseHeadersRead, token)
+        .ConfigureAwait(false);
+
+      Stream httpContent = await httpResponse.Content.ReadAsStreamAsync(token)
+        .ConfigureAwait(false);
+
+      return httpResponse.IsSuccessStatusCode
+        ? new(_serializer.Deserialize<Success<T>>(httpContent).Result)
+        : new(_serializer.Deserialize<Failure>(httpContent));
+    }
+    catch (TaskCanceledException)
+    {
+      return new(Canceled);
+    }
+    catch (OperationCanceledException)
+    {
+      return token.IsCancellationRequested ? new(Canceled) : new(TimedOut);
+    }
+    finally
+    {
+      httpRequest?.Dispose();
     }
 
-    public async Task<Response<FileInfo>> HandleAsync(
-      string fileId,
-      Stream destination,
-      CancellationToken token)
+    Func<IUploadable, HttpContent> GetMultipartCreator() => data =>
     {
-      var response = await HandleAsync(new GetFile(fileId), token).ConfigureAwait(false);
+      MultipartFormDataContent content = new();
 
-      if (!response.Ok) return response;
-
-      var path = response.Result.Path;
-
-      if (string.IsNullOrEmpty(path)) return NotFound;
-
-      HttpRequestMessage? httpRequest = null;
-
-      try
+      foreach (DataProperty property in _serializer.GetProperties(data))
       {
-        httpRequest = new()
+        content.Add(new StringContent(property.Value), property.Name);
+      }
+
+      foreach (InputFile? file in data.GetFiles().Where(file => file != null))
+      {
+        content.Add(new StreamContent(file!.Data)
         {
-          Method = HttpMethod.Get,
-          RequestUri = new($"/file/bot{_config.Token}/{path}", UriKind.Relative)
-        };
-
-        using var httpResponse = await _client.SendAsync(httpRequest, ResponseHeadersRead, token)
-          .ConfigureAwait(false);
-
-        await using var httpContent = await httpResponse.Content.ReadAsStreamAsync(token)
-          .ConfigureAwait(false);
-
-        if (!httpResponse.IsSuccessStatusCode)
-          return new(_serializer.Deserialize<Failure>(httpContent));
-
-        await httpContent.CopyToAsync(destination, token).ConfigureAwait(false);
-      }
-      finally
-      {
-        httpRequest?.Dispose();
+          Headers =
+          {
+            ContentType = Stream
+          }
+        }, file.Id, file.Id);
       }
 
+      return content;
+    };
+  }
+
+  public async Task<Response<FileInfo>> HandleAsync(
+    string fileId,
+    Stream destination,
+    CancellationToken token)
+  {
+    Response<FileInfo> response = await HandleAsync(new GetFile(fileId), token)
+      .ConfigureAwait(false);
+
+    if (!response.Ok)
+    {
       return response;
     }
 
-    private static readonly MediaTypeHeaderValue Stream =
-      MediaTypeHeaderValue.Parse("application/octet-stream");
+    string? path = response.Result.Path;
 
-    private static readonly Failure TimedOut =
-      new() {Description = "Timed Out", ErrorCode = 408};
+    if (string.IsNullOrEmpty(path))
+    {
+      return NotFound;
+    }
 
-    private static readonly Failure Canceled =
-      new() {Description = "Canceled", ErrorCode = 408};
+    HttpRequestMessage? httpRequest = null;
 
-    private static readonly Response<FileInfo> NotFound =
-      new(new Failure {Description = "Not Found", ErrorCode = 404});
+    try
+    {
+      httpRequest = new()
+      {
+        Method = HttpMethod.Get,
+        RequestUri = new($"/file/bot{_config.Token}/{path}", Relative)
+      };
+
+      using HttpResponseMessage httpResponse = await _client
+        .SendAsync(httpRequest, ResponseHeadersRead, token)
+        .ConfigureAwait(false);
+
+      await using Stream httpContent = await httpResponse.Content
+        .ReadAsStreamAsync(token)
+        .ConfigureAwait(false);
+
+      if (!httpResponse.IsSuccessStatusCode)
+      {
+        return new(_serializer.Deserialize<Failure>(httpContent));
+      }
+
+      await httpContent.CopyToAsync(destination, token).ConfigureAwait(false);
+    }
+    finally
+    {
+      httpRequest?.Dispose();
+    }
+
+    return response;
   }
+
+  private const string Json = "application/json";
+
+  private static readonly MediaTypeHeaderValue Stream =
+    MediaTypeHeaderValue.Parse("application/octet-stream");
+
+  private static readonly Failure TimedOut =
+    new()
+    {
+      Description = "Timed Out", ErrorCode = 408
+    };
+
+  private static readonly Failure Canceled =
+    new()
+    {
+      Description = "Canceled", ErrorCode = 408
+    };
+
+  private static readonly Response<FileInfo> NotFound =
+    new(new Failure
+    {
+      Description = "Not Found", ErrorCode = 404
+    });
 }
