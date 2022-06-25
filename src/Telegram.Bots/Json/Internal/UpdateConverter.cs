4459f8bbc7f371cc303fdfd0a04f48f7416e496e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:46:57 +0530

    Refactor UpdateConverter

--- src/Telegram.Bots/Json/Internal/UpdateConverter.cs
+++ src/Telegram.Bots/Json/Internal/UpdateConverter.cs
@@ -1,74 +1,86 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using System;
-using System.Linq;
+namespace Telegram.Bots.Json.Internal;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Telegram.Bots.Types;
+using System;
+using System.Linq;
+using Types;
+using static UpdateSchema;
 
-namespace Telegram.Bots.Json.Internal
+internal sealed class UpdateConverter : JsonConverter
 {
-  using static UpdateSchema;
+  public override bool CanWrite => false;
 
-  internal sealed class UpdateConverter : JsonConverter
+  public override void WriteJson(
+    JsonWriter writer,
+    object? value,
+    JsonSerializer serializer)
   {
-    public override bool CanWrite => false;
+    throw new NotImplementedException();
+  }
 
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
-      throw new NotImplementedException();
+  public override object? ReadJson(
+    JsonReader reader,
+    Type objectType,
+    object? existingValue,
+    JsonSerializer serializer)
+  {
+    JObject? data = JObject.Load(reader);
 
-    public override object? ReadJson(
-      JsonReader reader,
-      Type objectType,
-      object? existingValue,
-      JsonSerializer serializer)
+    if (data.Type == JTokenType.Null)
     {
-      var data = JObject.Load(reader);
-
-      if (data.Type == JTokenType.Null) return null;
-
-      return data.Properties().Single(property => property.Name != Id).Name switch
-      {
-        Message => Get<MessageUpdate>(),
-        EditedMessage => Get<EditedMessageUpdate>(),
-        ChannelPost => Get<ChannelPostUpdate>(),
-        EditedChannelPost => Get<EditedChannelPostUpdate>(),
-        InlineQuery => Get<InlineQueryUpdate>(),
-        ChosenInlineResult => Get<ChosenInlineResultUpdate>(),
-        CallbackQuery => Get<CallbackQueryUpdate>(),
-        ShippingQuery => Get<ShippingQueryUpdate>(),
-        PreCheckoutQuery => Get<PreCheckoutQueryUpdate>(),
-        Poll => Get<PollUpdate>(),
-        PollAnswer => Get<PollAnswerUpdate>(),
-        MyChatMember => Get<MyChatMemberUpdate>(),
-        ChatMember => Get<ChatMemberUpdate>(),
-        ChatJoinRequest => Get<ChatJoinRequestUpdate>(),
-        _ => null
-      };
-
-      T Get<T>() => data.ToObject<T>(serializer)!;
+      return null;
     }
 
-    public override bool CanConvert(Type objectType) => objectType == typeof(Update);
+    string name = data.Properties()
+      .Single(property => property.Name != Id).Name;
+
+    return name switch
+    {
+      UpdateSchema.Message => Get<MessageUpdate>(),
+      EditedMessage => Get<EditedMessageUpdate>(),
+      ChannelPost => Get<ChannelPostUpdate>(),
+      EditedChannelPost => Get<EditedChannelPostUpdate>(),
+      InlineQuery => Get<InlineQueryUpdate>(),
+      ChosenInlineResult => Get<ChosenInlineResultUpdate>(),
+      UpdateSchema.CallbackQuery => Get<CallbackQueryUpdate>(),
+      ShippingQuery => Get<ShippingQueryUpdate>(),
+      PreCheckoutQuery => Get<PreCheckoutQueryUpdate>(),
+      UpdateSchema.Poll => Get<PollUpdate>(),
+      UpdateSchema.PollAnswer => Get<PollAnswerUpdate>(),
+      MyChatMember => Get<MyChatMemberUpdate>(),
+      UpdateSchema.ChatMember => Get<ChatMemberUpdate>(),
+      UpdateSchema.ChatJoinRequest => Get<ChatJoinRequestUpdate>(),
+      _ => null
+    };
+
+    T Get<T>() => data.ToObject<T>(serializer)!;
   }
 
-  internal static class UpdateSchema
+  public override bool CanConvert(Type objectType)
   {
-    public const string Id = "update_id";
-    public const string Message = "message";
-    public const string EditedMessage = "edited_message";
-    public const string ChannelPost = "channel_post";
-    public const string EditedChannelPost = "edited_channel_post";
-    public const string InlineQuery = "inline_query";
-    public const string ChosenInlineResult = "chosen_inline_result";
-    public const string CallbackQuery = "callback_query";
-    public const string ShippingQuery = "shipping_query";
-    public const string PreCheckoutQuery = "pre_checkout_query";
-    public const string Poll = "poll";
-    public const string PollAnswer = "poll_answer";
-    public const string MyChatMember = "my_chat_member";
-    public const string ChatMember = "chat_member";
-    public const string ChatJoinRequest = "chat_join_request";
+    return objectType == typeof(Update);
   }
+}
+
+internal static class UpdateSchema
+{
+  public const string Id = "update_id";
+  public const string Message = "message";
+  public const string EditedMessage = "edited_message";
+  public const string ChannelPost = "channel_post";
+  public const string EditedChannelPost = "edited_channel_post";
+  public const string InlineQuery = "inline_query";
+  public const string ChosenInlineResult = "chosen_inline_result";
+  public const string CallbackQuery = "callback_query";
+  public const string ShippingQuery = "shipping_query";
+  public const string PreCheckoutQuery = "pre_checkout_query";
+  public const string Poll = "poll";
+  public const string PollAnswer = "poll_answer";
+  public const string MyChatMember = "my_chat_member";
+  public const string ChatMember = "chat_member";
+  public const string ChatJoinRequest = "chat_join_request";
 }
