1fcb9b8a4086a427f57944ed2e1a8a928554561b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:47:28 +0530

    Refactor Serializer

--- src/Telegram.Bots/Json/Serializer.cs
+++ src/Telegram.Bots/Json/Serializer.cs
@@ -1,115 +1,132 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Json;
+
+using Internal;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
-using Telegram.Bots.Json.Internal;
-using Telegram.Bots.Types;
+using Types;
 
-namespace Telegram.Bots.Json
+public sealed class Serializer : ISerializer
 {
-  public sealed class Serializer : ISerializer
+  private static readonly JsonSerializerSettings Settings = CreateSettings();
+
+  private readonly JsonSerializer _serializer = JsonSerializer.Create(Settings);
+
+  public string Serialize<T>(T value)
   {
-    private static readonly JsonSerializerSettings Settings = CreateSettings();
+    StringBuilder builder = new(256);
 
-    private readonly JsonSerializer _serializer = JsonSerializer.Create(Settings);
+    Serialize(value, new StringWriter(builder, CultureInfo.InvariantCulture));
 
-    public string Serialize<T>(T value)
+    return builder.ToString();
+  }
+
+  public void Serialize<T>(T value, Stream destination)
+  {
+    Serialize(value, new StreamWriter(destination));
+  }
+
+  public T Deserialize<T>(string value)
+  {
+    return Deserialize<T>(new StringReader(value));
+  }
+
+  public T Deserialize<T>(Stream stream)
+  {
+    return Deserialize<T>(new StreamReader(stream));
+  }
+
+  public IEnumerable<DataProperty> GetProperties<T>(T value)
+  {
+    if (value is null)
     {
-      var builder = new StringBuilder(256);
-
-      Serialize(value, new StringWriter(builder, CultureInfo.InvariantCulture));
-
-      return builder.ToString();
+      throw new ArgumentNullException(nameof(value));
     }
 
-    public void Serialize<T>(T value, Stream destination) =>
-      Serialize(value, new StreamWriter(destination));
-
-    public T Deserialize<T>(string value) => Deserialize<T>(new StringReader(value));
-
-    public T Deserialize<T>(Stream stream) => Deserialize<T>(new StreamReader(stream));
-
-    public IEnumerable<DataProperty> GetProperties<T>(T value)
-    {
-      if (value is null) throw new ArgumentNullException(nameof(value));
-
-      return JObject.FromObject(value, _serializer).Properties().Select(property =>
+    return JObject.FromObject(value, _serializer).Properties().Select(
+      property =>
         new DataProperty(property.Name, property.Value.ToString()));
-    }
+  }
 
-    public static void Modify(JsonSerializerSettings settings)
+  public static void Modify(JsonSerializerSettings settings)
+  {
+    if (settings is null)
     {
-      if (settings is null) throw new ArgumentNullException(nameof(settings));
-
-      settings.ContractResolver = Settings.ContractResolver;
-      settings.NullValueHandling = Settings.NullValueHandling;
-      settings.Converters = Settings.Converters;
+      throw new ArgumentNullException(nameof(settings));
     }
 
-    private void Serialize<T>(T value, TextWriter textWriter)
+    settings.ContractResolver = Settings.ContractResolver;
+    settings.NullValueHandling = Settings.NullValueHandling;
+    settings.Converters = Settings.Converters;
+  }
+
+  private void Serialize<T>(T value, TextWriter textWriter)
+  {
+    JsonTextWriter? writer = null;
+
+    try
     {
-      JsonTextWriter? writer = null;
+      writer = new JsonTextWriter(textWriter);
 
-      try
-      {
-        writer = new JsonTextWriter(textWriter);
-
-        _serializer.Serialize(writer, value);
-      }
-      finally
-      {
-        writer?.Close();
-      }
+      _serializer.Serialize(writer, value);
     }
-
-    private T Deserialize<T>(TextReader textReader)
+    finally
     {
-      JsonTextReader? reader = null;
-
-      try
-      {
-        reader = new JsonTextReader(textReader);
-
-        return _serializer.Deserialize<T>(reader)!;
-      }
-      finally
-      {
-        reader?.Close();
-      }
+      writer?.Close();
     }
+  }
 
-    private static JsonSerializerSettings CreateSettings()
+  private T Deserialize<T>(TextReader textReader)
+  {
+    JsonTextReader? reader = null;
+
+    try
     {
-      var strategy = new SnakeCaseNamingStrategy();
+      reader = new JsonTextReader(textReader);
 
-      return new JsonSerializerSettings
-      {
-        ContractResolver = new ContractResolver {NamingStrategy = strategy},
-        NullValueHandling = NullValueHandling.Ignore,
-        Converters =
-        {
-          new UnixDateTimeConverter(),
-          new StringEnumConverter(strategy),
-          new CallbackQueryConverter(),
-          new ChatMemberConverter(),
-          new InlineButtonConverter(),
-          new InputFileConverter(),
-          new MenuButtonConverter(),
-          new MessageConverter(),
-          new PollConverter(),
-          new TextButtonConverter(),
-          new UpdateConverter()
-        }
-      };
+      return _serializer.Deserialize<T>(reader)!;
     }
+    finally
+    {
+      reader?.Close();
+    }
+  }
+
+  private static JsonSerializerSettings CreateSettings()
+  {
+    SnakeCaseNamingStrategy strategy = new();
+
+    return new JsonSerializerSettings
+    {
+      ContractResolver = new ContractResolver
+      {
+        NamingStrategy = strategy
+      },
+      NullValueHandling = NullValueHandling.Ignore,
+      Converters =
+      {
+        new UnixDateTimeConverter(),
+        new StringEnumConverter(strategy),
+        new CallbackQueryConverter(),
+        new ChatMemberConverter(),
+        new InlineButtonConverter(),
+        new InputFileConverter(),
+        new MenuButtonConverter(),
+        new MessageConverter(),
+        new PollConverter(),
+        new TextButtonConverter(),
+        new UpdateConverter()
+      }
+    };
   }
 }
