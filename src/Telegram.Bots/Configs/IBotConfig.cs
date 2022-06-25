bfb78ee9dc1e0e8a4020c929db615bc17b1c4346
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:21:17 +0530

    Refactor IBotClient

--- src/Telegram.Bots/Configs/IBotConfig.cs
+++ src/Telegram.Bots/Configs/IBotConfig.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Configs
+namespace Telegram.Bots.Configs;
+
+public interface IBotConfig
 {
-  public interface IBotConfig
-  {
-    string Token { get; }
-  }
+  string Token { get; }
 }
