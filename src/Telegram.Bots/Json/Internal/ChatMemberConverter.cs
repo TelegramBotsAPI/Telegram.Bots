b236cdf39734b0cc35b95e1d7c06e6f36a591a3b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:30:01 +0530

    Refactor ChatMemberConverter

--- src/Telegram.Bots/Json/Internal/ChatMemberConverter.cs
+++ src/Telegram.Bots/Json/Internal/ChatMemberConverter.cs
@@ -1,60 +1,72 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using System;
-using System.Linq;
+namespace Telegram.Bots.Json.Internal;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Telegram.Bots.Types;
+using System;
+using System.Linq;
+using Types;
+using static ChatMemberSchema;
 
-namespace Telegram.Bots.Json.Internal
+internal sealed class ChatMemberConverter : JsonConverter
 {
-  using static ChatMemberSchema;
+  public override bool CanWrite => false;
 
-  internal sealed class ChatMemberConverter : JsonConverter
+  public override void WriteJson(
+    JsonWriter writer,
+    object? value,
+    JsonSerializer serializer)
   {
-    public override bool CanWrite => false;
+    throw new NotImplementedException();
+  }
 
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
-      throw new NotImplementedException();
+  public override object? ReadJson(
+    JsonReader reader,
+    Type objectType,
+    object? existingValue,
+    JsonSerializer serializer)
+  {
+    JObject data = JObject.Load(reader);
 
-    public override object? ReadJson(
-      JsonReader reader,
-      Type objectType,
-      object? existingValue,
-      JsonSerializer serializer)
+    if (data.Type == JTokenType.Null)
     {
-      var data = JObject.Load(reader);
-
-      if (data.Type == JTokenType.Null) return null;
-
-      return data.Properties().Single(property => property.Name == Status).Value.ToString() switch
-      {
-        Creator => Get<Creator>(),
-        Administrator => Get<Administrator>(),
-        Member => Get<NormalMember>(),
-        Restricted => Get<RestrictedMember>(),
-        Left => Get<LeftMember>(),
-        Kicked => Get<KickedMember>(),
-        _ => null
-      };
-
-      T Get<T>() => data.ToObject<T>(serializer)!;
+      return null;
     }
 
-    public override bool CanConvert(Type objectType) =>
-      objectType == typeof(ChatMember) ||
-      objectType == typeof(PrivilegedMember);
+    string status = data.Properties()
+      .Single(property => property.Name == Status).Value
+      .ToString();
+
+    return status switch
+    {
+      ChatMemberSchema.Creator => Get<Creator>(),
+      ChatMemberSchema.Administrator => Get<Administrator>(),
+      Member => Get<NormalMember>(),
+      Restricted => Get<RestrictedMember>(),
+      Left => Get<LeftMember>(),
+      Kicked => Get<KickedMember>(),
+      _ => null
+    };
+
+    T Get<T>() => data.ToObject<T>(serializer)!;
   }
 
-  internal static class ChatMemberSchema
+  public override bool CanConvert(Type objectType)
   {
-    public const string Status = "status";
-    public const string Creator = "creator";
-    public const string Administrator = "administrator";
-    public const string Member = "member";
-    public const string Restricted = "restricted";
-    public const string Left = "left";
-    public const string Kicked = "kicked";
+    return objectType == typeof(ChatMember) ||
+           objectType == typeof(PrivilegedMember);
   }
+}
+
+internal static class ChatMemberSchema
+{
+  public const string Status = "status";
+  public const string Creator = "creator";
+  public const string Administrator = "administrator";
+  public const string Member = "member";
+  public const string Restricted = "restricted";
+  public const string Left = "left";
+  public const string Kicked = "kicked";
 }
