b412ef17c5eebf34df71c3f46ae8ebace9056b34
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:38:57 +0530

    Refactor MenuButtonConverter

--- src/Telegram.Bots/Json/Internal/MenuButtonConverter.cs
+++ src/Telegram.Bots/Json/Internal/MenuButtonConverter.cs
@@ -1,56 +1,63 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-using System;
-using System.Linq;
+namespace Telegram.Bots.Json.Internal;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Telegram.Bots.Types;
+using System;
+using System.Linq;
+using Types;
+using static MenuButtonSchema;
 
-namespace Telegram.Bots.Json.Internal
+internal sealed class MenuButtonConverter : JsonConverter
 {
-  using static MenuButtonSchema;
-  
-  internal sealed class MenuButtonConverter : JsonConverter
+  public override void WriteJson(
+    JsonWriter writer,
+    object? value,
+    JsonSerializer serializer)
   {
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    writer.WriteValue(value);
+  }
+
+  public override object? ReadJson(
+    JsonReader reader,
+    Type objectType,
+    object? existingValue,
+    JsonSerializer serializer)
+  {
+    JObject? data = JObject.Load(reader);
+
+    if (data.Type == JTokenType.Null)
     {
-      writer.WriteValue(value);
+      return null;
     }
 
-    public override object? ReadJson(
-      JsonReader reader,
-      Type objectType,
-      object? existingValue,
-      JsonSerializer serializer)
+    string type = data.Properties()
+      .Single(property => property.Name == MenuButtonSchema.Type).Value
+      .ToString();
+
+    return type switch
     {
-      var data = JObject.Load(reader);
+      Default => Get<DefaultMenuButton>(),
+      Commands => Get<CommandsMenuButton>(),
+      WebApp => Get<WebAppMenuButton>(),
+      _ => null
+    };
 
-      if (data.Type == JTokenType.Null) return null;
-
-      var type = data.Properties()
-        .Single(property => property.Name == Type).Value
-        .ToString();
-
-      return type switch
-      {
-        Default => Get<DefaultMenuButton>(),
-        Commands => Get<CommandsMenuButton>(),
-        WebApp => Get<WebAppMenuButton>(),
-        _ => null
-      };
-
-      T Get<T>() => data.ToObject<T>(serializer)!;
-    }
-
-    public override bool CanConvert(Type type) => type == typeof(MenuButton);
+    T Get<T>() => data.ToObject<T>(serializer)!;
   }
-  
-  internal static class MenuButtonSchema
+
+  public override bool CanConvert(Type type)
   {
-    public const string Type = "type";
-    public const string Default = "default";
-    public const string Commands = "commands";
-    public const string WebApp = "web_app";
+    return type == typeof(MenuButton);
   }
+}
+
+internal static class MenuButtonSchema
+{
+  public const string Type = "type";
+  public const string Default = "default";
+  public const string Commands = "commands";
+  public const string WebApp = "web_app";
 }
