019b340ae745823fcc7a4d385024935b98e80b5b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-25 20:37:37 +0530

    Refactor InlineButtonConverter

--- src/Telegram.Bots/Json/Internal/InlineButtonConverter.cs
+++ src/Telegram.Bots/Json/Internal/InlineButtonConverter.cs
@@ -1,71 +1,86 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Json.Internal;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Telegram.Bots.Types;
+using Types;
+using static InlineButtonSchema;
 
-namespace Telegram.Bots.Json.Internal
+internal sealed class InlineButtonConverter : JsonConverter
 {
-  using static InlineButtonSchema;
-
-  internal sealed class InlineButtonConverter : JsonConverter
+  private static readonly HashSet<string> ButtonTypes = new()
   {
-    private static readonly HashSet<string> ButtonTypes = new()
+    Url,
+    InlineButtonSchema.LoginUrl,
+    CallbackData,
+    SwitchInlineQuery,
+    SwitchQueryCurrentChat,
+    CallbackGame,
+    Pay
+  };
+
+  public override bool CanWrite => false;
+
+  public override void WriteJson(
+    JsonWriter writer,
+    object? value,
+    JsonSerializer serializer)
+  {
+    throw new NotImplementedException();
+  }
+
+  public override object? ReadJson(
+    JsonReader reader,
+    Type objectType,
+    object? existingValue,
+    JsonSerializer serializer)
+  {
+    JObject? data = JObject.Load(reader);
+
+    if (data.Type == JTokenType.Null)
     {
-      Url,
-      LoginUrl,
-      CallbackData,
-      SwitchInlineQuery,
-      SwitchQueryCurrentChat,
-      CallbackGame,
-      Pay
-    };
-
-    public override bool CanWrite => false;
-
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
-      throw new NotImplementedException();
-
-    public override object? ReadJson(
-      JsonReader reader,
-      Type objectType,
-      object? existingValue,
-      JsonSerializer serializer)
-    {
-      var data = JObject.Load(reader);
-
-      if (data.Type == JTokenType.Null) return null;
-
-      return data.Properties().Single(property => ButtonTypes.Contains(property.Name)).Name switch
-      {
-        Url => Get<UrlButton>(),
-        LoginUrl => Get<LoginUrlButton>(),
-        CallbackData => Get<CallbackDataButton>(),
-        SwitchInlineQuery => Get<SwitchInlineQueryButton>(),
-        SwitchQueryCurrentChat => Get<SwitchInlineQueryCurrentChatButton>(),
-        CallbackGame => Get<CallbackGameButton>(),
-        Pay => Get<PayButton>(),
-        _ => null
-      };
-
-      T Get<T>() => data.ToObject<T>(serializer)!;
+      return null;
     }
 
-    public override bool CanConvert(Type objectType) => objectType == typeof(InlineButton);
+    string name = data.Properties()
+      .Single(property => ButtonTypes.Contains(property.Name)).Name;
+
+    return name switch
+    {
+      Url => Get<UrlButton>(),
+      InlineButtonSchema.LoginUrl => Get<LoginUrlButton>(),
+      CallbackData => Get<CallbackDataButton>(),
+      SwitchInlineQuery => Get<SwitchInlineQueryButton>(),
+      SwitchQueryCurrentChat => Get<SwitchInlineQueryCurrentChatButton>(),
+      CallbackGame => Get<CallbackGameButton>(),
+      Pay => Get<PayButton>(),
+      _ => null
+    };
+
+    T Get<T>() => data.ToObject<T>(serializer)!;
   }
 
-  internal static class InlineButtonSchema
+  public override bool CanConvert(Type objectType)
   {
-    public const string Url = "url";
-    public const string LoginUrl = "login_url";
-    public const string CallbackData = "callback_data";
-    public const string SwitchInlineQuery = "switch_inline_query";
-    public const string SwitchQueryCurrentChat = "switch_inline_query_current_chat";
-    public const string CallbackGame = "callback_game";
-    public const string Pay = "pay";
+    return objectType == typeof(InlineButton);
   }
+}
+
+internal static class InlineButtonSchema
+{
+  public const string Url = "url";
+  public const string LoginUrl = "login_url";
+  public const string CallbackData = "callback_data";
+  public const string SwitchInlineQuery = "switch_inline_query";
+
+  public const string SwitchQueryCurrentChat =
+    "switch_inline_query_current_chat";
+
+  public const string CallbackGame = "callback_game";
+  public const string Pay = "pay";
 }
