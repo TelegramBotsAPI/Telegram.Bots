2404984c99a4fbd5bdb9e092760fc4d87f6e354e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-27 23:05:48 +0530

    Refactor ExportChatInviteLink

--- src/Telegram.Bots/Requests/Admins/ExportChatInviteLink.cs
+++ src/Telegram.Bots/Requests/Admins/ExportChatInviteLink.cs
@@ -1,27 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Requests.Admins
 {
-  public abstract record ExportChatInviteLink<TChatId> : IRequest<string>, IChatTargetable<TChatId>
+  public abstract record ExportChatInviteLink<TChatId>(
+    TChatId ChatId) : IRequest<string>, IChatTargetable<TChatId>
   {
-    public TChatId ChatId { get; }
-
-    public string Method { get; } = "exportChatInviteLink";
-
-    protected ExportChatInviteLink(TChatId chatId) => ChatId = chatId;
+    public string Method => "exportChatInviteLink";
   }
 
-  public sealed record ExportChatInviteLink : ExportChatInviteLink<long>
-  {
-    public ExportChatInviteLink(long chatId) : base(chatId) { }
-  }
+  public sealed record ExportChatInviteLink(
+    long ChatId) : ExportChatInviteLink<long>(ChatId);
 
   namespace Usernames
   {
-    public sealed record ExportChatInviteLink : ExportChatInviteLink<string>
-    {
-      public ExportChatInviteLink(string username) : base(username) { }
-    }
+    public sealed record ExportChatInviteLink(
+      string ChatId) : ExportChatInviteLink<string>(ChatId);
   }
 }
