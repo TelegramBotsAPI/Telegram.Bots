a4b88ab7cf2d203003ada34a8e9703554e15ff48
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-28 22:43:34 +0530

    Refactor UnbanChatMember

--- src/Telegram.Bots/Requests/Admins/UnbanChatMember.cs
+++ src/Telegram.Bots/Requests/Admins/UnbanChatMember.cs
@@ -1,35 +1,25 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Requests.Admins
 {
-  public abstract record UnbanChatMember<TChatId> : IRequest<bool>, IChatMemberTargetable<TChatId>
+  public abstract record UnbanChatMember<TChatId>(
+    TChatId ChatId,
+    long UserId) : IRequest<bool>, IChatMemberTargetable<TChatId>
   {
-    public TChatId ChatId { get; }
-
-    public long UserId { get; }
-
     public bool? OnlyIfBanned { get; init; }
 
-    public string Method { get; } = "unbanChatMember";
-
-    protected UnbanChatMember(TChatId chatId, long userId)
-    {
-      ChatId = chatId;
-      UserId = userId;
-    }
+    public string Method => "unbanChatMember";
   }
 
-  public sealed record UnbanChatMember : UnbanChatMember<long>
-  {
-    public UnbanChatMember(long chatId, long userId) : base(chatId, userId) { }
-  }
+  public sealed record UnbanChatMember(
+    long ChatId,
+    long UserId) : UnbanChatMember<long>(ChatId, UserId);
 
   namespace Usernames
   {
-    public sealed record UnbanChatMember : UnbanChatMember<string>
-    {
-      public UnbanChatMember(string username, long userId) : base(username, userId) { }
-    }
+    public sealed record UnbanChatMember(
+      string ChatId,
+      long UserId) : UnbanChatMember<string>(ChatId, UserId);
   }
 }
