41c17673358e38bfc6057fbbad390d1e4e7c7ea8
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-30 21:12:15 +0530

    Refactor AnswerShippingQuery

--- src/Telegram.Bots/Requests/Payments/AnswerShippingQuery.cs
+++ src/Telegram.Bots/Requests/Payments/AnswerShippingQuery.cs
@@ -1,34 +1,35 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Requests.Payments;
 
 using System.Collections.Generic;
-using Telegram.Bots.Types.Payments;
+using Types.Payments;
 
-namespace Telegram.Bots.Requests.Payments
+public sealed record AnswerShippingQuery : IRequest<bool>
 {
-  public sealed record AnswerShippingQuery : IRequest<bool>
+  public string QueryId { get; }
+
+  public bool Ok { get; }
+
+  public IEnumerable<ShippingOption>? Options { get; }
+
+  public string? ErrorMessage { get; }
+
+  public string Method => "answerShippingQuery";
+
+  public AnswerShippingQuery(
+    string queryId,
+    IEnumerable<ShippingOption>? options)
   {
-    public string QueryId { get; }
+    QueryId = queryId;
+    Options = options;
+    Ok = true;
+  }
 
-    public bool Ok { get; }
-
-    public IEnumerable<ShippingOption>? Options { get; }
-
-    public string? ErrorMessage { get; }
-
-    public string Method { get; } = "answerShippingQuery";
-
-    public AnswerShippingQuery(string queryId, IEnumerable<ShippingOption>? options)
-    {
-      QueryId = queryId;
-      Options = options;
-      Ok = true;
-    }
-
-    public AnswerShippingQuery(string queryId, string? errorMessage)
-    {
-      QueryId = queryId;
-      ErrorMessage = errorMessage;
-    }
+  public AnswerShippingQuery(string queryId, string? errorMessage)
+  {
+    QueryId = queryId;
+    ErrorMessage = errorMessage;
   }
 }
