072071952e7257dcb68879b747513e392094fe99
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-30 21:05:34 +0530

    Refactor SetGameScore

--- src/Telegram.Bots/Requests/Games/SetGameScore.cs
+++ src/Telegram.Bots/Requests/Games/SetGameScore.cs
@@ -1,57 +1,39 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
-
-using Telegram.Bots.Types;
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Requests.Games
 {
-  public abstract record SetGameScoreBase : IRequest<GameMessage>, IUserTargetable
+  using Types;
+
+  public abstract record SetGameScoreBase(
+    long UserId,
+    uint Score) : IRequest<GameMessage>, IUserTargetable
   {
-    public long UserId { get; }
-
-    public uint Score { get; }
-
     public bool? Force { get; init; }
 
     public bool? DisableEdit { get; init; }
 
-    public string Method { get; } = "setGameScore";
-
-    protected SetGameScoreBase(long userId, uint score)
-    {
-      UserId = userId;
-      Score = score;
-    }
+    public string Method => "setGameScore";
   }
 
-  public abstract record SetGameScore<TChatId> : SetGameScoreBase, IChatMessageTargetable<TChatId>
-  {
-    public TChatId ChatId { get; }
+  public abstract record SetGameScore<TChatId>(
+    TChatId ChatId,
+    int MessageId,
+    long UserId,
+    uint Score) : SetGameScoreBase(UserId, Score),
+    IChatMessageTargetable<TChatId>;
 
-    public int MessageId { get; }
-
-    protected SetGameScore(TChatId chatId, int messageId, long userId, uint score) :
-      base(userId, score)
-    {
-      ChatId = chatId;
-      MessageId = messageId;
-    }
-  }
-
-  public sealed record SetGameScore : SetGameScore<long>
-  {
-    public SetGameScore(long chatId, int messageId, long userId, uint score) :
-      base(chatId, messageId, userId, score) { }
-  }
+  public sealed record SetGameScore(
+    long ChatId,
+    int MessageId,
+    long UserId,
+    uint Score) : SetGameScore<long>(ChatId, MessageId, UserId, Score);
 
   namespace Inline
   {
-    public sealed record SetGameScore : SetGameScoreBase, IInlineMessageTargetable
-    {
-      public string MessageId { get; }
-
-      public SetGameScore(string messageId, long userId, uint score) : base(userId, score) =>
-        MessageId = messageId;
-    }
+    public sealed record SetGameScore(
+      string MessageId,
+      long UserId,
+      uint Score) : SetGameScoreBase(UserId, Score), IInlineMessageTargetable;
   }
 }
