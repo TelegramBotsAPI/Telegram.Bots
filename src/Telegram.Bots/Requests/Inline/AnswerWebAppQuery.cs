1cc3dd9b2362c7c192072d8c8b6b29f5fa9b642f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-06-30 21:09:30 +0530

    Refactor AnswerWebAppQuery

--- src/Telegram.Bots/Requests/Inline/AnswerWebAppQuery.cs
+++ src/Telegram.Bots/Requests/Inline/AnswerWebAppQuery.cs
@@ -1,22 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-using Telegram.Bots.Types.Inline;
+namespace Telegram.Bots.Requests.Inline;
 
-namespace Telegram.Bots.Requests.Inline
+using Types.Inline;
+
+public sealed record AnswerWebAppQuery(
+  string QueryId,
+  InlineResult Result) : IRequest<SentWebAppMessage>
 {
-  public sealed record AnswerWebAppQuery : IRequest<SentWebAppMessage>
-  {
-    public string QueryId { get; }
-    
-    public InlineResult Result { get; }
-
-    public string Method { get; } = "answerWebAppQuery";
-    
-    public AnswerWebAppQuery(string queryId, InlineResult result)
-    {
-      QueryId = queryId;
-      Result = result;
-    }
-  }
+  public string Method => "answerWebAppQuery";
 }
