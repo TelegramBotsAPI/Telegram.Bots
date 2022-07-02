32bdb4b5d9be6c9c9c75d1bece0aede01b3b1601
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-02 22:11:19 +0530

    Refactor SendSticker

--- src/Telegram.Bots/Requests/Stickers/SendSticker.cs
+++ src/Telegram.Bots/Requests/Stickers/SendSticker.cs
@@ -1,21 +1,19 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-using System;
-using System.Collections.Generic;
-using Telegram.Bots.Types;
-
 namespace Telegram.Bots.Requests.Stickers
 {
-  public abstract record SendSticker<TChatId, TSticker> : IRequest<StickerMessage>,
-    IChatTargetable<TChatId>, INotifiable, IProtectable, IReplyable, IMarkupable
+  using System;
+  using System.Collections.Generic;
+  using Types;
+
+  public abstract record SendSticker<TChatId, TSticker>(
+    TChatId ChatId,
+    TSticker Sticker) : IRequest<StickerMessage>, IChatTargetable<TChatId>,
+    INotifiable, IProtectable, IReplyable, IMarkupable
   {
-    public TChatId ChatId { get; }
-
-    public TSticker Sticker { get; }
-
     public bool? DisableNotification { get; init; }
-    
+
     public bool? ProtectContent { get; init; }
 
     public int? ReplyToMessageId { get; init; }
@@ -24,52 +22,47 @@ namespace Telegram.Bots.Requests.Stickers
 
     public ReplyMarkup? ReplyMarkup { get; init; }
 
-    public string Method { get; } = "sendSticker";
+    public string Method => "sendSticker";
+  }
 
-    protected SendSticker(TChatId chatId, TSticker sticker)
+  public abstract record SendStickerFile<TChatId>(
+    TChatId ChatId,
+    InputFile Sticker) : SendSticker<TChatId, InputFile>(ChatId, Sticker),
+    IUploadable
+  {
+    public IEnumerable<InputFile?> GetFiles()
     {
-      ChatId = chatId;
-      Sticker = sticker;
+      return new[]
+      {
+        Sticker
+      };
     }
   }
 
-  public abstract record SendStickerFile<TChatId> : SendSticker<TChatId, InputFile>, IUploadable
-  {
-    protected SendStickerFile(TChatId chatId, InputFile sticker) : base(chatId, sticker) { }
+  public sealed record SendCachedSticker(
+    long ChatId,
+    string Sticker) : SendSticker<long, string>(ChatId, Sticker);
 
-    public IEnumerable<InputFile?> GetFiles() => new[] {Sticker};
-  }
+  public sealed record SendStickerUrl(
+    long ChatId,
+    Uri Sticker) : SendSticker<long, Uri>(ChatId, Sticker);
 
-  public sealed record SendCachedSticker : SendSticker<long, string>
-  {
-    public SendCachedSticker(long chatId, string sticker) : base(chatId, sticker) { }
-  }
-
-  public sealed record SendStickerUrl : SendSticker<long, Uri>
-  {
-    public SendStickerUrl(long chatId, Uri sticker) : base(chatId, sticker) { }
-  }
-
-  public sealed record SendStickerFile : SendStickerFile<long>
-  {
-    public SendStickerFile(long chatId, InputFile sticker) : base(chatId, sticker) { }
-  }
+  public sealed record SendStickerFile(
+    long ChatId,
+    InputFile Sticker) : SendStickerFile<long>(ChatId, Sticker);
 
   namespace Usernames
   {
-    public sealed record SendCachedSticker : SendSticker<string, string>
-    {
-      public SendCachedSticker(string username, string sticker) : base(username, sticker) { }
-    }
+    public sealed record SendCachedSticker(
+      string ChatId,
+      string Sticker) : SendSticker<string, string>(ChatId, Sticker);
 
-    public sealed record SendStickerUrl : SendSticker<string, Uri>
-    {
-      public SendStickerUrl(string username, Uri sticker) : base(username, sticker) { }
-    }
+    public sealed record SendStickerUrl(
+      string ChatId,
+      Uri Sticker) : SendSticker<string, Uri>(ChatId, Sticker);
 
-    public sealed record SendStickerFile : SendStickerFile<string>
-    {
-      public SendStickerFile(string username, InputFile sticker) : base(username, sticker) { }
-    }
+    public sealed record SendStickerFile(
+      string ChatId,
+      InputFile Sticker) : SendStickerFile<string>(ChatId, Sticker);
   }
 }
