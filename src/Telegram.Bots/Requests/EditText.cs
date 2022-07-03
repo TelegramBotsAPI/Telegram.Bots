ff91620dc60b9932dc1ec2c26db87b54ab476b8c
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-03 21:13:43 +0530

    Refactor EditText

--- src/Telegram.Bots/Requests/EditText.cs
+++ src/Telegram.Bots/Requests/EditText.cs
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
-
-using System.Collections.Generic;
-using Telegram.Bots.Types;
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Requests
 {
-  public abstract record EditTextBase<TResult> : IRequest<TResult>, IInlineMarkupable
-  {
-    public string Text { get; }
+  using System.Collections.Generic;
+  using Types;
 
+  public abstract record EditTextBase<TResult>(
+    string Text) : IRequest<TResult>, IInlineMarkupable
+  {
     public ParseMode? ParseMode { get; init; }
 
     public IEnumerable<MessageEntity>? Entities { get; init; }
@@ -18,46 +17,32 @@ namespace Telegram.Bots.Requests
 
     public InlineKeyboardMarkup? ReplyMarkup { get; init; }
 
-    public string Method { get; } = "editMessageText";
-
-    protected EditTextBase(string text) => Text = text;
+    public string Method => "editMessageText";
   }
 
-  public abstract record EditText<TChatId> : EditTextBase<TextMessage>,
-    IChatMessageTargetable<TChatId>
-  {
-    public TChatId ChatId { get; }
+  public abstract record EditText<TChatId>(
+    TChatId ChatId,
+    int MessageId,
+    string Text) : EditTextBase<TextMessage>(Text),
+    IChatMessageTargetable<TChatId>;
 
-    public int MessageId { get; }
-
-    protected EditText(TChatId chatId, int messageId, string text) : base(text)
-    {
-      ChatId = chatId;
-      MessageId = messageId;
-    }
-  }
-
-  public sealed record EditText : EditText<long>
-  {
-    public EditText(long chatId, int messageId, string text) : base(chatId, messageId, text) { }
-  }
+  public sealed record EditText(
+    long ChatId,
+    int MessageId,
+    string Text) : EditText<long>(ChatId, MessageId, Text);
 
   namespace Usernames
   {
-    public sealed record EditText : EditText<string>
-    {
-      public EditText(string username, int messageId, string text) :
-        base(username, messageId, text) { }
-    }
+    public sealed record EditText(
+      string ChatId,
+      int MessageId,
+      string Text) : EditText<string>(ChatId, MessageId, Text);
   }
 
   namespace Inline
   {
-    public sealed record EditText : EditTextBase<bool>, IInlineMessageTargetable
-    {
-      public string MessageId { get; }
-
-      public EditText(string messageId, string text) : base(text) => MessageId = messageId;
-    }
+    public sealed record EditText(
+      string MessageId,
+      string Text) : EditTextBase<bool>(Text), IInlineMessageTargetable;
   }
 }
