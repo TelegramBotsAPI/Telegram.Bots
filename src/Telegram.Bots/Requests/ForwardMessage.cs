5febf8838f1faf0f5e5bef4de678210bf930ce9f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-03 21:14:51 +0530

    Refactor ForwardMessage

--- src/Telegram.Bots/Requests/ForwardMessage.cs
+++ src/Telegram.Bots/Requests/ForwardMessage.cs
@@ -1,57 +1,46 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-using Telegram.Bots.Types;
-
 namespace Telegram.Bots.Requests
 {
-  public abstract record ForwardMessage<TChatId, TFromChatId> : IRequest<Message>,
-    IChatMessageTargetable<TChatId>, INotifiable, IProtectable
+  using Types;
+
+  public abstract record ForwardMessage<TChatId, TFromChatId>(
+    TChatId ChatId,
+    TFromChatId FromChatId,
+    int MessageId) : IRequest<Message>, IChatMessageTargetable<TChatId>,
+    INotifiable, IProtectable
   {
-    public TChatId ChatId { get; }
-
-    public TFromChatId FromChatId { get; }
-
-    public int MessageId { get; }
-
     public bool? DisableNotification { get; init; }
-    
+
     public bool? ProtectContent { get; init; }
 
-    public string Method { get; } = "forwardMessage";
-
-    protected ForwardMessage(TChatId chatId, TFromChatId fromChatId, int messageId)
-    {
-      ChatId = chatId;
-      FromChatId = fromChatId;
-      MessageId = messageId;
-    }
+    public string Method => "forwardMessage";
   }
 
-  public sealed record ForwardMessage : ForwardMessage<long, long>
-  {
-    public ForwardMessage(long chatId, long fromChatId, int messageId) :
-      base(chatId, fromChatId, messageId) { }
-  }
+  public sealed record ForwardMessage(
+    long ChatId,
+    long FromChatId,
+    int MessageId) : ForwardMessage<long, long>(ChatId, FromChatId, MessageId);
 
-  public sealed record ForwardMessageViaUsername : ForwardMessage<string, long>
-  {
-    public ForwardMessageViaUsername(string username, long fromChatId, int messageId) :
-      base(username, fromChatId, messageId) { }
-  }
+  public sealed record ForwardMessageViaUsername(
+    string ChatId,
+    long FromChatId,
+    int MessageId) :
+    ForwardMessage<string, long>(ChatId, FromChatId, MessageId);
 
   namespace Usernames
   {
-    public sealed record ForwardMessage : ForwardMessage<string, string>
-    {
-      public ForwardMessage(string username, string fromUsername, int messageId) :
-        base(username, fromUsername, messageId) { }
-    }
+    public sealed record ForwardMessage(
+      string ChatId,
+      string FromChatId,
+      int MessageId) :
+      ForwardMessage<string, string>(ChatId, FromChatId, MessageId);
 
-    public sealed record ForwardMessageViaId : ForwardMessage<long, string>
-    {
-      public ForwardMessageViaId(long chatId, string fromUsername, int messageId) :
-        base(chatId, fromUsername, messageId) { }
-    }
+    public sealed record ForwardMessageViaId(
+      long ChatId,
+      string FromChatId,
+      int MessageId) :
+      ForwardMessage<long, string>(ChatId, FromChatId, MessageId);
   }
 }
