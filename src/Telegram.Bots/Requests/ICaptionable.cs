3ceaceb1a6a408d2155964a76065fc10c3f0cc86
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-04 19:59:08 +0530

    Refactor ICaptionable

--- src/Telegram.Bots/Requests/ICaptionable.cs
+++ src/Telegram.Bots/Requests/ICaptionable.cs
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Requests;
 
 using System.Collections.Generic;
-using Telegram.Bots.Types;
+using Types;
 
-namespace Telegram.Bots.Requests
+public interface ICaptionable
 {
-  public interface ICaptionable
-  {
-    string? Caption { get; init; }
+  string? Caption { get; init; }
 
-    ParseMode? ParseMode { get; init; }
+  ParseMode? ParseMode { get; init; }
 
-    IEnumerable<MessageEntity>? CaptionEntities { get; init; }
-  }
+  IEnumerable<MessageEntity>? CaptionEntities { get; init; }
 }
