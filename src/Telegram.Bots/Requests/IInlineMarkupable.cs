74f98cefa15cdeb73d9ca56baef81d5edcecf675
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-04 19:59:45 +0530

    Refactor IInlineMarkupable

--- src/Telegram.Bots/Requests/IInlineMarkupable.cs
+++ src/Telegram.Bots/Requests/IInlineMarkupable.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using Telegram.Bots.Types;
+namespace Telegram.Bots.Requests;
 
-namespace Telegram.Bots.Requests
+using Types;
+
+public interface IInlineMarkupable
 {
-  public interface IInlineMarkupable
-  {
-    InlineKeyboardMarkup? ReplyMarkup { get; init; }
-  }
+  InlineKeyboardMarkup? ReplyMarkup { get; init; }
 }
