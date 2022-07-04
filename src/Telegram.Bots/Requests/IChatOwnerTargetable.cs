3bf2c1a3d29880ade517ba10bf69e37ff2abc36b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-04 19:59:30 +0530

    Refactor IChatOwnerTargetable

--- src/Telegram.Bots/Requests/IChatOwnerTargetable.cs
+++ src/Telegram.Bots/Requests/IChatOwnerTargetable.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-namespace Telegram.Bots.Requests
+namespace Telegram.Bots.Requests;
+
+public interface IChatOwnerTargetable<out TChatId> : IChatTargetable<TChatId>
 {
-  public interface IChatOwnerTargetable<out TChatId> : IChatTargetable<TChatId>
-  {
-    long SenderChatId { get; }
-  }
+  long SenderChatId { get; }
 }
