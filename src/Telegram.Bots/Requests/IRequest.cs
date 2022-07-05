9d2e8e715d80c2d49acf35a6a67b0d6f9de1ac6e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-05 20:43:27 +0530

    Refactor IRequest

--- src/Telegram.Bots/Requests/IRequest.cs
+++ src/Telegram.Bots/Requests/IRequest.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Requests
+namespace Telegram.Bots.Requests;
+
+public interface IRequest<TResult>
 {
-  public interface IRequest<TResult>
-  {
-    string Method { get; }
-  }
+  string Method { get; }
 }
