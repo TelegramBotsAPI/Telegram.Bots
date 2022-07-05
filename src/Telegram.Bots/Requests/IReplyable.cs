4f4ba766b5d5f12552621be53222af5704fd5974
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-05 20:43:19 +0530

    Refactor IReplyable

--- src/Telegram.Bots/Requests/IReplyable.cs
+++ src/Telegram.Bots/Requests/IReplyable.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Requests
+namespace Telegram.Bots.Requests;
+
+public interface IReplyable
 {
-  public interface IReplyable
-  {
-    int? ReplyToMessageId { get; init; }
+  int? ReplyToMessageId { get; init; }
 
-    bool? AllowSendingWithoutReply { get; init; }
-  }
+  bool? AllowSendingWithoutReply { get; init; }
 }
