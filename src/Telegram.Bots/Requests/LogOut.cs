018f01dbc2811a12556d7171a480c67ff1d3eb28
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-05 20:43:58 +0530

    Refactor LogOut

--- src/Telegram.Bots/Requests/LogOut.cs
+++ src/Telegram.Bots/Requests/LogOut.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Requests
+namespace Telegram.Bots.Requests;
+
+public sealed record LogOut : IRequest<bool>
 {
-  public sealed record LogOut : IRequest<bool>
-  {
-    public string Method { get; } = "logOut";
-  }
+  public string Method => "logOut";
 }
