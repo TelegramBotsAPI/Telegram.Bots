1535c1687a7722279e836b4b57e3cb29f39a65f8
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-07 00:06:33 +0530

    Refactor SendLocation

--- src/Telegram.Bots/Requests/SendLocation.cs
+++ src/Telegram.Bots/Requests/SendLocation.cs
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-using Telegram.Bots.Types;
-
 namespace Telegram.Bots.Requests
 {
-  public abstract record SendLocation<TChatId> : IRequest<LocationMessage>,
-    IChatTargetable<TChatId>, INotifiable, IProtectable, IReplyable, IMarkupable
+  using Types;
+
+  public abstract record SendLocation<TChatId>(
+    TChatId ChatId,
+    double Latitude,
+    double Longitude) : IRequest<LocationMessage>, IChatTargetable<TChatId>,
+    INotifiable, IProtectable, IReplyable, IMarkupable
   {
-    public TChatId ChatId { get; }
-
-    public double Latitude { get; }
-
-    public double Longitude { get; }
-
     public double? HorizontalAccuracy { get; init; }
 
     public int? LivePeriod { get; init; }
@@ -23,7 +20,7 @@ namespace Telegram.Bots.Requests
     public uint? ProximityAlertRadius { get; init; }
 
     public bool? DisableNotification { get; init; }
-    
+
     public bool? ProtectContent { get; init; }
 
     public int? ReplyToMessageId { get; init; }
@@ -32,28 +29,19 @@ namespace Telegram.Bots.Requests
 
     public ReplyMarkup? ReplyMarkup { get; init; }
 
-    public string Method { get; } = "sendLocation";
-
-    protected SendLocation(TChatId chatId, double latitude, double longitude)
-    {
-      ChatId = chatId;
-      Latitude = latitude;
-      Longitude = longitude;
-    }
+    public string Method => "sendLocation";
   }
 
-  public sealed record SendLocation : SendLocation<long>
-  {
-    public SendLocation(long chatId, double latitude, double longitude) :
-      base(chatId, latitude, longitude) { }
-  }
+  public sealed record SendLocation(
+    long ChatId,
+    double Latitude,
+    double Longitude) : SendLocation<long>(ChatId, Latitude, Longitude);
 
   namespace Usernames
   {
-    public sealed record SendLocation : SendLocation<string>
-    {
-      public SendLocation(string username, double latitude, double longitude) :
-        base(username, latitude, longitude) { }
-    }
+    public sealed record SendLocation(
+      string ChatId,
+      double Latitude,
+      double Longitude) : SendLocation<string>(ChatId, Latitude, Longitude);
   }
 }
