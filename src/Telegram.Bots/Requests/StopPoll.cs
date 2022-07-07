d4aabdcfc2c4c8eb758d6b05696d8b23685e4151
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-07 18:06:59 +0530

    Refactor StopPoll

--- src/Telegram.Bots/Requests/StopPoll.cs
+++ src/Telegram.Bots/Requests/StopPoll.cs
@@ -1,40 +1,29 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
-
-using Telegram.Bots.Types;
+// Copyright © 2020-2022 Aman Agnihotri
 
 namespace Telegram.Bots.Requests
 {
+  using Types;
+
   public abstract record StopPollBase : IRequest<PollMessage>, IInlineMarkupable
   {
     public InlineKeyboardMarkup? ReplyMarkup { get; init; }
 
-    public string Method { get; } = "stopPoll";
+    public string Method => "stopPoll";
   }
 
-  public abstract record StopPoll<TChatId> : StopPollBase, IChatMessageTargetable<TChatId>
-  {
-    public TChatId ChatId { get; }
+  public abstract record StopPoll<TChatId>(
+    TChatId ChatId,
+    int MessageId) : StopPollBase, IChatMessageTargetable<TChatId>;
 
-    public int MessageId { get; }
-
-    protected StopPoll(TChatId chatId, int messageId)
-    {
-      ChatId = chatId;
-      MessageId = messageId;
-    }
-  }
-
-  public sealed record StopPoll : StopPoll<long>
-  {
-    public StopPoll(long chatId, int messageId) : base(chatId, messageId) { }
-  }
+  public sealed record StopPoll(
+    long ChatId,
+    int MessageId) : StopPoll<long>(ChatId, MessageId);
 
   namespace Usernames
   {
-    public sealed record StopPoll : StopPoll<string>
-    {
-      public StopPoll(string username, int messageId) : base(username, messageId) { }
-    }
+    public sealed record StopPoll(
+      string ChatId,
+      int MessageId) : StopPoll<string>(ChatId, MessageId);
   }
 }
