ade5f48fd543ee371034c279d68756a34143052c
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-07 18:06:45 +0530

    Refactor SetMyDefaultAdminRights

--- src/Telegram.Bots/Requests/SetMyDefaultAdminRights.cs
+++ src/Telegram.Bots/Requests/SetMyDefaultAdminRights.cs
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-using Telegram.Bots.Types;
+namespace Telegram.Bots.Requests;
 
-namespace Telegram.Bots.Requests
+using Types;
+
+public sealed record SetMyDefaultAdminRights : IRequest<bool>
 {
-  public sealed record SetMyDefaultAdminRights : IRequest<bool>
-  {
-    public ChatAdminRights? Rights { get; init; }
+  public ChatAdminRights? Rights { get; init; }
 
-    public bool? ForChannels { get; init; }
+  public bool? ForChannels { get; init; }
 
-    public string Method { get; } = "setMyDefaultAdministratorRights";
-  }
+  public string Method => "setMyDefaultAdministratorRights";
 }
