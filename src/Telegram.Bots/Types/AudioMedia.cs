4a05122b08ce96cdade920ba5eca26fc940764be
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-09 21:36:03 +0530

    Refactor AudioMedia

--- src/Telegram.Bots/Types/AudioMedia.cs
+++ src/Telegram.Bots/Types/AudioMedia.cs
@@ -1,42 +1,47 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public abstract record AudioMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
 {
-  public abstract record AudioMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
+  public override InputMediaType Type => InputMediaType.Audio;
+
+  protected AudioMedia(TMedia media) : base(media) { }
+}
+
+public sealed record CachedAudio : AudioMedia<string>
+{
+  public CachedAudio(string fileId) : base(fileId) { }
+}
+
+public sealed record AudioUrl : AudioMedia<Uri>
+{
+  public AudioUrl(Uri url) : base(url) { }
+}
+
+public sealed record AudioFile : AudioMedia<InputFile>, IUploadableMedia
+{
+  public int? Duration { get; init; }
+
+  public string? Performer { get; init; }
+
+  public string? Title { get; init; }
+
+  public InputFile? Thumb { get; init; }
+
+  public bool? DisableContentTypeDetection { get; init; }
+
+  public AudioFile(InputFile file) : base(file) { }
+
+  public IEnumerable<InputFile?> GetFiles()
   {
-    public override InputMediaType Type { get; } = InputMediaType.Audio;
-
-    protected AudioMedia(TMedia media) : base(media) { }
-  }
-
-  public sealed record CachedAudio : AudioMedia<string>
-  {
-    public CachedAudio(string fileId) : base(fileId) { }
-  }
-
-  public sealed record AudioUrl : AudioMedia<Uri>
-  {
-    public AudioUrl(Uri url) : base(url) { }
-  }
-
-  public sealed record AudioFile : AudioMedia<InputFile>, IUploadableMedia
-  {
-    public int? Duration { get; init; }
-
-    public string? Performer { get; init; }
-
-    public string? Title { get; init; }
-
-    public InputFile? Thumb { get; init; }
-
-    public bool? DisableContentTypeDetection { get; init; }
-
-    public AudioFile(InputFile file) : base(file) { }
-
-    public IEnumerable<InputFile?> GetFiles() => new[] {Media, Thumb};
+    return new[]
+    {
+      Media, Thumb
+    };
   }
 }
