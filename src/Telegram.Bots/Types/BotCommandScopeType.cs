4d5838d2d0fa3a7e6d167a60c5e38d5846aa9644
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-09 21:38:26 +0530

    Refactor BotCommandScopeType

--- src/Telegram.Bots/Types/BotCommandScopeType.cs
+++ src/Telegram.Bots/Types/BotCommandScopeType.cs
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2021 Aman Agnihotri
+// Copyright © 2021-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public enum BotCommandScopeType
 {
-  public enum BotCommandScopeType
-  {
-    Default,
-    AllPrivateChats,
-    AllGroupChats,
-    AllChatAdministrators,
-    Chat,
-    ChatAdministrators,
-    ChatMember
-  }
+  Default,
+  AllPrivateChats,
+  AllGroupChats,
+  AllChatAdministrators,
+  Chat,
+  ChatAdministrators,
+  ChatMember
 }
