390217822eb9b5495ad00fbd649721c483dd58cc
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-09 21:41:33 +0530

    Refactor CallbackQuery

--- src/Telegram.Bots/Types/CallbackQuery.cs
+++ src/Telegram.Bots/Types/CallbackQuery.cs
@@ -1,42 +1,41 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public abstract record CallbackQuery
 {
-  public abstract record CallbackQuery
-  {
-    public string Id { get; init; } = null!;
+  public string Id { get; init; } = null!;
 
-    public User From { get; init; } = null!;
+  public User From { get; init; } = null!;
 
-    public string ChatInstance { get; init; } = null!;
-  }
+  public string ChatInstance { get; init; } = null!;
+}
 
-  public sealed record MessageCallbackQuery : CallbackQuery
-  {
-    public Message Message { get; init; } = null!;
+public sealed record MessageCallbackQuery : CallbackQuery
+{
+  public Message Message { get; init; } = null!;
 
-    public string Data { get; init; } = null!;
-  }
+  public string Data { get; init; } = null!;
+}
 
-  public sealed record GameCallbackQuery : CallbackQuery
-  {
-    public Message Message { get; init; } = null!;
+public sealed record GameCallbackQuery : CallbackQuery
+{
+  public Message Message { get; init; } = null!;
 
-    public string ShortName { get; init; } = null!;
-  }
+  public string ShortName { get; init; } = null!;
+}
 
-  public sealed record InlineMessageCallbackQuery : CallbackQuery
-  {
-    public string MessageId { get; init; } = null!;
+public sealed record InlineMessageCallbackQuery : CallbackQuery
+{
+  public string MessageId { get; init; } = null!;
 
-    public string Data { get; init; } = null!;
-  }
+  public string Data { get; init; } = null!;
+}
 
-  public sealed record InlineGameCallbackQuery : CallbackQuery
-  {
-    public string MessageId { get; init; } = null!;
+public sealed record InlineGameCallbackQuery : CallbackQuery
+{
+  public string MessageId { get; init; } = null!;
 
-    public string ShortName { get; init; } = null!;
-  }
+  public string ShortName { get; init; } = null!;
 }
