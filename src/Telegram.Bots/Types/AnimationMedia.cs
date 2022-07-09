e7eda176de0dc45ccfef109bc4d9d515c57bad3f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-09 21:35:31 +0530

    Refactor AnimationMedia

--- src/Telegram.Bots/Types/AnimationMedia.cs
+++ src/Telegram.Bots/Types/AnimationMedia.cs
@@ -1,42 +1,47 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public abstract record AnimationMedia<TMedia> : InputMedia<TMedia>
 {
-  public abstract record AnimationMedia<TMedia> : InputMedia<TMedia>
+  public override InputMediaType Type => InputMediaType.Animation;
+
+  protected AnimationMedia(TMedia media) : base(media) { }
+}
+
+public sealed record CachedAnimation : AnimationMedia<string>
+{
+  public CachedAnimation(string fileId) : base(fileId) { }
+}
+
+public sealed record AnimationUrl : AnimationMedia<Uri>
+{
+  public AnimationUrl(Uri url) : base(url) { }
+}
+
+public sealed record AnimationFile : AnimationMedia<InputFile>, IUploadableMedia
+{
+  public int? Duration { get; init; }
+
+  public int? Width { get; init; }
+
+  public int? Height { get; init; }
+
+  public InputFile? Thumb { get; init; }
+
+  public bool? DisableContentTypeDetection { get; init; }
+
+  public AnimationFile(InputFile file) : base(file) { }
+
+  public IEnumerable<InputFile?> GetFiles()
   {
-    public override InputMediaType Type { get; } = InputMediaType.Animation;
-
-    protected AnimationMedia(TMedia media) : base(media) { }
-  }
-
-  public sealed record CachedAnimation : AnimationMedia<string>
-  {
-    public CachedAnimation(string fileId) : base(fileId) { }
-  }
-
-  public sealed record AnimationUrl : AnimationMedia<Uri>
-  {
-    public AnimationUrl(Uri url) : base(url) { }
-  }
-
-  public sealed record AnimationFile : AnimationMedia<InputFile>, IUploadableMedia
-  {
-    public int? Duration { get; init; }
-
-    public int? Width { get; init; }
-
-    public int? Height { get; init; }
-
-    public InputFile? Thumb { get; init; }
-
-    public bool? DisableContentTypeDetection { get; init; }
-
-    public AnimationFile(InputFile file) : base(file) { }
-
-    public IEnumerable<InputFile?> GetFiles() => new[] {Media, Thumb};
+    return new[]
+    {
+      Media, Thumb
+    };
   }
 }
