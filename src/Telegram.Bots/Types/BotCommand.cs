601d6f38f8e0a51aa375809664de899df3f6cc29
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-09 21:36:33 +0530

    Refactor BotCommand

--- src/Telegram.Bots/Types/BotCommand.cs
+++ src/Telegram.Bots/Types/BotCommand.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record BotCommand
 {
-  public sealed record BotCommand
-  {
-    public string Command { get; init; } = null!;
+  public string Command { get; init; } = null!;
 
-    public string Description { get; init; } = null!;
-  }
+  public string Description { get; init; } = null!;
 }
