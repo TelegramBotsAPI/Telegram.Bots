0d55247d10c4ba4358aa91764b2f95411f187a80
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-10 22:12:41 +0530

    Refactor ChatPermissions

--- src/Telegram.Bots/Types/ChatPermissions.cs
+++ src/Telegram.Bots/Types/ChatPermissions.cs
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record ChatPermissions
 {
-  public sealed record ChatPermissions
-  {
-    public bool? CanSendMessages { get; init; }
+  public bool? CanSendMessages { get; init; }
 
-    public bool? CanSendMediaMessages { get; init; }
+  public bool? CanSendMediaMessages { get; init; }
 
-    public bool? CanSendPolls { get; init; }
+  public bool? CanSendPolls { get; init; }
 
-    public bool? CanSendOtherMessages { get; init; }
+  public bool? CanSendOtherMessages { get; init; }
 
-    public bool? CanAddWebPagePreviews { get; init; }
+  public bool? CanAddWebPagePreviews { get; init; }
 
-    public bool? CanChangeInfo { get; init; }
+  public bool? CanChangeInfo { get; init; }
 
-    public bool? CanInviteUsers { get; init; }
+  public bool? CanInviteUsers { get; init; }
 
-    public bool? CanPinMessages { get; init; }
-  }
+  public bool? CanPinMessages { get; init; }
 }
