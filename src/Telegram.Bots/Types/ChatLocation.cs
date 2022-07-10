dc925d36db84910018525c1b07d146aaaa43b144
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-10 22:11:19 +0530

    Refactor ChatLocation

--- src/Telegram.Bots/Types/ChatLocation.cs
+++ src/Telegram.Bots/Types/ChatLocation.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record ChatLocation
 {
-  public sealed record ChatLocation
-  {
-    public Location Location { get; init; } = null!;
+  public Location Location { get; init; } = null!;
 
-    public string Address { get; init; } = null!;
-  }
+  public string Address { get; init; } = null!;
 }
