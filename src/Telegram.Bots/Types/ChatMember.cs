15bfc9dc55d363998a49c5e1ad44fde36862fc75
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-10 22:12:04 +0530

    Refactor ChatMember

--- src/Telegram.Bots/Types/ChatMember.cs
+++ src/Telegram.Bots/Types/ChatMember.cs
@@ -1,97 +1,95 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Types;
+
 using System;
+using Status = ChatMemberStatus;
 
-namespace Telegram.Bots.Types
+public abstract record ChatMember
 {
-  using Status = ChatMemberStatus;
+  public User User { get; init; } = null!;
 
-  public abstract record ChatMember
-  {
-    public User User { get; init; } = null!;
+  public abstract Status Status { get; }
+}
 
-    public abstract Status Status { get; }
-  }
+public abstract record PrivilegedMember : ChatMember
+{
+  public string? CustomTitle { get; init; }
 
-  public abstract record PrivilegedMember : ChatMember
-  {
-    public string? CustomTitle { get; init; }
+  public bool IsAnonymous { get; init; }
+}
 
-    public bool IsAnonymous { get; init; }
-  }
+public sealed record Creator : PrivilegedMember
+{
+  public override Status Status { get; } = Status.Creator;
+}
 
-  public sealed record Creator : PrivilegedMember
-  {
-    public override Status Status { get; } = Status.Creator;
-  }
+public sealed record Administrator : PrivilegedMember
+{
+  public override Status Status { get; } = Status.Administrator;
 
-  public sealed record Administrator : PrivilegedMember
-  {
-    public override Status Status { get; } = Status.Administrator;
+  public bool CanBeEdited { get; init; }
 
-    public bool CanBeEdited { get; init; }
+  public bool CanManageChat { get; init; }
 
-    public bool CanManageChat { get; init; }
+  public bool CanPostMessages { get; init; }
 
-    public bool CanPostMessages { get; init; }
+  public bool CanEditMessages { get; init; }
 
-    public bool CanEditMessages { get; init; }
+  public bool CanDeleteMessages { get; init; }
 
-    public bool CanDeleteMessages { get; init; }
+  public bool CanManageVideoChats { get; init; }
 
-    public bool CanManageVideoChats { get; init; }
+  public bool CanRestrictMembers { get; init; }
 
-    public bool CanRestrictMembers { get; init; }
+  public bool CanPromoteMembers { get; init; }
 
-    public bool CanPromoteMembers { get; init; }
+  public bool CanChangeInfo { get; init; }
 
-    public bool CanChangeInfo { get; init; }
+  public bool CanInviteUsers { get; init; }
 
-    public bool CanInviteUsers { get; init; }
+  public bool CanPinMessages { get; init; }
+}
 
-    public bool CanPinMessages { get; init; }
-  }
+public sealed record NormalMember : ChatMember
+{
+  public override Status Status { get; } = Status.Member;
+}
 
-  public sealed record NormalMember : ChatMember
-  {
-    public override Status Status { get; } = Status.Member;
-  }
+public sealed record RestrictedMember : ChatMember
+{
+  public override Status Status { get; } = Status.Restricted;
 
-  public sealed record RestrictedMember : ChatMember
-  {
-    public override Status Status { get; } = Status.Restricted;
+  public DateTime UntilDate { get; init; }
 
-    public DateTime UntilDate { get; init; }
+  public bool CanChangeInfo { get; init; }
 
-    public bool CanChangeInfo { get; init; }
+  public bool CanInviteUsers { get; init; }
 
-    public bool CanInviteUsers { get; init; }
+  public bool CanPinMessages { get; init; }
 
-    public bool CanPinMessages { get; init; }
+  public bool IsMember { get; init; }
 
-    public bool IsMember { get; init; }
+  public bool CanSendMessages { get; init; }
 
-    public bool CanSendMessages { get; init; }
+  public bool CanSendMediaMessages { get; init; }
 
-    public bool CanSendMediaMessages { get; init; }
+  public bool CanSendPolls { get; init; }
 
-    public bool CanSendPolls { get; init; }
+  public bool CanSendOtherMessages { get; init; }
 
-    public bool CanSendOtherMessages { get; init; }
+  public bool CanAddWebPagePreviews { get; init; }
+}
 
-    public bool CanAddWebPagePreviews { get; init; }
-  }
+public sealed record KickedMember : ChatMember
+{
+  public override Status Status { get; } = Status.Kicked;
 
-  public sealed record KickedMember : ChatMember
-  {
-    public override Status Status { get; } = Status.Kicked;
+  public DateTime UntilDate { get; init; }
+}
 
-    public DateTime UntilDate { get; init; }
-  }
-
-  public sealed record LeftMember : ChatMember
-  {
-    public override Status Status { get; } = Status.Left;
-  }
+public sealed record LeftMember : ChatMember
+{
+  public override Status Status { get; } = Status.Left;
 }
