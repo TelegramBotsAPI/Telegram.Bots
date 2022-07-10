5a348e9f280fca79303e2f7dabc99bb356cea3e5
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-10 22:05:44 +0530

    Refactor Chat

--- src/Telegram.Bots/Types/Chat.cs
+++ src/Telegram.Bots/Types/Chat.cs
@@ -1,49 +1,48 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public record Chat
 {
-  public record Chat
-  {
-    public long Id { get; init; }
+  public long Id { get; init; }
 
-    public ChatType Type { get; init; }
+  public ChatType Type { get; init; }
 
-    public string? Title { get; init; }
+  public string? Title { get; init; }
 
-    public string? Username { get; init; }
+  public string? Username { get; init; }
 
-    public string? FirstName { get; init; }
+  public string? FirstName { get; init; }
 
-    public string? LastName { get; init; }
-  }
+  public string? LastName { get; init; }
+}
 
-  public sealed record ChatInfo : Chat
-  {
-    public ChatPhoto? Photo { get; init; }
+public sealed record ChatInfo : Chat
+{
+  public ChatPhoto? Photo { get; init; }
 
-    public string? Bio { get; init; }
+  public string? Bio { get; init; }
 
-    public string? Description { get; init; }
+  public string? Description { get; init; }
 
-    public string? InviteLink { get; init; }
+  public string? InviteLink { get; init; }
 
-    public Message? Pinned { get; init; }
+  public Message? Pinned { get; init; }
 
-    public ChatPermissions? Permissions { get; init; }
+  public ChatPermissions? Permissions { get; init; }
 
-    public int SlowModeDelay { get; init; }
-    
-    public bool HasPrivateForwards { get; init; }
-    
-    public bool HasProtectedContent { get; init; }
+  public int SlowModeDelay { get; init; }
 
-    public string? StickerSetName { get; init; }
+  public bool HasPrivateForwards { get; init; }
 
-    public bool CanSetStickerSet { get; init; }
+  public bool HasProtectedContent { get; init; }
 
-    public long? LinkedChatId { get; init; }
+  public string? StickerSetName { get; init; }
 
-    public ChatLocation? Location { get; init; }
-  }
+  public bool CanSetStickerSet { get; init; }
+
+  public long? LinkedChatId { get; init; }
+
+  public ChatLocation? Location { get; init; }
 }
