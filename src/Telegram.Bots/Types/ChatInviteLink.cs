281d8e2deeb37d4a1d70c3c647bab89365aec2c7
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-10 22:10:35 +0530

    Refactor ChatInviteLink

--- src/Telegram.Bots/Types/ChatInviteLink.cs
+++ src/Telegram.Bots/Types/ChatInviteLink.cs
@@ -1,28 +1,27 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2021 Aman Agnihotri
+// Copyright © 2021-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 
-namespace Telegram.Bots.Types
+public sealed record ChatInviteLink
 {
-  public sealed record ChatInviteLink
-  {
-    public string InviteLink { get; init; } = null!;
+  public string InviteLink { get; init; } = null!;
 
-    public User Creator { get; init; } = null!;
-    
-    public bool CreatesJoinRequest { get; init; }
+  public User Creator { get; init; } = null!;
 
-    public bool IsPrimary { get; init; }
+  public bool CreatesJoinRequest { get; init; }
 
-    public bool IsRevoked { get; init; }
-    
-    public string? Name { get; init; }
+  public bool IsPrimary { get; init; }
 
-    public DateTime? ExpireDate { get; init; }
+  public bool IsRevoked { get; init; }
 
-    public int? MemberLimit { get; init; }
-    
-    public int? PendingJoinRequestCount { get; init; }
-  }
+  public string? Name { get; init; }
+
+  public DateTime? ExpireDate { get; init; }
+
+  public int? MemberLimit { get; init; }
+
+  public int? PendingJoinRequestCount { get; init; }
 }
