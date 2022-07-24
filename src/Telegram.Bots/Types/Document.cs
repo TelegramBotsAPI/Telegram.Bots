808ffe9b27ea0fbf5751e9a162f2083eb3c692b6
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-24 15:29:03 +0530

    Refactor Document

--- src/Telegram.Bots/Types/Document.cs
+++ src/Telegram.Bots/Types/Document.cs
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record Document : File
 {
-  public sealed record Document : File
-  {
-    public Photo? Thumb { get; init; }
+  public Photo? Thumb { get; init; }
 
-    public string? Name { get; init; }
+  public string? Name { get; init; }
 
-    public string? MimeType { get; init; }
-  }
+  public string? MimeType { get; init; }
 }
