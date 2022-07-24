d76b3bf80ff3526d4945496a6c7b2ffbebbbae81
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-24 15:29:29 +0530

    Refactor DocumentMedia

--- src/Telegram.Bots/Types/DocumentMedia.cs
+++ src/Telegram.Bots/Types/DocumentMedia.cs
@@ -1,36 +1,42 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public abstract record DocumentMedia<TMedia> : InputMedia<TMedia>,
+  IGroupableMedia
 {
-  public abstract record DocumentMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
+  public override InputMediaType Type => InputMediaType.Document;
+
+  protected DocumentMedia(TMedia media) : base(media) { }
+}
+
+public sealed record CachedDocument : DocumentMedia<string>
+{
+  public CachedDocument(string fileId) : base(fileId) { }
+}
+
+public sealed record DocumentUrl : DocumentMedia<Uri>
+{
+  public DocumentUrl(Uri url) : base(url) { }
+}
+
+public sealed record DocumentFile : DocumentMedia<InputFile>, IUploadableMedia
+{
+  public InputFile? Thumb { get; init; }
+
+  public bool? DisableContentTypeDetection { get; init; }
+
+  public DocumentFile(InputFile file) : base(file) { }
+
+  public IEnumerable<InputFile?> GetFiles()
   {
-    public override InputMediaType Type { get; } = InputMediaType.Document;
-
-    protected DocumentMedia(TMedia media) : base(media) { }
-  }
-
-  public sealed record CachedDocument : DocumentMedia<string>
-  {
-    public CachedDocument(string fileId) : base(fileId) { }
-  }
-
-  public sealed record DocumentUrl : DocumentMedia<Uri>
-  {
-    public DocumentUrl(Uri url) : base(url) { }
-  }
-
-  public sealed record DocumentFile : DocumentMedia<InputFile>, IUploadableMedia
-  {
-    public InputFile? Thumb { get; init; }
-
-    public bool? DisableContentTypeDetection { get; init; }
-
-    public DocumentFile(InputFile file) : base(file) { }
-
-    public IEnumerable<InputFile?> GetFiles() => new[] {Media, Thumb};
+    return new[]
+    {
+      Media, Thumb
+    };
   }
 }
