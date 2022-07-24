7f71e41380ac4029e742092069b3758a5bf1f4ff
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-07-24 15:21:58 +0530

    Refactor ChatType

--- src/Telegram.Bots/Types/ChatType.cs
+++ src/Telegram.Bots/Types/ChatType.cs
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public enum ChatType
 {
-  public enum ChatType
-  {
-    Sender,
-    Private,
-    Group,
-    Supergroup,
-    Channel
-  }
+  Sender,
+  Private,
+  Group,
+  Supergroup,
+  Channel
 }
