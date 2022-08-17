7ce03c8cee0dfc0a268618d906711c1caf548a25
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-17 22:24:59 +0530

    Refactor InputMedia

--- src/Telegram.Bots/Types/InputMedia.cs
+++ src/Telegram.Bots/Types/InputMedia.cs
@@ -1,36 +1,31 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public interface IInputMedia
 {
-  public interface IInputMedia
-  {
-    InputMediaType Type { get; }
-  }
+  InputMediaType Type { get; }
+}
 
-  public interface IGroupableMedia : IInputMedia { }
+public interface IGroupableMedia : IInputMedia { }
 
-  public interface IUploadableMedia : IInputMedia
-  {
-    bool? DisableContentTypeDetection { get; init; }
+public interface IUploadableMedia : IInputMedia
+{
+  bool? DisableContentTypeDetection { get; init; }
 
-    IEnumerable<InputFile?> GetFiles();
-  }
+  IEnumerable<InputFile?> GetFiles();
+}
 
-  public abstract record InputMedia<TMedia> : IInputMedia
-  {
-    public abstract InputMediaType Type { get; }
+public abstract record InputMedia<TMedia>(TMedia Media) : IInputMedia
+{
+  public abstract InputMediaType Type { get; }
 
-    public TMedia Media { get; }
+  public string? Caption { get; init; }
 
-    public string? Caption { get; init; }
+  public ParseMode? ParseMode { get; init; }
 
-    public ParseMode? ParseMode { get; init; }
-
-    public IEnumerable<MessageEntity>? CaptionEntities { get; init; }
-
-    protected InputMedia(TMedia media) => Media = media;
-  }
+  public IEnumerable<MessageEntity>? CaptionEntities { get; init; }
 }
