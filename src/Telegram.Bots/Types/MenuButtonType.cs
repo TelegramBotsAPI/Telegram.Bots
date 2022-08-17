230b9e4f0b4cc49c08096a3431d8afafbeb38ed2
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-17 22:28:15 +0530

    Refactor MenuButtonType

--- src/Telegram.Bots/Types/MenuButtonType.cs
+++ src/Telegram.Bots/Types/MenuButtonType.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public enum MenuButtonType
 {
-  public enum MenuButtonType
-  {
-    Default,
-    Commands,
-    WebApp
-  }
+  Default,
+  Commands,
+  WebApp
 }
