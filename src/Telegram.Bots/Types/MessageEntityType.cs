56f4ba643a82324fe0c9a216766c7518aaaba0bd
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-17 22:32:18 +0530

    Refactor MessageEntityType

--- src/Telegram.Bots/Types/MessageEntityType.cs
+++ src/Telegram.Bots/Types/MessageEntityType.cs
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public enum MessageEntityType
 {
-  public enum MessageEntityType
-  {
-    Bold,
-    BotCommand,
-    Cashtag,
-    Code,
-    Email,
-    Hashtag,
-    Italic,
-    Mention,
-    PhoneNumber,
-    Pre,
-    Strikethrough,
-    Spoiler,
-    TextLink,
-    TextMention,
-    Underline,
-    Url
-  }
+  Bold,
+  BotCommand,
+  Cashtag,
+  Code,
+  Email,
+  Hashtag,
+  Italic,
+  Mention,
+  PhoneNumber,
+  Pre,
+  Strikethrough,
+  Spoiler,
+  TextLink,
+  TextMention,
+  Underline,
+  Url
 }
