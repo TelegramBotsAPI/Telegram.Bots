1188a46e535254b4cd5331661ede8cb4742291c3
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:35:51 +0530

    Refactor ReplyMarkup

--- src/Telegram.Bots/Types/ReplyMarkup.cs
+++ src/Telegram.Bots/Types/ReplyMarkup.cs
@@ -1,50 +1,39 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using System.Collections.Generic;
+namespace Telegram.Bots.Types;
 
-namespace Telegram.Bots.Types
+using Keyboard = System.Collections.Generic.IEnumerable<Button[]>;
+using InlineKeyboard = System.Collections.Generic.IEnumerable<InlineButton[]>;
+
+public abstract record ReplyMarkup;
+
+public sealed record KeyboardMarkup(Keyboard Keyboard) : ReplyMarkup
 {
-  using Keyboard = IEnumerable<Button[]>;
-  using InlineKeyboard = IEnumerable<InlineButton[]>;
+  public bool? Resize { get; init; }
 
-  public abstract record ReplyMarkup { }
+  public bool? OneTime { get; init; }
 
-  public sealed record KeyboardMarkup : ReplyMarkup
-  {
-    public Keyboard Keyboard { get; }
+  public string? InputFieldPlaceholder { get; init; }
 
-    public bool? Resize { get; init; }
+  public bool? Selective { get; init; }
+}
 
-    public bool? OneTime { get; init; }
+public sealed record InlineKeyboardMarkup(
+  InlineKeyboard Keyboard) : ReplyMarkup;
 
-    public string? InputFieldPlaceholder { get; init; }
+public sealed record ForceReplyMarkup : ReplyMarkup
+{
+  public bool ForceReply => true;
 
-    public bool? Selective { get; init; }
+  public string? InputFieldPlaceholder { get; init; }
 
-    public KeyboardMarkup(Keyboard keyboard) => Keyboard = keyboard;
-  }
+  public bool? Selective { get; init; }
+}
 
-  public sealed record InlineKeyboardMarkup : ReplyMarkup
-  {
-    public InlineKeyboard Keyboard { get; }
+public sealed record RemoveMarkup : ReplyMarkup
+{
+  public bool RemoveKeyboard => true;
 
-    public InlineKeyboardMarkup(InlineKeyboard keyboard) => Keyboard = keyboard;
-  }
-
-  public sealed record ForceReplyMarkup : ReplyMarkup
-  {
-    public bool ForceReply { get; } = true;
-
-    public string? InputFieldPlaceholder { get; init; }
-
-    public bool? Selective { get; init; }
-  }
-
-  public sealed record RemoveMarkup : ReplyMarkup
-  {
-    public bool RemoveKeyboard { get; } = true;
-
-    public bool? Selective { get; init; }
-  }
+  public bool? Selective { get; init; }
 }
