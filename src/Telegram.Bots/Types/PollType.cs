47474f445e8dbb43975b5965e171813b76b54f1a
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:34:22 +0530

    Refactor PollType

--- src/Telegram.Bots/Types/PollType.cs
+++ src/Telegram.Bots/Types/PollType.cs
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public enum PollType
 {
-  public enum PollType
-  {
-    Regular,
-    Quiz
-  }
+  Regular,
+  Quiz
 }
