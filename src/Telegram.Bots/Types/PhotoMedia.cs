93891d50ba6a59aacc23fc20a363bb327cd6ba47
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:32:30 +0530

    Refactor PhotoMedia

--- src/Telegram.Bots/Types/PhotoMedia.cs
+++ src/Telegram.Bots/Types/PhotoMedia.cs
@@ -1,34 +1,39 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public abstract record PhotoMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
 {
-  public abstract record PhotoMedia<TMedia> : InputMedia<TMedia>, IGroupableMedia
+  public override InputMediaType Type => InputMediaType.Photo;
+
+  protected PhotoMedia(TMedia media) : base(media) { }
+}
+
+public sealed record CachedPhoto : PhotoMedia<string>
+{
+  public CachedPhoto(string fileId) : base(fileId) { }
+}
+
+public sealed record PhotoUrl : PhotoMedia<Uri>
+{
+  public PhotoUrl(Uri url) : base(url) { }
+}
+
+public sealed record PhotoFile : PhotoMedia<InputFile>, IUploadableMedia
+{
+  public PhotoFile(InputFile file) : base(file) { }
+
+  public bool? DisableContentTypeDetection { get; init; }
+
+  public IEnumerable<InputFile?> GetFiles()
   {
-    public override InputMediaType Type { get; } = InputMediaType.Photo;
-
-    protected PhotoMedia(TMedia media) : base(media) { }
-  }
-
-  public sealed record CachedPhoto : PhotoMedia<string>
-  {
-    public CachedPhoto(string fileId) : base(fileId) { }
-  }
-
-  public sealed record PhotoUrl : PhotoMedia<Uri>
-  {
-    public PhotoUrl(Uri url) : base(url) { }
-  }
-
-  public sealed record PhotoFile : PhotoMedia<InputFile>, IUploadableMedia
-  {
-    public PhotoFile(InputFile file) : base(file) { }
-
-    public bool? DisableContentTypeDetection { get; init; }
-
-    public IEnumerable<InputFile?> GetFiles() => new[] {Media};
+    return new[]
+    {
+      Media
+    };
   }
 }
