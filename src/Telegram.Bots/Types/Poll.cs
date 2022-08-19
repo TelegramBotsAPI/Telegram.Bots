817d319301f09fbdfca3941a0d2f4485fb87d954
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:33:02 +0530

    Refactor Poll

--- src/Telegram.Bots/Types/Poll.cs
+++ src/Telegram.Bots/Types/Poll.cs
@@ -1,47 +1,46 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types
+public abstract record Poll
 {
-  public abstract record Poll
-  {
-    public string Id { get; init; } = null!;
+  public string Id { get; init; } = null!;
 
-    public string Question { get; init; } = null!;
+  public string Question { get; init; } = null!;
 
-    public IReadOnlyList<PollOption> Options { get; init; } = null!;
+  public IReadOnlyList<PollOption> Options { get; init; } = null!;
 
-    public int TotalVoterCount { get; init; }
+  public int TotalVoterCount { get; init; }
 
-    public bool IsClosed { get; init; }
+  public bool IsClosed { get; init; }
 
-    public bool IsAnonymous { get; init; }
+  public bool IsAnonymous { get; init; }
 
-    public abstract PollType? Type { get; }
+  public abstract PollType? Type { get; }
 
-    public int? OpenPeriod { get; init; }
+  public int? OpenPeriod { get; init; }
 
-    public DateTime? CloseDate { get; init; }
-  }
+  public DateTime? CloseDate { get; init; }
+}
 
-  public sealed record RegularPoll : Poll
-  {
-    public override PollType? Type { get; } = PollType.Regular;
+public sealed record RegularPoll : Poll
+{
+  public override PollType? Type => PollType.Regular;
 
-    public bool? AllowsMultipleAnswers { get; init; }
-  }
+  public bool? AllowsMultipleAnswers { get; init; }
+}
 
-  public sealed record QuizPoll : Poll
-  {
-    public override PollType? Type { get; } = PollType.Quiz;
+public sealed record QuizPoll : Poll
+{
+  public override PollType? Type => PollType.Quiz;
 
-    public uint? CorrectOptionId { get; init; }
+  public uint? CorrectOptionId { get; init; }
 
-    public string? Explanation { get; init; }
+  public string? Explanation { get; init; }
 
-    public IReadOnlyList<MessageEntity>? ExplanationEntities { get; init; }
-  }
+  public IReadOnlyList<MessageEntity>? ExplanationEntities { get; init; }
 }
