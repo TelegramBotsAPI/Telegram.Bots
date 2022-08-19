83ac95c34167b39c65510f75edf27598860fe26e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:34:40 +0530

    Refactor ProximityAlertTriggered

--- src/Telegram.Bots/Types/ProximityAlertTriggered.cs
+++ src/Telegram.Bots/Types/ProximityAlertTriggered.cs
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record ProximityAlertTriggered
 {
-  public sealed record ProximityAlertTriggered
-  {
-    public User Traveler { get; init; } = null!;
+  public User Traveler { get; init; } = null!;
 
-    public User Watcher { get; init; } = null!;
+  public User Watcher { get; init; } = null!;
 
-    public int Distance { get; init; }
-  }
+  public int Distance { get; init; }
 }
