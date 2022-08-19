0c126774ab90363bfeffcd0a7f0fea683e915dc4
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-19 20:34:11 +0530

    Refactor PollOption

--- src/Telegram.Bots/Types/PollOption.cs
+++ src/Telegram.Bots/Types/PollOption.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record PollOption
 {
-  public sealed record PollOption
-  {
-    public string Text { get; init; } = null!;
+  public string Text { get; init; } = null!;
 
-    public uint VoterCount { get; init; }
-  }
+  public uint VoterCount { get; init; }
 }
