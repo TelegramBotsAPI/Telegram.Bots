0528b90fb12970be28152679f459bfcba0f2c07c
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-20 23:21:23 +0530

    Refactor Venue

--- src/Telegram.Bots/Types/Venue.cs
+++ src/Telegram.Bots/Types/Venue.cs
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record Venue
 {
-  public sealed record Venue
-  {
-    public string Title { get; init; } = null!;
+  public string Title { get; init; } = null!;
 
-    public string Address { get; init; } = null!;
+  public string Address { get; init; } = null!;
 
-    public Location Location { get; init; } = null!;
+  public Location Location { get; init; } = null!;
 
-    public string? FoursquareId { get; init; }
+  public string? FoursquareId { get; init; }
 
-    public string? FoursquareType { get; init; }
+  public string? FoursquareType { get; init; }
 
-    public string? GooglePlaceId { get; init; }
+  public string? GooglePlaceId { get; init; }
 
-    public string? GooglePlaceType { get; init; }
-  }
+  public string? GooglePlaceType { get; init; }
 }
