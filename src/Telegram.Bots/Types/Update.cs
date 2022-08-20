2ae908861309a15fbb5a5b8bc819f3b573e9ab64
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-20 23:19:00 +0530

    Refactor Update

--- src/Telegram.Bots/Types/Update.cs
+++ src/Telegram.Bots/Types/Update.cs
@@ -1,83 +1,82 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using Telegram.Bots.Types.Inline;
-using Telegram.Bots.Types.Payments;
+namespace Telegram.Bots.Types;
 
-namespace Telegram.Bots.Types
+using Inline;
+using Payments;
+
+public abstract record Update
 {
-  public abstract record Update
-  {
-    public int Id { get; init; }
-  }
+  public int Id { get; init; }
+}
 
-  public sealed record MessageUpdate : Update
-  {
-    public Message Data { get; init; } = null!;
-  }
+public sealed record MessageUpdate : Update
+{
+  public Message Data { get; init; } = null!;
+}
 
-  public sealed record EditedMessageUpdate : Update
-  {
-    public Message Data { get; init; } = null!;
-  }
+public sealed record EditedMessageUpdate : Update
+{
+  public Message Data { get; init; } = null!;
+}
 
-  public sealed record ChannelPostUpdate : Update
-  {
-    public Message Data { get; init; } = null!;
-  }
+public sealed record ChannelPostUpdate : Update
+{
+  public Message Data { get; init; } = null!;
+}
 
-  public sealed record EditedChannelPostUpdate : Update
-  {
-    public Message Data { get; init; } = null!;
-  }
+public sealed record EditedChannelPostUpdate : Update
+{
+  public Message Data { get; init; } = null!;
+}
 
-  public sealed record InlineQueryUpdate : Update
-  {
-    public InlineQuery Data { get; init; } = null!;
-  }
+public sealed record InlineQueryUpdate : Update
+{
+  public InlineQuery Data { get; init; } = null!;
+}
 
-  public sealed record ChosenInlineResultUpdate : Update
-  {
-    public ChosenInlineResult Data { get; init; } = null!;
-  }
+public sealed record ChosenInlineResultUpdate : Update
+{
+  public ChosenInlineResult Data { get; init; } = null!;
+}
 
-  public sealed record CallbackQueryUpdate : Update
-  {
-    public CallbackQuery Data { get; init; } = null!;
-  }
+public sealed record CallbackQueryUpdate : Update
+{
+  public CallbackQuery Data { get; init; } = null!;
+}
 
-  public sealed record ShippingQueryUpdate : Update
-  {
-    public ShippingQuery Data { get; init; } = null!;
-  }
+public sealed record ShippingQueryUpdate : Update
+{
+  public ShippingQuery Data { get; init; } = null!;
+}
 
-  public sealed record PreCheckoutQueryUpdate : Update
-  {
-    public PreCheckoutQuery Data { get; init; } = null!;
-  }
+public sealed record PreCheckoutQueryUpdate : Update
+{
+  public PreCheckoutQuery Data { get; init; } = null!;
+}
 
-  public sealed record PollUpdate : Update
-  {
-    public Poll Data { get; init; } = null!;
-  }
+public sealed record PollUpdate : Update
+{
+  public Poll Data { get; init; } = null!;
+}
 
-  public sealed record PollAnswerUpdate : Update
-  {
-    public PollAnswer Data { get; init; } = null!;
-  }
+public sealed record PollAnswerUpdate : Update
+{
+  public PollAnswer Data { get; init; } = null!;
+}
 
-  public sealed record MyChatMemberUpdate : Update
-  {
-    public ChatMemberUpdated Data { get; init; } = null!;
-  }
+public sealed record MyChatMemberUpdate : Update
+{
+  public ChatMemberUpdated Data { get; init; } = null!;
+}
 
-  public sealed record ChatMemberUpdate : Update
-  {
-    public ChatMemberUpdated Data { get; init; } = null!;
-  }
-  
-  public sealed record ChatJoinRequestUpdate : Update
-  {
-    public ChatJoinRequest Data { get; init; } = null!;
-  }
+public sealed record ChatMemberUpdate : Update
+{
+  public ChatMemberUpdated Data { get; init; } = null!;
+}
+
+public sealed record ChatJoinRequestUpdate : Update
+{
+  public ChatJoinRequest Data { get; init; } = null!;
 }
