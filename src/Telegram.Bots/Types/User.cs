2fcd4a8e1761830139fdd52b5a21ce83f24a10be
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-20 23:19:44 +0530

    Refactor User

--- src/Telegram.Bots/Types/User.cs
+++ src/Telegram.Bots/Types/User.cs
@@ -1,29 +1,28 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public record User
 {
-  public record User
-  {
-    public long Id { get; init; }
+  public long Id { get; init; }
 
-    public bool IsBot { get; init; }
+  public bool IsBot { get; init; }
 
-    public string FirstName { get; init; } = null!;
+  public string FirstName { get; init; } = null!;
 
-    public string? LastName { get; init; }
+  public string? LastName { get; init; }
 
-    public string? Username { get; init; }
+  public string? Username { get; init; }
 
-    public string? LanguageCode { get; init; }
-  }
+  public string? LanguageCode { get; init; }
+}
 
-  public sealed record MyBot : User
-  {
-    public bool CanJoinGroups { get; init; }
+public sealed record MyBot : User
+{
+  public bool CanJoinGroups { get; init; }
 
-    public bool CanReadAllGroupMessages { get; init; }
+  public bool CanReadAllGroupMessages { get; init; }
 
-    public bool SupportsInlineQueries { get; init; }
-  }
+  public bool SupportsInlineQueries { get; init; }
 }
