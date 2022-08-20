6af8667cbbe4ef45c5e21edb8ae798a4063d4bdd
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-08-20 23:21:47 +0530

    Refactor Video

--- src/Telegram.Bots/Types/Video.cs
+++ src/Telegram.Bots/Types/Video.cs
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record Video : File
 {
-  public sealed record Video : File
-  {
-    public int Width { get; init; }
+  public int Width { get; init; }
 
-    public int Height { get; init; }
+  public int Height { get; init; }
 
-    public int Duration { get; init; }
+  public int Duration { get; init; }
 
-    public Photo? Thumb { get; init; }
+  public Photo? Thumb { get; init; }
 
-    public string? Name { get; init; }
+  public string? Name { get; init; }
 
-    public string? MimeType { get; init; }
-  }
+  public string? MimeType { get; init; }
 }
