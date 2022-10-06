d139e7ae829237ae99f237392ec6038e69520783
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-06 22:26:50 +0530

    Refactor VideoNote

--- src/Telegram.Bots/Types/VideoNote.cs
+++ src/Telegram.Bots/Types/VideoNote.cs
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record VideoNote : File
 {
-  public sealed record VideoNote : File
-  {
-    public int Length { get; init; }
+  public int Length { get; init; }
 
-    public int Duration { get; init; }
+  public int Duration { get; init; }
 
-    public Photo? Thumb { get; init; }
-  }
+  public Photo? Thumb { get; init; }
 }
