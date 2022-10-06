12db25be4e987d5750f2a664eb5fd4fe8f03368a
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-06 22:27:06 +0530

    Refactor Voice

--- src/Telegram.Bots/Types/Voice.cs
+++ src/Telegram.Bots/Types/Voice.cs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
+namespace Telegram.Bots.Types;
+
+public sealed record Voice : File
 {
-  public sealed record Voice : File
-  {
-    public int Duration { get; init; }
+  public int Duration { get; init; }
 
-    public string? MimeType { get; init; }
-  }
+  public string? MimeType { get; init; }
 }
