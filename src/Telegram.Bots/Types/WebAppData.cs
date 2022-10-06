cf13f686009615338a16b3a3e376906674a7355f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-06 22:27:50 +0530

    Refactor WebAppData

--- src/Telegram.Bots/Types/WebAppData.cs
+++ src/Telegram.Bots/Types/WebAppData.cs
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
-{
-  public sealed record WebAppData
-  {
-    public string Data { get; init; } = null!;
-    
-    public string ButtonText { get; init; } = null!;
-  }
-}
+namespace Telegram.Bots.Types;
+
+public sealed record WebAppData(string Data, string ButtonText);
--- test/Telegram.Bots.Tests.Units/Json/ContractResolverTests.cs
+++ test/Telegram.Bots.Tests.Units/Json/ContractResolverTests.cs
@@ -144,7 +144,7 @@ namespace Telegram.Bots.Tests.Units.Json
       (@"""video_chat_ended"":{", new VideoChatEndedMessage {Ended = new(0)}),
       (@"""video_chat_participants_invited"":{",
         new VideoChatParticipantsInvitedMessage {ParticipantsInvited = new(new List<User>())}),
-      (@"""web_app_data"":{", new WebAppDataMessage {Data = new()}),
+      (@"""web_app_data"":{", new WebAppDataMessage {Data = new("", "")}),
       (@"""passport_data"":{", new PassportDataMessage {PassportData = new PassportData()}),
       (@"""proximity_alert_triggered"":{",
         new ProximityAlertTriggeredMessage
