679371358af36fb08e86eaaf878abdd0a92a3bcb
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-06 22:24:03 +0530

    Refactor VideoChatStarted

--- src/Telegram.Bots/Types/VideoChatStarted.cs
+++ src/Telegram.Bots/Types/VideoChatStarted.cs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types
-{
-  public sealed record VideoChatStarted;
-}
+namespace Telegram.Bots.Types;
+
+public sealed record VideoChatStarted;
