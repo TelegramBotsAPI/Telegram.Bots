e45f4d08b3d1ec461f933ed5e589c7612eab7137
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-07 20:03:52 +0530

    Refactor StickerSet

--- src/Telegram.Bots/Types/Stickers/StickerSet.cs
+++ src/Telegram.Bots/Types/Stickers/StickerSet.cs
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Types.Stickers;
+
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types.Stickers
+public sealed record StickerSet
 {
-  public sealed record StickerSet
-  {
-    public string Name { get; init; } = null!;
+  public string Name { get; init; } = null!;
 
-    public string Title { get; init; } = null!;
+  public string Title { get; init; } = null!;
 
-    public bool IsAnimated { get; init; }
-    
-    public bool IsVideo { get; init; }
+  public bool IsAnimated { get; init; }
 
-    public bool ContainsMasks { get; init; }
+  public bool IsVideo { get; init; }
 
-    public IReadOnlyList<Sticker> Stickers { get; init; } = null!;
+  public bool ContainsMasks { get; init; }
 
-    public Photo? Thumb { get; init; }
-  }
+  public IReadOnlyList<Sticker> Stickers { get; init; } = null!;
+
+  public Photo? Thumb { get; init; }
 }
