84d29b395a45c7a11d3f74255a6bb8325063e3df
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-07 20:04:49 +0530

    Refactor CallbackGame

--- src/Telegram.Bots/Types/Games/CallbackGame.cs
+++ src/Telegram.Bots/Types/Games/CallbackGame.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Games
+namespace Telegram.Bots.Types.Games;
+
+public sealed record CallbackGame
 {
-  public sealed record CallbackGame
-  {
-    public static readonly CallbackGame Default = new();
-  }
+  public static readonly CallbackGame Default = new();
 }
