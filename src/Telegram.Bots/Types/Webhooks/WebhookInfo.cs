2bd91616dcff43a7d7b30940775ab41dc91f12d3
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-07 20:01:18 +0530

    Refactor WebhookInfo

--- src/Telegram.Bots/Types/Webhooks/WebhookInfo.cs
+++ src/Telegram.Bots/Types/Webhooks/WebhookInfo.cs
@@ -1,29 +1,28 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Types.Webhooks;
+
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types.Webhooks
+public sealed record WebhookInfo
 {
-  public sealed record WebhookInfo
-  {
-    public Uri? Url { get; init; }
+  public Uri? Url { get; init; }
 
-    public bool HasCustomCertificate { get; init; }
+  public bool HasCustomCertificate { get; init; }
 
-    public uint PendingUpdateCount { get; init; }
+  public uint PendingUpdateCount { get; init; }
 
-    public string? IpAddress { get; init; }
+  public string? IpAddress { get; init; }
 
-    public DateTime? LastErrorDate { get; init; }
+  public DateTime? LastErrorDate { get; init; }
 
-    public string? LastErrorMessage { get; init; }
-    
-    public DateTime? LastSynchronizationErrorDate { get; init; }
+  public string? LastErrorMessage { get; init; }
 
-    public uint? MaxConnections { get; init; }
+  public DateTime? LastSynchronizationErrorDate { get; init; }
 
-    public IReadOnlyList<UpdateType>? AllowedUpdates { get; init; }
-  }
+  public uint? MaxConnections { get; init; }
+
+  public IReadOnlyList<UpdateType>? AllowedUpdates { get; init; }
 }
