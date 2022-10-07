8259ce50c4677a9cd3edbeb3c0f767e79869953e
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-07 20:05:17 +0530

    Refactor Game

--- src/Telegram.Bots/Types/Games/Game.cs
+++ src/Telegram.Bots/Types/Games/Game.cs
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Games;
 
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types.Games
+public sealed record Game
 {
-  public sealed record Game
-  {
-    public string Title { get; init; } = null!;
+  public string Title { get; init; } = null!;
 
-    public string Description { get; init; } = null!;
+  public string Description { get; init; } = null!;
 
-    public IReadOnlyList<Photo> PhotoSet { get; init; } = null!;
+  public IReadOnlyList<Photo> PhotoSet { get; init; } = null!;
 
-    public string? Text { get; init; }
+  public string? Text { get; init; }
 
-    public IReadOnlyList<MessageEntity>? TextEntities { get; init; }
+  public IReadOnlyList<MessageEntity>? TextEntities { get; init; }
 
-    public Animation? Animation { get; init; }
-  }
+  public Animation? Animation { get; init; }
 }
