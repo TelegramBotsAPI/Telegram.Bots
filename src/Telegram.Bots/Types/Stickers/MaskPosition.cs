f98c260f29f4b9510c2624ea5c5fa737a1bff925
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-07 20:02:19 +0530

    Refactor MaskPosition

--- src/Telegram.Bots/Types/Stickers/MaskPosition.cs
+++ src/Telegram.Bots/Types/Stickers/MaskPosition.cs
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Stickers
+namespace Telegram.Bots.Types.Stickers;
+
+public sealed record MaskPosition
 {
-  public sealed record MaskPosition
-  {
-    public MaskPositionPoint Point { get; init; }
+  public MaskPositionPoint Point { get; init; }
 
-    public float XShift { get; init; }
+  public float XShift { get; init; }
 
-    public float YShift { get; init; }
+  public float YShift { get; init; }
 
-    public float Scale { get; init; }
-  }
+  public float Scale { get; init; }
 }
