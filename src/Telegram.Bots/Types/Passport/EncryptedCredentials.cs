35edcab0469c6624e95487d085a51e097407ffaa
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-08 19:14:05 +0530

    Refactor EncryptedCredentials

--- src/Telegram.Bots/Types/Passport/EncryptedCredentials.cs
+++ src/Telegram.Bots/Types/Passport/EncryptedCredentials.cs
@@ -1,14 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Passport
-{
-  public sealed record EncryptedCredentials
-  {
-    public string Data { get; init; } = null!;
+namespace Telegram.Bots.Types.Passport;
 
-    public string Hash { get; init; } = null!;
-
-    public string Secret { get; init; } = null!;
-  }
-}
+public sealed record EncryptedCredentials(
+  string Data,
+  string Hash,
+  string Secret);
