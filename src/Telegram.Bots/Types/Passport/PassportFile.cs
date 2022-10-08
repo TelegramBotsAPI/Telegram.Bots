d700f68b321939192f306221fdd889aba710987b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-08 19:17:08 +0530

    Refactor PassportFile

--- src/Telegram.Bots/Types/Passport/PassportFile.cs
+++ src/Telegram.Bots/Types/Passport/PassportFile.cs
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Passport;
 
 using System;
 
-namespace Telegram.Bots.Types.Passport
+public sealed record PassportFile
 {
-  public sealed record PassportFile
-  {
-    public string Id { get; init; } = null!;
+  public string Id { get; init; } = null!;
 
-    public string UniqueId { get; init; } = null!;
+  public string UniqueId { get; init; } = null!;
 
-    public uint Size { get; init; }
+  public uint Size { get; init; }
 
-    public DateTime Date { get; init; } = DateTime.UnixEpoch;
-  }
+  public DateTime Date { get; init; } = DateTime.UnixEpoch;
 }
