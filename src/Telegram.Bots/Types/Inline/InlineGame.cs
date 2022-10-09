89930e5fe7b97724a6f351fa1e41f9237105c509
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-09 19:36:21 +0530

    Refactor InlineGame

--- src/Telegram.Bots/Types/Inline/InlineGame.cs
+++ src/Telegram.Bots/Types/Inline/InlineGame.cs
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Inline
+namespace Telegram.Bots.Types.Inline;
+
+public sealed record InlineGame : InlineResult
 {
-  public sealed record InlineGame : InlineResult
+  public override ResultType Type { get; } = ResultType.Game;
+
+  public string ShortName { get; }
+
+  public InlineGame(string id, string shortName) : base(id)
   {
-    public override ResultType Type { get; } = ResultType.Game;
-
-    public string ShortName { get; }
-
-    public InlineGame(string id, string shortName) : base(id) => ShortName = shortName;
+    ShortName = shortName;
   }
 }
