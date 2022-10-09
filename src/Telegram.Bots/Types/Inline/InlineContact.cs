522a5f32e6381c7d390fd9a22a47f131e15e24db
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-09 19:35:56 +0530

    Refactor InlineContact

--- src/Telegram.Bots/Types/Inline/InlineContact.cs
+++ src/Telegram.Bots/Types/Inline/InlineContact.cs
@@ -1,32 +1,34 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Inline;
 
 using System;
 
-namespace Telegram.Bots.Types.Inline
+public sealed record InlineContact : ReplaceableResult, IThumbable
 {
-  public sealed record InlineContact : ReplaceableResult, IThumbable
+  public override ResultType Type { get; } = ResultType.Contact;
+
+  public string PhoneNumber { get; }
+
+  public string FirstName { get; }
+
+  public string? LastName { get; init; }
+
+  public string? Vcard { get; init; }
+
+  public Uri? Thumb { get; init; }
+
+  public int? ThumbWidth { get; init; }
+
+  public int? ThumbHeight { get; init; }
+
+  public InlineContact(
+    string id,
+    string phoneNumber,
+    string firstName) : base(id)
   {
-    public override ResultType Type { get; } = ResultType.Contact;
-
-    public string PhoneNumber { get; }
-
-    public string FirstName { get; }
-
-    public string? LastName { get; init; }
-
-    public string? Vcard { get; init; }
-
-    public Uri? Thumb { get; init; }
-
-    public int? ThumbWidth { get; init; }
-
-    public int? ThumbHeight { get; init; }
-
-    public InlineContact(string id, string phoneNumber, string firstName) : base(id)
-    {
-      PhoneNumber = phoneNumber;
-      FirstName = firstName;
-    }
+    PhoneNumber = phoneNumber;
+    FirstName = firstName;
   }
 }
