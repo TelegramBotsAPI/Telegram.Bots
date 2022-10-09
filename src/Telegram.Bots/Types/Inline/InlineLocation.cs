5ef5ae282e049a9ff9fc41ec72a5bf72de8aa750
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-09 19:36:50 +0530

    Refactor InlineLocation

--- src/Telegram.Bots/Types/Inline/InlineLocation.cs
+++ src/Telegram.Bots/Types/Inline/InlineLocation.cs
@@ -1,39 +1,42 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Inline;
 
 using System;
 
-namespace Telegram.Bots.Types.Inline
+public sealed record InlineLocation : ReplaceableResult, IThumbable
 {
-  public sealed record InlineLocation : ReplaceableResult, IThumbable
+  public override ResultType Type { get; } = ResultType.Location;
+
+  public string Title { get; }
+
+  public double Latitude { get; }
+
+  public double Longitude { get; }
+
+  public double? HorizontalAccuracy { get; init; }
+
+  public int? LivePeriod { get; init; }
+
+  public uint? Heading { get; init; }
+
+  public uint? ProximityAlertRadius { get; init; }
+
+  public Uri? Thumb { get; init; }
+
+  public int? ThumbWidth { get; init; }
+
+  public int? ThumbHeight { get; init; }
+
+  public InlineLocation(
+    string id,
+    string title,
+    double latitude,
+    double longitude) : base(id)
   {
-    public override ResultType Type { get; } = ResultType.Location;
-
-    public string Title { get; }
-
-    public double Latitude { get; }
-
-    public double Longitude { get; }
-
-    public double? HorizontalAccuracy { get; init; }
-
-    public int? LivePeriod { get; init; }
-
-    public uint? Heading { get; init; }
-
-    public uint? ProximityAlertRadius { get; init; }
-
-    public Uri? Thumb { get; init; }
-
-    public int? ThumbWidth { get; init; }
-
-    public int? ThumbHeight { get; init; }
-
-    public InlineLocation(string id, string title, double latitude, double longitude) : base(id)
-    {
-      Title = title;
-      Latitude = latitude;
-      Longitude = longitude;
-    }
+    Title = title;
+    Latitude = latitude;
+    Longitude = longitude;
   }
 }
