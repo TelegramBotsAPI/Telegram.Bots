f06fc3cea5dbfa11f3828474d8f6fb1339add626
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-09 19:35:39 +0530

    Refactor InlineArticle

--- src/Telegram.Bots/Types/Inline/InlineArticle.cs
+++ src/Telegram.Bots/Types/Inline/InlineArticle.cs
@@ -1,28 +1,30 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Inline;
 
 using System;
 
-namespace Telegram.Bots.Types.Inline
+public sealed record InlineArticle : ReplaceableResult, IThumbable
 {
-  public sealed record InlineArticle : ReplaceableResult, IThumbable
+  public override ResultType Type { get; } = ResultType.Article;
+
+  public string Title { get; }
+
+  public Uri? Url { get; init; }
+
+  public bool? HideUrl { get; init; }
+
+  public string? Description { get; init; }
+
+  public Uri? Thumb { get; init; }
+
+  public int? ThumbWidth { get; init; }
+
+  public int? ThumbHeight { get; init; }
+
+  public InlineArticle(string id, string title) : base(id)
   {
-    public override ResultType Type { get; } = ResultType.Article;
-
-    public string Title { get; }
-
-    public Uri? Url { get; init; }
-
-    public bool? HideUrl { get; init; }
-
-    public string? Description { get; init; }
-
-    public Uri? Thumb { get; init; }
-
-    public int? ThumbWidth { get; init; }
-
-    public int? ThumbHeight { get; init; }
-
-    public InlineArticle(string id, string title) : base(id) => Title = title;
+    Title = title;
   }
 }
