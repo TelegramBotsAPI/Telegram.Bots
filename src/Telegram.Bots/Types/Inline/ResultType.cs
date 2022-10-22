c0d61f35b8d5d648840281cca7000b56fdc0a40b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-22 11:06:05 +0530

    Refactor ResultType

--- src/Telegram.Bots/Types/Inline/ResultType.cs
+++ src/Telegram.Bots/Types/Inline/ResultType.cs
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Inline
+namespace Telegram.Bots.Types.Inline;
+
+public enum ResultType
 {
-  public enum ResultType
-  {
-    Article,
-    Audio,
-    Contact,
-    Document,
-    Game,
-    Gif,
-    Location,
-    Mpeg4Gif,
-    Photo,
-    Sticker,
-    Venue,
-    Video,
-    Voice
-  }
+  Article,
+  Audio,
+  Contact,
+  Document,
+  Game,
+  Gif,
+  Location,
+  Mpeg4Gif,
+  Photo,
+  Sticker,
+  Venue,
+  Video,
+  Voice
 }
