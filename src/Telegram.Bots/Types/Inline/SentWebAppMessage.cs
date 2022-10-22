7c2f3e89c29fd77109ebb2a670ec754cd130b1f9
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-22 11:06:15 +0530

    Refactor SentWebAppMessage

--- src/Telegram.Bots/Types/Inline/SentWebAppMessage.cs
+++ src/Telegram.Bots/Types/Inline/SentWebAppMessage.cs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Inline
+namespace Telegram.Bots.Types.Inline;
+
+public sealed record SentWebAppMessage
 {
-  public sealed record SentWebAppMessage
-  {
-    public string? MessageId { get; init; }
-  }
+  public string? MessageId { get; init; }
 }
