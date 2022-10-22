31ef288cd9840d21de252b1ff4a0d494d49fa0ac
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-22 11:05:37 +0530

    Refactor InlineVoice

--- src/Telegram.Bots/Types/Inline/InlineVoice.cs
+++ src/Telegram.Bots/Types/Inline/InlineVoice.cs
@@ -1,41 +1,42 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Types.Inline;
 
 using System;
 using System.Collections.Generic;
 
-namespace Telegram.Bots.Types.Inline
+public abstract record InlineVoice<TVoice> : ReplaceableResult, ICaptionable
 {
-  public abstract record InlineVoice<TVoice> : ReplaceableResult, ICaptionable
+  public override ResultType Type { get; } = ResultType.Voice;
+
+  public string Title { get; }
+
+  public TVoice Voice { get; }
+
+  public string? Caption { get; init; }
+
+  public ParseMode? ParseMode { get; init; }
+
+  public IEnumerable<MessageEntity>? CaptionEntities { get; init; }
+
+  protected InlineVoice(string id, string title, TVoice voice) : base(id)
   {
-    public override ResultType Type { get; } = ResultType.Voice;
-
-    public string Title { get; }
-
-    public TVoice Voice { get; }
-
-    public string? Caption { get; init; }
-
-    public ParseMode? ParseMode { get; init; }
-
-    public IEnumerable<MessageEntity>? CaptionEntities { get; init; }
-
-    protected InlineVoice(string id, string title, TVoice voice) : base(id)
-    {
-      Title = title;
-      Voice = voice;
-    }
+    Title = title;
+    Voice = voice;
   }
+}
 
-  public sealed record InlineVoice : InlineVoice<Uri>
-  {
-    public int? Duration { get; init; }
+public sealed record InlineVoice : InlineVoice<Uri>
+{
+  public int? Duration { get; init; }
 
-    public InlineVoice(string id, string title, Uri voice) : base(id, title, voice) { }
-  }
+  public InlineVoice(string id, string title, Uri voice) : base(id, title,
+    voice) { }
+}
 
-  public sealed record InlineCachedVoice : InlineVoice<string>
-  {
-    public InlineCachedVoice(string id, string title, string voice) : base(id, title, voice) { }
-  }
+public sealed record InlineCachedVoice : InlineVoice<string>
+{
+  public InlineCachedVoice(string id, string title, string voice) : base(id,
+    title, voice) { }
 }
