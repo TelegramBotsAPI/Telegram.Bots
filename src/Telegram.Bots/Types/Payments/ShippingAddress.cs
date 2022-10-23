85cef1b23d892b8ced7ea2a425602e92e92293c1
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-23 19:26:11 +0530

    Refactor ShippingAddress

--- src/Telegram.Bots/Types/Payments/ShippingAddress.cs
+++ src/Telegram.Bots/Types/Payments/ShippingAddress.cs
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Payments
+namespace Telegram.Bots.Types.Payments;
+
+public sealed record ShippingAddress
 {
-  public sealed record ShippingAddress
-  {
-    public string CountryCode { get; init; } = null!;
+  public string CountryCode { get; init; } = null!;
 
-    public string State { get; init; } = null!;
+  public string State { get; init; } = null!;
 
-    public string City { get; init; } = null!;
+  public string City { get; init; } = null!;
 
-    public string StreetLine1 { get; init; } = null!;
+  public string StreetLine1 { get; init; } = null!;
 
-    public string StreetLine2 { get; init; } = null!;
+  public string StreetLine2 { get; init; } = null!;
 
-    public string PostCode { get; init; } = null!;
-  }
+  public string PostCode { get; init; } = null!;
 }
