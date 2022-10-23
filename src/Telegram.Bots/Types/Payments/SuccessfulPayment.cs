a9f43ab44bb78619b80e7cee49dfa2ae9bc0bec2
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-23 19:26:29 +0530

    Refactor SuccessfulPayment

--- src/Telegram.Bots/Types/Payments/SuccessfulPayment.cs
+++ src/Telegram.Bots/Types/Payments/SuccessfulPayment.cs
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Payments
+namespace Telegram.Bots.Types.Payments;
+
+public sealed record SuccessfulPayment
 {
-  public sealed record SuccessfulPayment
-  {
-    public string Currency { get; init; } = null!;
+  public string Currency { get; init; } = null!;
 
-    public int TotalAmount { get; init; }
+  public int TotalAmount { get; init; }
 
-    public string InvoicePayload { get; init; } = null!;
+  public string InvoicePayload { get; init; } = null!;
 
-    public string? ShippingOptionId { get; init; }
+  public string? ShippingOptionId { get; init; }
 
-    public OrderInfo? OrderInfo { get; init; }
+  public OrderInfo? OrderInfo { get; init; }
 
-    public string TelegramPaymentChargeId { get; init; } = null!;
+  public string TelegramPaymentChargeId { get; init; } = null!;
 
-    public string ProviderPaymentChargeId { get; init; } = null!;
-  }
+  public string ProviderPaymentChargeId { get; init; } = null!;
 }
