efa5b88564b50bfa9fe51a0dbd0631527667b286
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-23 19:26:23 +0530

    Refactor ShippingQuery

--- src/Telegram.Bots/Types/Payments/ShippingQuery.cs
+++ src/Telegram.Bots/Types/Payments/ShippingQuery.cs
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Payments
+namespace Telegram.Bots.Types.Payments;
+
+public sealed record ShippingQuery
 {
-  public sealed record ShippingQuery
-  {
-    public string Id { get; init; } = null!;
+  public string Id { get; init; } = null!;
 
-    public User From { get; init; } = null!;
+  public User From { get; init; } = null!;
 
-    public string InvoicePayment { get; init; } = null!;
+  public string InvoicePayment { get; init; } = null!;
 
-    public ShippingAddress ShippingAddress { get; init; } = null!;
-  }
+  public ShippingAddress ShippingAddress { get; init; } = null!;
 }
