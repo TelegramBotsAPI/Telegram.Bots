16b0dfd6cc18ba0980f0ff2fb7724a67e9549908
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-23 19:25:47 +0530

    Refactor Invoice

--- src/Telegram.Bots/Types/Payments/Invoice.cs
+++ src/Telegram.Bots/Types/Payments/Invoice.cs
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-namespace Telegram.Bots.Types.Payments
+namespace Telegram.Bots.Types.Payments;
+
+public sealed record Invoice
 {
-  public sealed record Invoice
-  {
-    public string Title { get; init; } = null!;
+  public string Title { get; init; } = null!;
 
-    public string Description { get; init; } = null!;
+  public string Description { get; init; } = null!;
 
-    public string StartParameter { get; init; } = null!;
+  public string StartParameter { get; init; } = null!;
 
-    public string Currency { get; init; } = null!;
+  public string Currency { get; init; } = null!;
 
-    public int TotalAmount { get; init; }
-  }
+  public int TotalAmount { get; init; }
 }
