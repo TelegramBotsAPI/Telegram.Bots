2c5b8b81ce62dfcc5072a5e33671a6babda0d9e1
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-29 21:33:27 +0530

    Refactor PollingConfig

--- src/Telegram.Bots.Extensions.Polling/Configs/PollingConfig.cs
+++ src/Telegram.Bots.Extensions.Polling/Configs/PollingConfig.cs
@@ -1,27 +1,26 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
+
+namespace Telegram.Bots.Extensions.Polling.Configs;
 
 using System.Collections.Generic;
-using Telegram.Bots.Types;
+using Types;
 
-namespace Telegram.Bots.Extensions.Polling.Configs
+public sealed class PollingConfig
 {
-  public sealed class PollingConfig
+  public int Limit { get; }
+
+  public int Timeout { get; }
+
+  public IEnumerable<UpdateType>? AllowedUpdates { get; }
+
+  public PollingConfig(
+    int limit = 100,
+    int timeout = 60,
+    IEnumerable<UpdateType>? allowedUpdates = default)
   {
-    public int Limit { get; }
-
-    public int Timeout { get; }
-
-    public IEnumerable<UpdateType>? AllowedUpdates { get; }
-
-    public PollingConfig(
-      int limit = 100,
-      int timeout = 60,
-      IEnumerable<UpdateType>? allowedUpdates = default)
-    {
-      Limit = limit;
-      Timeout = timeout;
-      AllowedUpdates = allowedUpdates;
-    }
+    Limit = limit;
+    Timeout = timeout;
+    AllowedUpdates = allowedUpdates;
   }
 }
