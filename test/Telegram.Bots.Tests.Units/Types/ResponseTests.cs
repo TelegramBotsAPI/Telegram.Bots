6a6336e9d8e1b38e7931c13548083323898bccaf
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-29 21:36:49 +0530

    Refactor ResponseTests

--- test/Telegram.Bots.Tests.Units/Types/ResponseTests.cs
+++ test/Telegram.Bots.Tests.Units/Types/ResponseTests.cs
@@ -1,33 +1,37 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
-using Telegram.Bots.Types;
+namespace Telegram.Bots.Tests.Units.Types;
+
+using Bots.Types;
 using Xunit;
 
-namespace Telegram.Bots.Tests.Units.Types
+public sealed class ResponseTests
 {
-  public sealed class ResponseTests
+  [Fact(DisplayName =
+    "Success Response has Ok as True and Failure Value as Default")]
+  public void SuccessResponseHasOkAsTrueAndFailureValueAsDefault()
   {
-    [Fact(DisplayName = "Success Response has Ok as True and Failure Value as Default")]
-    public void SuccessResponseHasOkAsTrueAndFailureValueAsDefault()
+    Response<int> response = new(4);
+
+    Assert.True(response.Ok);
+    Assert.Equal(4, response.Result);
+    Assert.Equal(default, response.Failure);
+  }
+
+  [Fact(DisplayName =
+    "Failure Response has Ok as False and Result Value as Default")]
+  public void FailureResponseHasOkAsFalseAndResultValueAsDefault()
+  {
+    Response<int> response = new(new Failure
     {
-      var response = new Response<int>(4);
+      Description = "Timeout", ErrorCode = 408
+    });
 
-      Assert.True(response.Ok);
-      Assert.Equal(4, response.Result);
-      Assert.Equal(default, response.Failure);
-    }
-
-    [Fact(DisplayName = "Failure Response has Ok as False and Result Value as Default")]
-    public void FailureResponseHasOkAsFalseAndResultValueAsDefault()
-    {
-      var response = new Response<int>(new Failure {Description = "Timeout", ErrorCode = 408});
-
-      Assert.False(response.Ok);
-      Assert.Equal(default, response.Result);
-      Assert.Equal("Timeout", response.Failure.Description);
-      Assert.Equal(408, response.Failure.ErrorCode);
-      Assert.Equal(default, response.Failure.Parameters);
-    }
+    Assert.False(response.Ok);
+    Assert.Equal(default, response.Result);
+    Assert.Equal("Timeout", response.Failure.Description);
+    Assert.Equal(408, response.Failure.ErrorCode);
+    Assert.Equal(default, response.Failure.Parameters);
   }
 }
