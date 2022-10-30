a61411fe2f87238354b5374745ad0700019bd30b
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-30 13:03:34 +0530

    Refactor InlineButtonTests

--- test/Telegram.Bots.Tests.Units/Json/InlineButtonTests.cs
+++ test/Telegram.Bots.Tests.Units/Json/InlineButtonTests.cs
@@ -1,38 +1,40 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Tests.Units.Json;
+
+using Bots.Json;
+using Bots.Types;
 using System;
-using Telegram.Bots.Json;
-using Telegram.Bots.Types;
 using Xunit;
 
-namespace Telegram.Bots.Tests.Units.Json
+public sealed class InlineButtonTests : IClassFixture<Serializer>
 {
-  public sealed class InlineButtonTests : IClassFixture<Serializer>
+  private readonly Serializer _serializer;
+
+  public InlineButtonTests(Serializer serializer)
   {
-    private readonly Serializer _serializer;
+    _serializer = serializer;
+  }
 
-    public InlineButtonTests(Serializer serializer) => _serializer = serializer;
+  public static TheoryData<(string, Type)> DeserializationData => new()
+  {
+    (@"{""url"":""https://example.com""}", typeof(UrlButton)),
+    (@"{""login_url"":{}}", typeof(LoginUrlButton)),
+    (@"{""callback_data"":""""}", typeof(CallbackDataButton)),
+    (@"{""switch_inline_query"":""""}", typeof(SwitchInlineQueryButton)),
+    (@"{""switch_inline_query_current_chat"":""""}",
+      typeof(SwitchInlineQueryCurrentChatButton)),
+    (@"{""callback_game"":{}}", typeof(CallbackGameButton)),
+    (@"{""pay"":true}", typeof(PayButton))
+  };
 
-    public static TheoryData<(string, Type)> DeserializationData => new()
-    {
-      (@"{""url"":""https://example.com""}", typeof(UrlButton)),
-      (@"{""login_url"":{}}", typeof(LoginUrlButton)),
-      (@"{""callback_data"":""""}", typeof(CallbackDataButton)),
-      (@"{""switch_inline_query"":""""}", typeof(SwitchInlineQueryButton)),
-      (@"{""switch_inline_query_current_chat"":""""}",
-        typeof(SwitchInlineQueryCurrentChatButton)),
-      (@"{""callback_game"":{}}", typeof(CallbackGameButton)),
-      (@"{""pay"":true}", typeof(PayButton))
-    };
+  [Theory(DisplayName = "InlineButton deserializes correctly")]
+  [MemberData(nameof(DeserializationData))]
+  public void InlineButtonDeserializesCorrectly((string, Type) tuple)
+  {
+    (string value, Type type) = tuple;
 
-    [Theory(DisplayName = "InlineButton deserializes correctly")]
-    [MemberData(nameof(DeserializationData))]
-    public void InlineButtonDeserializesCorrectly((string, Type) tuple)
-    {
-      var (value, type) = tuple;
-
-      Assert.IsAssignableFrom(type, _serializer.Deserialize<InlineButton>(value));
-    }
+    Assert.IsAssignableFrom(type, _serializer.Deserialize<InlineButton>(value));
   }
 }
