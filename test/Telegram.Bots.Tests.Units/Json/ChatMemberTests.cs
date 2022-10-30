58aad631e6f82fa664fa072b73398bdeba4154f6
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-30 13:03:02 +0530

    Refactor ChatMemberTests

--- test/Telegram.Bots.Tests.Units/Json/ChatMemberTests.cs
+++ test/Telegram.Bots.Tests.Units/Json/ChatMemberTests.cs
@@ -1,36 +1,38 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-// Copyright © 2020-2021 Aman Agnihotri
+// Copyright © 2020-2022 Aman Agnihotri
 
+namespace Telegram.Bots.Tests.Units.Json;
+
+using Bots.Json;
+using Bots.Types;
 using System;
-using Telegram.Bots.Json;
-using Telegram.Bots.Types;
 using Xunit;
 
-namespace Telegram.Bots.Tests.Units.Json
+public sealed class ChatMemberTests : IClassFixture<Serializer>
 {
-  public sealed class ChatMemberTests : IClassFixture<Serializer>
+  private readonly Serializer _serializer;
+
+  public ChatMemberTests(Serializer serializer)
   {
-    private readonly Serializer _serializer;
+    _serializer = serializer;
+  }
 
-    public ChatMemberTests(Serializer serializer) => _serializer = serializer;
+  public static TheoryData<(string, Type)> DeserializationData => new()
+  {
+    (@"{""status"":""creator""}", typeof(Creator)),
+    (@"{""status"":""administrator""}", typeof(Administrator)),
+    (@"{""status"":""member""}", typeof(NormalMember)),
+    (@"{""status"":""restricted""}", typeof(RestrictedMember)),
+    (@"{""status"":""left""}", typeof(LeftMember)),
+    (@"{""status"":""kicked""}", typeof(KickedMember))
+  };
 
-    public static TheoryData<(string, Type)> DeserializationData => new()
-    {
-      (@"{""status"":""creator""}", typeof(Creator)),
-      (@"{""status"":""administrator""}", typeof(Administrator)),
-      (@"{""status"":""member""}", typeof(NormalMember)),
-      (@"{""status"":""restricted""}", typeof(RestrictedMember)),
-      (@"{""status"":""left""}", typeof(LeftMember)),
-      (@"{""status"":""kicked""}", typeof(KickedMember))
-    };
+  [Theory(DisplayName = "ChatMember deserializes correctly")]
+  [MemberData(nameof(DeserializationData))]
+  public void ChatMemberDeserializesCorrectly((string, Type) tuple)
+  {
+    (string value, Type type) = tuple;
 
-    [Theory(DisplayName = "ChatMember deserializes correctly")]
-    [MemberData(nameof(DeserializationData))]
-    public void ChatMemberDeserializesCorrectly((string, Type) tuple)
-    {
-      var (value, type) = tuple;
-
-      Assert.IsAssignableFrom(type, _serializer.Deserialize<ChatMember>(value));
-    }
+    Assert.IsAssignableFrom(type, _serializer.Deserialize<ChatMember>(value));
   }
 }
