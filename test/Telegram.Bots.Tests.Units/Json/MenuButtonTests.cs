e70eff4b4643c7c0f334cce1f05cc50962cb36a9
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-10-30 13:07:29 +0530

    Refactor MenuButtonTests

--- test/Telegram.Bots.Tests.Units/Json/MenuButtonTests.cs
+++ test/Telegram.Bots.Tests.Units/Json/MenuButtonTests.cs
@@ -1,52 +1,56 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2022 Aman Agnihotri
 
+namespace Telegram.Bots.Tests.Units.Json;
+
+using Bots.Json;
+using Bots.Types;
 using System;
-using Telegram.Bots.Json;
-using Telegram.Bots.Types;
 using Xunit;
 
-namespace Telegram.Bots.Tests.Units.Json
+public sealed class MenuButtonTests : IClassFixture<Serializer>
 {
-  public sealed class MenuButtonTests : IClassFixture<Serializer>
+  private readonly Serializer _serializer;
+
+  public MenuButtonTests(Serializer serializer)
   {
-    private readonly Serializer _serializer;
+    _serializer = serializer;
+  }
 
-    public MenuButtonTests(Serializer serializer) => _serializer = serializer;
+  private static readonly Uri Uri = new("https://example.com");
 
-    private static readonly Uri Uri = new("https://example.com");
-
-    public static readonly TheoryData<(string, MenuButton)> SerializationData = new()
+  public static readonly TheoryData<(string, MenuButton)> SerializationData =
+    new()
     {
       (@"{""type"":""default""}", new DefaultMenuButton()),
       (@"{""type"":""commands""}", new CommandsMenuButton()),
-      (@"{""type"":""web_app"",""text"":""test"",""web_app"":{""url"":""https://example.com""}}",
+      (@"{""type"":""web_app"",""text"":""test""," +
+        @"""web_app"":{""url"":""https://example.com""}}",
         new WebAppMenuButton("test", new WebAppInfo(Uri)))
     };
 
-    public static TheoryData<(string, Type)> DeserializationData => new()
-    {
-      (@"{""type"":""default""}", typeof(DefaultMenuButton)),
-      (@"{""type"":""commands""}", typeof(CommandsMenuButton)),
-      (@"{""type"":""web_app""}", typeof(WebAppMenuButton))
-    };
+  public static TheoryData<(string, Type)> DeserializationData => new()
+  {
+    (@"{""type"":""default""}", typeof(DefaultMenuButton)),
+    (@"{""type"":""commands""}", typeof(CommandsMenuButton)),
+    (@"{""type"":""web_app""}", typeof(WebAppMenuButton))
+  };
 
-    [Theory(DisplayName = "MenuButton serializes correctly")]
-    [MemberData(nameof(SerializationData))]
-    public void MenuButtonSerializesCorrectly((string, MenuButton) tuple)
-    {
-      var (value, button) = tuple;
+  [Theory(DisplayName = "MenuButton serializes correctly")]
+  [MemberData(nameof(SerializationData))]
+  public void MenuButtonSerializesCorrectly((string, MenuButton) tuple)
+  {
+    (string value, MenuButton button) = tuple;
 
-      Assert.Equal(value, _serializer.Serialize(button));
-    }
+    Assert.Equal(value, _serializer.Serialize(button));
+  }
 
-    [Theory(DisplayName = "MenuButton deserializes correctly")]
-    [MemberData(nameof(DeserializationData))]
-    public void MenuButtonDeserializesCorrectly((string, Type) tuple)
-    {
-      var (value, type) = tuple;
+  [Theory(DisplayName = "MenuButton deserializes correctly")]
+  [MemberData(nameof(DeserializationData))]
+  public void MenuButtonDeserializesCorrectly((string, Type) tuple)
+  {
+    (string value, Type type) = tuple;
 
-      Assert.IsAssignableFrom(type, _serializer.Deserialize<MenuButton>(value));
-    }
+    Assert.IsAssignableFrom(type, _serializer.Deserialize<MenuButton>(value));
   }
 }
